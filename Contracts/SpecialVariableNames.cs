a500fcd039aeac18896f66c7cd1513668fce34ca
paulstovell <devnull@localhost>
2011-11-12 14:52:26 +0000

    Added a convention that allows you to specify a directory you would like your package to be copied to after PreDeploy.ps1

--- Contracts/SpecialVariableNames.cs
+++ Contracts/SpecialVariableNames.cs
@@ -4,15 +4,16 @@ namespace Octopus.Shared.Contracts
 {
     public static class SpecialVariables
     {
-        // Set by Octopus
+        // Set by Octopus Server exclusively
         public static readonly string EnvironmentName = "OctopusEnvironmentName";
-        
-        public static readonly string PackageName = "OctopusPackageName"; 
+        public static readonly string MachineName = "OctopusMachineName";
+        public static readonly string PackageName = "OctopusPackageName";
         public static readonly string PackageVersion = "OctopusPackageVersion";
         public static readonly string PackageNameAndVersion = "OctopusPackageNameAndVersion";
-        public static readonly string DateTimeToday = "OctopusDateTimeToday";
-        public static readonly string WebSiteName = "OctopusWebSiteName";
-        public static readonly string MachineName = "OctopusMachineName";
+        
+        // Defaulted by Tentacle, but overridable by user
         public static readonly string PackageDirectoryPath = "OctopusPackageDirectoryPath";
+        public static readonly string PurgePackageDirectoryBeforeCopy = "OctopusPurgePackageDirectoryBeforeCopy";
+        public static readonly string WebSiteName = "OctopusWebSiteName";     
     }
 }
