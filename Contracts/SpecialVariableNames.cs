b38e342fe7e9ed8dd6ed4aa732a5c1167721169a
paulstovell <devnull@localhost>
2011-11-12 13:10:18 +0000

    Added a few built-in special variables

--- Contracts/SpecialVariableNames.cs
+++ Contracts/SpecialVariableNames.cs
@@ -5,5 +5,12 @@ namespace Octopus.Shared.Contracts
     public static class SpecialVariables
     {
         public static readonly string EnvironmentName = "OctopusEnvironmentName";
+        public static readonly string PackageName = "OctopusPackageName";
+        public static readonly string PackageVersion = "OctopusPackageVersion";
+        public static readonly string PackageNameAndVersion = "OctopusPackageNameAndVersion";
+        public static readonly string DateTimeToday = "OctopusDateTimeToday";
+        public static readonly string WebSiteName = "OctopusWebSiteName";
+        public static readonly string MachineName = "OctopusMachineName";
+        public static readonly string PackageDirectoryPath = "OctopusPackageDirectoryPath";
     }
 }
