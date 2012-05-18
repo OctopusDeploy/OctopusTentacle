b5483a15c4d930eef94b5e83d573c6cf0ff0f283
Paul Stovell <paul@paulstovell.com>
2012-05-18 21:07:08 +0100

    Tentacle now uses environment names when calculating package installation paths. When a package already exists and -force is used, a new folder will be created instead of trying to purge the old one.

---
+++ Util/IOctopusFileSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Octopus.Shared.Util
+{
+    public interface IOctopusFileSystem
+    {
+        bool FileExists(string path);
+        bool DirectoryExists(string path);
+        void DeleteFile(string path);
+        void DeleteDirectory(string path);
+        IEnumerable<string> EnumerateDirectories(string parentDirectoryPath);
+        IEnumerable<string> EnumerateDirectoriesRecursively(string parentDirectoryPath);
+        IEnumerable<string> EnumerateFiles(string parentDirectoryPath, params string[] searchPatterns);
+        IEnumerable<string> EnumerateFilesRecursively(string parentDirectoryPath, params string[] searchPatterns);
+        long GetFileSize(string path);
+        string ReadFile(string path);
+        void AppendToFile(string path, string contents);
+        void OverwriteFile(string path, string contents);
+        Stream OpenFile(string path, FileAccess access = FileAccess.ReadWrite, FileShare share = FileShare.Read);
+        Stream CreateTemporaryFile(string extension, out string path);
+        void CopyDirectory(string sourceDirectory, string targetDirectory, int overwriteFileRetryAttempts = 3);
+        void PurgeDirectory(string targetDirectory, int deleteFileRetryAttempts = 3);
+        void EnsureDirectoryExists(string directoryPath);
+    }
+}
---
+++ Util/OctopusPhysicalFileSystem.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Octopus.Shared.Util
+{
+    public class OctopusPhysicalFileSystem : IOctopusFileSystem
+    {
+        public bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public bool DirectoryExists(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
+
+        public void DeleteDirectory(string path)
+        {
+            Directory.Delete(path, true);
+        }
+
+        public IEnumerable<string> EnumerateFiles(string parentDirectoryPath, params string[] searchPatterns)
+        {
+            return searchPatterns.Length == 0 
+                ? Directory.EnumerateFiles(parentDirectoryPath, "*", SearchOption.TopDirectoryOnly) 
+                : searchPatterns.SelectMany(pattern => Directory.EnumerateFiles(parentDirectoryPath, pattern, SearchOption.TopDirectoryOnly));
+        }
+
+        public IEnumerable<string> EnumerateFilesRecursively(string parentDirectoryPath, params string[] searchPatterns)
+        {
+            return searchPatterns.Length == 0
+                ? Directory.EnumerateFiles(parentDirectoryPath, "*", SearchOption.AllDirectories)
+                : searchPatterns.SelectMany(pattern => Directory.EnumerateFiles(parentDirectoryPath, pattern, SearchOption.AllDirectories));
+        }
+
+        public IEnumerable<string> EnumerateDirectories(string parentDirectoryPath)
+        {
+            return Directory.EnumerateDirectories(parentDirectoryPath);
+        }
+
+        public IEnumerable<string> EnumerateDirectoriesRecursively(string parentDirectoryPath)
+        {
+            return Directory.EnumerateDirectories(parentDirectoryPath, "*", SearchOption.AllDirectories);
+        } 
+
+        public long GetFileSize(string path)
+        {
+            return new FileInfo(path).Length;
+        }
+
+        public string ReadFile(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public void AppendToFile(string path, string contents)
+        {
+            File.AppendAllText(path, contents);
+        }
+
+        public void OverwriteFile(string path, string contents)
+        {
+            File.WriteAllText(path, contents);
+        }
+
+        public Stream OpenFile(string path, FileAccess access, FileShare share)
+        {
+            return new FileStream(path, FileMode.OpenOrCreate, access, share);
+        }
+
+        public Stream CreateTemporaryFile(string extension, out string path)
+        {
+            if (!extension.StartsWith("."))
+                extension = "." + extension;
+
+            path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            path = Path.Combine(path, Assembly.GetEntryAssembly().GetName().Name);
+            path = Path.Combine(path, "Temp");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            path = Path.Combine(path, Guid.NewGuid() + extension);
+
+            return OpenFile(path, FileAccess.ReadWrite, FileShare.Read);
+        }
+
+        public void PurgeDirectory(string targetDirectory, int deleteFileRetryAttempts = 3)
+        {
+            if (!DirectoryExists(targetDirectory))
+            {
+                return;
+            }
+
+            foreach (var file in EnumerateFilesRecursively(targetDirectory))
+            {
+                for (var i = 0; i < deleteFileRetryAttempts; i++)
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch
+                    {
+                        Thread.Sleep(100);
+
+                        if (i == deleteFileRetryAttempts - 1)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void EnsureDirectoryExists(string directoryPath)
+        {
+            if (!DirectoryExists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+
+        public void CopyDirectory(string sourceDirectory, string targetDirectory, int overwriteFileRetryAttempts = 3)
+        {
+            if (!DirectoryExists(sourceDirectory))
+                return;
+
+            if (!DirectoryExists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            var files = Directory.GetFiles(sourceDirectory, "*");
+            foreach (var sourceFile in files)
+            {
+                var targetFile = Path.Combine(targetDirectory, Path.GetFileName(sourceFile));
+
+                for (var i = 0; i < overwriteFileRetryAttempts; i++)
+                {
+                    try
+                    {
+                        File.Copy(sourceFile, targetFile, true);
+                    }
+                    catch
+                    {
+                        Thread.Sleep(100);
+
+                        if (i == overwriteFileRetryAttempts - 1)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+
+            foreach (var childSourceDirectory in Directory.GetDirectories(sourceDirectory))
+            {
+                var name = Path.GetFileName(childSourceDirectory);
+                var childTargetDirectory = Path.Combine(targetDirectory, name);
+                CopyDirectory(childSourceDirectory, childTargetDirectory);
+            }
+        }
+    }
+}
