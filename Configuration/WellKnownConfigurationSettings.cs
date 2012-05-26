3214881707ec1657d8e56fa5673072770cae6c9f
Paul Stovell <paul@paulstovell.com>
2012-05-26 23:56:46 +0100

    Added trusted certs for the Tentacle installer

--- Configuration/WellKnownConfigurationSettings.cs
+++ Configuration/WellKnownConfigurationSettings.cs
@@ -1,10 +1,33 @@
-﻿namespace Octopus.Shared.Configuration
+﻿using System.Linq;
+
+namespace Octopus.Shared.Configuration
 {
-    public class WellKnownConfigurationSettings
+    public static class WellKnownConfigurationSettings
     {
         /// <summary>
         /// This setting tells RavenDB where to store its data.
         /// </summary>
         public static string StoragePath = "Octopus.Storage.Path";
+
+        /// <summary>
+        /// A comma separated list of trusted Octopus certificate thumbprints.
+        /// </summary>
+        public static string TrustedOctopusThumbprints = "Tentacle.Security.TrustedOctopusThumbprints";
+
+        public static string[] GetTrustedOctopusThumbprints(this IGlobalConfiguration globalConfiguration)
+        {
+            var value = globalConfiguration.Get(TrustedOctopusThumbprints);
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return new string[0];
+            }
+
+            return value.Split(';', ',').Where(i => !string.IsNullOrWhiteSpace(i)).Select(i => i.Trim()).Distinct().ToArray();
+        }
+
+        public static void SetTrustedOctopusThumbprints(this IGlobalConfiguration globalConfiguration, string[] thumbprints)
+        {
+            globalConfiguration.Set(TrustedOctopusThumbprints, string.Join(",", thumbprints));
+        }
     }
 }
