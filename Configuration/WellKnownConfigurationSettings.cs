978679ce968f233509f112adbce5c5dea2197447
Paul Stovell <paul@paulstovell.com>
2012-05-26 18:56:51 +0100

    Updating installer to use WPF and support RavenDB
    The installer now uses WPF for the Octopus installer, and configures the
    storage path for RavenDB.

---
+++ Configuration/WellKnownConfigurationSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Octopus.Shared.Configuration
+{
+    public class WellKnownConfigurationSettings
+    {
+        /// <summary>
+        /// This setting tells RavenDB where to store its data.
+        /// </summary>
+        public static string StoragePath = "Octopus.Storage.Path";
+    }
+}
