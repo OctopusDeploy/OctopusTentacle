e7f895d79549fec8c4b99d3a0196a287a07203ca
Paul Stovell <paul@paulstovell.com>
2012-06-16 12:08:24 +0100

    Updated all links to centralize them in one file

--- Util/OutboundLinks.cs
+++ Util/OutboundLinks.cs
@@ -3,5 +3,22 @@
     public class OutboundLinks
     {
         public static readonly string WhatIsIncludedInAnonymousUsageStatistics = "http://octopusdeploy.com/documentation/security/outbound";
+        public static readonly string NuGetPackageHostingOptions = "http://octopusdeploy.com/documentation/packaging";
+        public static readonly string HowDoIGetALicense = "http://octopusdeploy.com/purchase";
+        public static readonly string ProductHomePage = "http://octopusdeploy.com";
+        public static readonly string ProductDownloadPage = "http://octopusdeploy.com/downloads";
+        public static readonly string ProductBacklog = "https://trello.com/board/octopus/4e907de70880ba000079b75c";
+        public static readonly string DocumentationHome = "http://octopusdeploy.com/documentation";
+        public static readonly string DocumentationPackaging = "http://octopusdeploy.com/documentation/packaging";
+        public static readonly string DocumentationVariables = "http://octopusdeploy.com/documentation/features/variables";
+        public static readonly string DocumentationPowerShell = "http://octopusdeploy.com/documentation/features/powershell";
+        public static readonly string ExternalToolOctoPack = "http://octopusdeploy.com/documentation/packaging/octopack";
+        public static readonly string ExternalToolOctoTools = "https://github.com/OctopusDeploy/Octopus-Tools";
+        public static readonly string HelpGeneral = "http://help.octopusdeploy.com";
+        public static readonly string HelpReportAProblem = "http://help.octopusdeploy.com/discussions/problems";
+        public static readonly string HelpAskQuestion = "http://help.octopusdeploy.com/discussions/questions";
+        public static readonly string HelpMakeSuggestion = "http://help.octopusdeploy.com/discussions/suggestions";
+        public static readonly string WhatIsMarkdown = "http://en.wikipedia.org/wiki/Markdown";
+        public static readonly string CheckForUpgrades = "http://octopusdeploy.com/downloads/upgrade";
     }
 }
