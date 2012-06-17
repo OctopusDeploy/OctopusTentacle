afbc786bf590feb342ebd3204a532ac024b2391d
Paul Stovell <paul@paulstovell.com>
2012-06-17 23:51:04 +0100

    Minor UX improvements

---
+++ Util/OctopusFileSystemModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace Octopus.Shared.Util
+{
+    public class OctopusFileSystemModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<OctopusPhysicalFileSystem>().As<IOctopusFileSystem>();
+        }
+    }
+}
