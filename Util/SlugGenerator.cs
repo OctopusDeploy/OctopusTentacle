42fa2906997abd7b4baf8fa1f243eb2a1a9ce643
Paul Stovell <paul@paulstovell.com>
2012-07-09 17:03:15 +0100

    Upgrade to Ravendb-960 and fixed import bugs

---
+++ Util/SlugGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Octopus.Shared.Util;
+
+namespace Octopus.Portal.Models.Shared
+{
+    public class SlugGenerator
+    {
+        public static string GenerateSlug(string input, Func<string, bool> isInUse)
+        {
+            input = ConvertToLowecaseAndDashes(input);
+
+            return Uniquifier.UniquifyString(input, isInUse);
+        }
+
+        static string ConvertToLowecaseAndDashes(string value)
+        {
+            value = value ?? string.Empty;
+            value = value.ToLower();
+            value = Regex.Replace(value, "\\s", "-");
+            value = new string(value.Select(x => (char.IsLetterOrDigit(x) || x == '-') ? x : '-').ToArray());
+            value = Regex.Replace(value, "-+", "-");
+            value = value.Trim('-', '/');
+            return value;
+        }
+    }
+}
