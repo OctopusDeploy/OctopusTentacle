7c777c97cad56435a5b026ef3882e7d51b591b75
Paul Stovell <paul@paulstovell.com>
2012-07-25 15:13:27 +0100

    Support ID's of older Octopus tasks

--- Activities/ActivitySerializer.cs
+++ Activities/ActivitySerializer.cs
@@ -32,7 +32,29 @@ namespace Octopus.Shared.Activities
             {
                 var serializer = new XmlSerializer(typeof(ActivityElement));
                 var result = (ActivityElement)serializer.Deserialize(xmlTextReader);
+
+                // Old versions of Octopus didn't persist an ID, so we have to generate a unique ID each time
+                var i = 1;
+                AssignId(result, ref i);
+
                 return result;
+            }
+        }
+
+        static void AssignId(ActivityElement result, ref int i)
+        {
+            if (result.Id == 0)
+            {
+                result.Id = i;
+                i++;
+            }
+
+            if (result.Children == null)
+                return;
+
+            foreach (var child in result.Children)
+            {
+                AssignId(child, ref i);
             }
         }
 
