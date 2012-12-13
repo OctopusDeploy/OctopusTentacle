323a513e6c59d577d363b637e3729eb0d62d4051
Paul Stovell <paul@paulstovell.com>
2012-12-14 08:19:40 +1000

    Reintroduced null activity log

---
+++ Activities/NullActivityLog.cs
@@ -0,0 +1,36 @@
+using System;
+using Octopus.Shared.Diagnostics;
+using log4net;
+using log4net.Core;
+
+namespace Octopus.Shared.Activities
+{
+    public class NullActivityLog : AbstractActivityLog
+    {
+        readonly ILog log;
+
+        public NullActivityLog() : this(null)
+        {
+        }
+
+        public NullActivityLog(ILog log)
+        {
+            this.log = log ?? Logger.Default;
+        }
+
+        protected override void Write(Level level, object message)
+        {
+            log.Logger.Log(typeof(NullActivityLog), level, message, null);
+        }
+
+        public override IActivityLog OverwritePrevious()
+        {
+            return this;
+        }
+
+        public override string GetLog()
+        {
+            return string.Empty;
+        }
+    }
+}
