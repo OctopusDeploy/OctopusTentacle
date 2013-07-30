23fdd9953b414eeec0a3c84e66b0916a0935b21c
Paul Stovell <paul@paulstovell.com>
2013-07-30 18:44:29 +1000

    Implemented the backup task

---
+++ Communications/Logging/IActivityLogStorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Octopus.Shared.Communications.Logging
+{
+    public interface IActivityLogStorage
+    {
+        void Append(LogMessage logMessage);
+        IList<ActivityLogTreeNode> GetLog(string correlationId);
+    }
+}
