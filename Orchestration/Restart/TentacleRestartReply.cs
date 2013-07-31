2c35127abbf3402d175bae129bfa95826c35b6c9
nblumhardt <nblumhardt@nblumhardt.com>
2013-08-01 09:45:39 +1000

    Tentacle restarter - initial version.

---
+++ Orchestration/Restart/TentacleRestartReply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Pipefish;
+
+namespace Octopus.Shared.Orchestration.Restart
+{
+    public class TentacleRestartReply : IMessage
+    {
+        public bool RestartedSuccessfully { get; private set; }
+        public string RunningVersion { get; private set; }
+
+        public TentacleRestartReply(bool restartedSuccessfully, string runningVersion)
+        {
+            RestartedSuccessfully = restartedSuccessfully;
+            RunningVersion = runningVersion;
+        }
+    }
+}
---
+++ Orchestration/Restart/TentacleRestartRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Octopus.Shared.Communications.Conversations;
+using Pipefish;
+
+namespace Octopus.Shared.Orchestration.Restart
+{
+    [BeginsConversationEndedBy(typeof(TentacleRestartReply))]
+    public class TentacleRestartRequest : IMessage
+    {
+    }
+}
