ecf30d1ac14f94f2c668404bb44419416dffbd00
nblumhardt <nblumhardt@nblumhardt.com>
2013-08-02 08:47:20 +1000

    Initial version of the upgrade orchestrator; refactoring/instrumentation next.

--- Platform/Upgrade/UpgradeTentacleCommand.cs
+++ Platform/Upgrade/UpgradeTentacleCommand.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using Octopus.Shared.Packages;
-using Pipefish;
+using Octopus.Shared.Platform.Logging;
 
 namespace Octopus.Shared.Platform.Upgrade
 {
-    public class UpgradeTentacleCommand : IMessage
+    public class UpgradeTentacleCommand : IMessageWithLogger
     {
-        public string TentacleSquid { get; set; }
-        public StoredPackage Package { get; set; }
+        public string TentacleSquid { get; private set; }
+        public StoredPackage Package { get; private set; }
+        public LoggerReference Logger { get; private set; }
+
+        public UpgradeTentacleCommand(string tentacleSquid, LoggerReference logger, StoredPackage package)
+        {
+            TentacleSquid = tentacleSquid;
+            Logger = logger;
+            Package = package;
+        }
     }
 }
--- Platform/Upgrade/UpgradeTentacleResult.cs
+++ Platform/Upgrade/UpgradeTentacleResult.cs
@@ -4,11 +4,13 @@ namespace Octopus.Shared.Platform.Upgrade
 {
     public class UpgradeTentacleResult : ResultMessage
     {
+        public string TentacleSquid { get; set; }
         public string Version { get; private set; }
 
-        public UpgradeTentacleResult(bool wasSuccessful, string details, string version)
+        public UpgradeTentacleResult(bool wasSuccessful, string details, string tentacleSquid, string version)
             : base(wasSuccessful, details)
         {
+            TentacleSquid = tentacleSquid;
             Version = version;
         }
     }
