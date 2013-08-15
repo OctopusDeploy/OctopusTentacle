9a3c6a63a88661cf5cde5fc6cd7b251c448aa6c6
nblumhardt <nblumhardt@nblumhardt.com>
2013-08-15 12:06:12 +1000

    Converted FTP/Azure steps to the new step model.

---
+++ Platform/Deploy/ServerSide/StartServerStepCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Octopus.Shared.Contracts;
+using Octopus.Shared.Platform.Deployment;
+using Octopus.Shared.Platform.Logging;
+
+namespace Octopus.Shared.Platform.Deploy.ServerSide
+{
+    public class StartServerStepCommand: StartDeploymentStepCommand
+    {
+        public PackageMetadata Package { get; private set; }
+
+        public StartServerStepCommand(LoggerReference logger, string deploymentId, string deploymentStepId, PackageMetadata package)
+            : base(logger, deploymentId, deploymentStepId)
+        {
+            Package = package;
+        }
+    }
+}
