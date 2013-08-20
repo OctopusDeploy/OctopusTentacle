10309df331972bb80d32edb2582b1ccac439cdf2
nblumhardt <nblumhardt@nblumhardt.com>
2013-08-21 09:06:45 +1000

    Slimming deployment context: move variable evaluation out of IDeploymentContext.

---
+++ Contracts/VariableDictionaryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Octopus.Shared.Contracts
+{
+    public static class VariableDictionaryExtensions
+    {
+        public static void Evaluate(this VariableDictionary variables)
+        {
+            var evaluator = new VariableEvaluator();
+            evaluator.Evaluate(variables);
+        }
+    }
+}
