026df71d68fa5d7b01cba408109e91806784e2a8
nblumhardt <nblumhardt@nblumhardt.com>
2013-08-20 14:54:08 +1000

    API test for interruptions/manual steps.

--- Web/WebRoutes.cs
+++ Web/WebRoutes.cs
@@ -112,6 +112,7 @@ namespace Octopus.Shared.Web
 
             public static class Interruptions
             {
+                public static string Template = "/api/interruptions{/id}{?skip,nonStale,regarding,pendingOnly}";
                 public static string Index = "/api/interruptions{?skip,nonStale,regarding,pendingOnly}";
                 public static string Get = "/api/interruptions/{id}";
                 public static string Submit = "/api/interruptions/{id}/submit";
