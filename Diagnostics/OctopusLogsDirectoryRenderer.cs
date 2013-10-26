2a0771fc28104fbb4ad72a31881cb7f4db010f4c
Paul Stovell <paul@paulstovell.com>
2013-10-26 14:55:22 +1000

    Rolling logs - fixes OctopusDeploy/Issues#103

--- Diagnostics/OctopusLogsDirectoryRenderer.cs
+++ Diagnostics/OctopusLogsDirectoryRenderer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text;
 using NLog;
 using NLog.LayoutRenderers;
@@ -8,6 +9,28 @@ namespace Octopus.Shared.Diagnostics
     [LayoutRenderer("octopusLogsDirectory")]
     public class OctopusLogsDirectoryRenderer : LayoutRenderer
     {
+        static OctopusLogsDirectoryRenderer()
+        {
+            // Normally, we log to a special directory under C:\Octopus\Logs. However, this folder is configurable - the user may use
+            // D:\MyOctopus\Logs instead. Since we don't know at startup, but we may still need to log some things, we'll log them to 
+            // the local application data folder instead.
+
+            var specialFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Octopus\\Logs");
+            try
+            {
+                if (!Directory.Exists(specialFolder))
+                {
+                    Directory.CreateDirectory(specialFolder);
+                }
+
+                LogsDirectory = specialFolder;
+            }
+            // ReSharper disable once EmptyGeneralCatchClause
+            catch
+            {
+            }
+        }
+
         public static string LogsDirectory = null;
 
         protected override void Append(StringBuilder builder, LogEventInfo logEvent)
