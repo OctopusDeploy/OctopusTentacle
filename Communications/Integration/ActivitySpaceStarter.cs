26176a55fe6b71b098b769f80bfa4f502c273237
nblumhardt <nblumhardt@nblumhardt.com>
2013-11-05 10:15:04 +1000

    Avoid an ObjectDisposedException on shutdown if the clock thread is still active.

--- Communications/Integration/ActivitySpaceStarter.cs
+++ Communications/Integration/ActivitySpaceStarter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Octopus.Platform.Diagnostics;
+using Pipefish;
+using Pipefish.Core;
 using Pipefish.Hosting;
 
 namespace Octopus.Shared.Communications.Integration
@@ -22,6 +24,14 @@ namespace Octopus.Shared.Communications.Integration
         public void Stop()
         {
             Log.Octopus().Verbose("Stopping activity space");
+            
+            // Stop the clock thread.
+            // It would be nice to detach all actors automatically when disposing the space,
+            // but the concurrency involved requires some more careful thought.
+            IActor clock;
+            if (space.TryGetActor(WellKnownActors.Clock, out clock))
+                space.Detach(clock);
+
             space.Dispose();
         }
     }
