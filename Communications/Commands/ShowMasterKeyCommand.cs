01134bca063a8b1b3ad09b2dacd2c0f5e2ca5bd6
nblumhardt <nblumhardt@nblumhardt.com>
2013-11-14 15:41:00 +1000

    ShowMasterKeyCommand for OctopusDeploy/Issues#15.

---
+++ Communications/Commands/ShowMasterKeyCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Octopus.Platform.Deployment.Configuration;
+using Octopus.Platform.Diagnostics;
+using Octopus.Shared.Configuration;
+using Octopus.Shared.Startup;
+
+namespace Octopus.Shared.Communications.Commands
+{
+    public class ShowMasterKeyCommand : AbstractStandardCommand
+    {
+        readonly ILog log;
+        readonly Lazy<IMasterKeyConfiguration> masterKeyConfiguration;
+
+        public ShowMasterKeyCommand(
+            ILog log,
+            Lazy<IMasterKeyConfiguration> masterKeyConfiguration,
+            IApplicationInstanceSelector selector)
+            : base(selector)
+        {
+            this.log = log;
+            this.masterKeyConfiguration = masterKeyConfiguration;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            log.Info("Octopus Master Key:");
+            var base64 = Convert.ToBase64String(masterKeyConfiguration.Value.MasterKey);
+            Console.Out.WriteLine(base64);
+        }
+    }
+}
