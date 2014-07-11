70b4fb525f8ca6925df4ae6576137aa575a3909f
nblumhardt <nblumhardt@nblumhardt.com>
2014-07-11 16:41:43 +1000

    Support rendering markdown in templates using #{Variable | Markdown} syntax.

---
+++ Variables/VariableFunctionsModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Autofac;
+using MarkdownSharp;
+using Octopus.Platform.Variables.Templates.Evaluator;
+
+namespace Octopus.Shared.Variables
+{
+    public class VariableFunctionsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            var options = new MarkdownOptions();
+            options.AutoHyperlink = true;
+            options.LinkEmails = true;
+
+            BuiltInFunctions.Register("Markdown", s => new Markdown(options).Transform(s.Trim()));
+        }
+    }
+}
