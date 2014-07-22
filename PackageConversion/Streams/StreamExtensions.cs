ebd56f171439f3b91f8460291c5a9bd0b074eece
Daniel Little <lavinski@live.com.au>
2014-07-22 15:31:11 +1000

    Added package conversion logic to SshPackageSynchronization

---
+++ PackageConversion/Streams/StreamExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Octopus.Shared.PackageConversion.Streams
+{
+    public static class StreamExtensions
+    {
+        public static void CopyTo(this Stream source, Stream destination, long length, int bufferSize = 1024 * 4)
+        {
+            var buffer = new byte[bufferSize]; // 4K is optimum
+            while (length > 0) {
+
+                var bytesToRead = (int)(length > bufferSize ? bufferSize : length);
+                length = length - bytesToRead;
+
+                int numRead = source.Read(buffer, 0, bytesToRead);
+                if (numRead <= 0) {
+                    break;
+                }
+                destination.Write(buffer, 0, numRead);
+            }
+        }
+    }
+}
