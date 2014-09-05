7cc50f7da100efa83f2ffc5a2c345ed23bff3a6f
Daniel Little <lavinski@live.com.au>
2014-09-05 13:45:56 +1000

    Fix for octopus upgrade model (had missing instance name)

--- Conventions/HostedSiphonConventions.cs
+++ Conventions/HostedSiphonConventions.cs
@@ -4,14 +4,16 @@ namespace Octopus.Shared.Conventions
 {
     public static class HostedSiphonConventions
     {
+        static string postfix = "Siphon";
+
         public static string GetHostedApplicationName(string octopusApplicationName)
         {
-            return octopusApplicationName + "Siphon";
+            return octopusApplicationName + postfix;
         }
 
         public static string GetServerApplicationNameFromHosted(string siphonApplicationName)
         {
-            var index = siphonApplicationName.IndexOf("Siphon", System.StringComparison.Ordinal);
+            var index = siphonApplicationName.IndexOf(postfix, System.StringComparison.Ordinal);
             return siphonApplicationName.Substring(0, index);
         }
     }
