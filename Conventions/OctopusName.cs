d1e5f44cf06a408b11653175283f7e620cf69c0b
Daniel Little <lavinski@live.com.au>
2014-09-17 09:17:26 +1000

    Enable health checks for hosted agent

---
+++ Conventions/OctopusName.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Octopus.Shared.Conventions
+{
+    public static class OctopusName
+    {
+        public static readonly string SelfEnvironment = "Octopus";
+        public static readonly string SiphonName = "Octopus Server";
+    }
+}
