c33eef96491195cf3f6df9028207f0b57d9de5b8
Daniel Little <lavinski@live.com.au>
2014-09-30 15:36:47 +1000

    Remove new index and expression tree builder. Also simplified GetTargetRoles

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octopus.Shared
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> ToEnumerable<T>(this IEnumerable<T> source)
+        {
+            return source;
+        }
+    }
+}
