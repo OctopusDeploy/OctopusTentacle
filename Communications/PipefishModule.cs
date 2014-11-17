1f11451e66116ac8f1b8f035c697da44863dd295
Paul Stovell <paul@paulstovell.com>
2014-11-18 09:51:53 +1000

    Switch to in memory message storage

--- Communications/PipefishModule.cs
+++ Communications/PipefishModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -27,6 +28,51 @@ using Module = Autofac.Module;
 
 namespace Octopus.Shared.Communications
 {
+    public class InMemoryMessageStore : IMessageStore
+    {
+        readonly ConcurrentDictionary<string, IMessageStoreSubscription> queues = new ConcurrentDictionary<string, IMessageStoreSubscription>();
+
+        public void Subscribe(IMessageStoreSubscription subscription)
+        {
+            if (!queues.TryAdd(subscription.Name, subscription))
+                throw new InvalidOperationException("Space already subscribed");
+        }
+
+        public bool Unsubscribe(string space)
+        {
+            IMessageStoreSubscription queue;
+            return queues.TryRemove(space, out queue);
+        }
+
+        public bool TryRevoke(Guid messageId, out Message revokedMessage)
+        {
+            foreach (var messageStoreSubscription in queues.Values)
+            {
+                if (messageStoreSubscription.TryRevoke(messageId, out revokedMessage))
+                    return true;
+            }
+
+            revokedMessage = null;
+            return false;
+        }
+
+        public void ConfirmAccepted(Message message)
+        {
+            // Do nothing, we don't support persistent messages
+        }
+
+        public void Store(Message message)
+        {
+            IMessageStoreSubscription subscribedQueue;
+            if (!queues.TryGetValue(message.To.Space, out subscribedQueue))
+            {
+                return;
+            }
+
+            subscribedQueue.Accept(message);
+        }
+    }
+
     public class PipefishModule : Module
     {
         readonly Assembly[] assemblies;
@@ -106,9 +152,7 @@ namespace Octopus.Shared.Communications
                 .As<IActivitySpaceStarter>()
                 .SingleInstance();
 
-            builder.Register(c => new DirectoryMessageStore(
-                    c.Resolve<ICommunicationsConfiguration>().MessagesDirectory,
-                    c.Resolve<IStorageStreamTransform>()))
+            builder.Register(c => new InMemoryMessageStore())
                 .As<IMessageStore>()
                 .SingleInstance();
 
