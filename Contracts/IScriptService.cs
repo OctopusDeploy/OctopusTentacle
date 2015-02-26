595af640c889ae2a0229b2385b4d61a528d2e060
Paul Stovell <paul@paulstovell.com>
2015-02-26 16:38:10 +1000

    Implemented script isolation on Tentacles

--- Contracts/IScriptService.cs
+++ Contracts/IScriptService.cs
@@ -4,7 +4,7 @@ namespace Octopus.Shared.Contracts
 {
     public interface IScriptService
     {
-        ScriptTicket StartScript(StartScriptCommand command, bool requiresLock = false);
+        ScriptTicket StartScript(StartScriptCommand command);
         ScriptStatusResponse GetStatus(ScriptStatusRequest request);
         ScriptStatusResponse CancelScript(CancelScriptCommand command);
         ScriptStatusResponse CompleteScript(CompleteScriptCommand command);
--- Contracts/StartScriptCommand.cs
+++ Contracts/StartScriptCommand.cs
@@ -1,19 +1,44 @@
 using System;
+using Newtonsoft.Json;
 
 namespace Octopus.Shared.Contracts
 {
     public class StartScriptCommand
     {
         readonly string scriptBody;
+        readonly ScriptIsolationLevel isolation;
 
-        public StartScriptCommand(string scriptBody)
+        public StartScriptCommand(string scriptBody) : this(scriptBody, null)
+        {
+        }
+
+        [JsonConstructor]
+        public StartScriptCommand(string scriptBody, ScriptIsolationLevel isolation)
         {
             this.scriptBody = scriptBody;
+            this.isolation = isolation ?? new NoIsolationLevel();
         }
 
         public string ScriptBody
         {
             get { return scriptBody; }
         }
+
+        public ScriptIsolationLevel Isolation
+        {
+            get { return isolation; }
+        }
+    }
+
+    public abstract class ScriptIsolationLevel
+    {
+    }
+
+    public class NoIsolationLevel : ScriptIsolationLevel
+    {
+    }
+
+    public class FullIsolationLevel : ScriptIsolationLevel
+    {
     }
 }
