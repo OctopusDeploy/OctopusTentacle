cd9c0b89cfcb163a2164080a775a36c36589c2bd
Henrik Andersson <henrik.a.andersson@outlook.com>
2015-03-10 12:13:06 +1000

    Fix uploading same package to multiple machines

--- Packages/PackageToAcquire.cs
+++ Packages/PackageToAcquire.cs
@@ -6,15 +6,25 @@ namespace Octopus.Shared.Packages
 {
     public class PackageToAcquire
     {
-        readonly Lazy<Stream> packageStream;
+        readonly Lazy<byte[]> packageStream;
 
         public PackageToAcquire(IPackageAcquirer packageAcquirer, PackageMetadata package, IFeed feed, PackageCachePolicy packageCachePolicy)
         {
-            packageStream = new Lazy<Stream>(() => 
-                packageAcquirer.Download(package, feed, packageCachePolicy));
+            packageStream = new Lazy<byte[]>(() =>
+            {
+                var downloadedPackage = packageAcquirer.Download(package, feed, packageCachePolicy);
+                var length = downloadedPackage.Length;
+                var buffer = new byte[1024*128];
+                while (length > 0)
+                {
+                    var read = downloadedPackage.Read(buffer, 0, (int) Math.Min(buffer.Length, length));
+                    length -= read;
+                }
+                return buffer;
+            });
         }
 
-        public Stream Download()
+        public byte[] Download()
         {
             return packageStream.Value;
         }
