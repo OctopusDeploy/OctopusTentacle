00dcaa78543b8aaab31e13a6dd0c08883be21a9d
Paul Stovell <paul@paulstovell.com>
2015-05-11 14:03:31 +1000

    Move ReSharper annotations to Octopus.Shared

---
+++ Properties/HtmlAttributeValueAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Octopus.Shared.Properties
+{
+    [AttributeUsage(
+        AttributeTargets.Parameter | AttributeTargets.Field |
+        AttributeTargets.Property, Inherited = true)]
+    public sealed class HtmlAttributeValueAttribute : Attribute
+    {
+        public HtmlAttributeValueAttribute([NotNull] string name)
+        {
+            Name = name;
+        }
+
+        [NotNull] public string Name { get; private set; }
+    }
+}
---
+++ Properties/NotNullAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Octopus.Shared.Properties
+{
+    /// <summary>
+    /// Indicates that the value of the marked element could never be <c>null</c>
+    /// </summary>
+    /// <example><code>
+    /// [NotNull] public object Foo() {
+    ///   return null; // Warning: Possible 'null' assignment
+    /// }
+    /// </code></example>
+    [AttributeUsage(
+        AttributeTargets.Method | AttributeTargets.Parameter |
+        AttributeTargets.Property | AttributeTargets.Delegate |
+        AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public sealed class NotNullAttribute : Attribute { }
+}
