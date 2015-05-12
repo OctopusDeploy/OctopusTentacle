ed4fdebc1677f68d581248090abde92a7a421a5a
Robert Erez <robert.erez@gmail.com>
2015-05-12 11:04:05 +1000

    move environment variable magic strings into central static class

---
+++ Variables/EnvironmentVariables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octopus.Shared.Variables
+{
+    public static class EnvironmentVariables
+    {
+        public const string TentacleVersion = "TentacleVersion";
+
+        public const string TentacleHome = "TentacleHome";
+
+        public const string TentacleApplications = "TentacleApplications";
+
+        public const string TentacleJournal = "TentacleJournal";
+
+        public const string TentacleInstanceName = "TentacleInstanceName";
+
+        public const string TentacleExecutablePath = "TentacleExecutablePath";
+    }
+}
