258fc397afe9acb962b27300d8f8df309034c85c
Ian Mercer <ian@signswift.com>
2015-05-18 15:41:27 +1000

    Add a utility class for MimeTypes to/from file Extensions implementing only the ones we care about

---
+++ Util/MimeTypes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octopus.Shared.Util
+{
+    /// <summary>
+    /// MimeType to/from file extension mappings
+    /// </summary>
+    /// <remarks>
+    /// Using System.Web or Nancy a more complete set could be created, or we can
+    /// add mappings here as needed.
+    /// </remarks>
+    public class MimeType
+    {
+        public static string ForExtension(string extension) 
+        {
+            switch (extension.ToLowerInvariant().TrimStart('.'))
+            {
+                case "png":
+                    return "image/png";
+                case "jpg":
+                    return "image/jpg";
+                default:
+                    throw new Exception("Mimetype not currently mapped for extension '" + extension + "'");
+            }
+        }
+
+        public static string ToExtension(string mimeType)
+        {
+            switch (mimeType.ToLowerInvariant())
+            {
+                case "image/png":
+                    return ".png";
+                case "image/jpg":
+                    return ".jpg";
+                default:
+                    throw new Exception("Mimetype not currently mapped: '" + mimeType + "'");
+            }
+        }
+    }; 
+}
