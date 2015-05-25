afa7e8ef0d23fbd671c6897024579f31b10a1944
Ian Mercer <ian@signswift.com>
2015-05-25 15:43:02 +1000

    Change ImportResult to a Summary<T> class that can handle arbitrary key, increment values Change Json comparison used to check objects to ignore excluded fields (e.g. Machines now match) Add some machines to MockDatabase Bug fixes in script import/export

--- Util/ReplaceStatus.cs
+++ Util/ReplaceStatus.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Octopus.Shared.Util
 {
     public enum ReplaceStatus
     {
         Created,
+        Deleted,
         Updated,
-        Skipped
+        Skipped,
+        MissingDependencies,
+        PropertyAdded,
+        PropertyDeleted,
+        PropertySkipped,
+        PropertyUpdated
     }
 }
