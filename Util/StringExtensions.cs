2af75cecaff04c8e171d6818be0b83b042a0ff01
Ian Mercer <ian@signswift.com>
2015-07-09 14:55:32 -0700

    Clean up key generation, remove all GetHashcode calls, limit ToString() output to a sensible length Add extension methods for limiting strings with Ellipses (obeying  total character count)

--- Util/StringExtensions.cs
+++ Util/StringExtensions.cs
@@ -12,11 +12,28 @@ namespace Octopus.Shared.Util
         {
             text = text ?? string.Empty;
             text = text.Split('\n').First();
-            if (text.Length > length)
-            {
-                text = text.Substring(0, length) + "...";
-            }
-            return text.Trim();
+            return text.LimitWithEllipses(length);
+        }
+
+        /// <summary>
+        /// Substring but OK if shorter
+        /// </summary>
+        public static string Limit(this string str, int characterCount)
+        {
+            if (string.IsNullOrEmpty(str)) return string.Empty;
+            if (str.Trim().Length <= characterCount) return str.Trim();
+            else return str.Substring(0, characterCount).Trim();
+        }
+
+        /// <summary>
+        /// Substring with elipses but OK if shorter, will take 3 characters off character count if necessary
+        /// </summary>
+        public static string LimitWithEllipses(this string str, int characterCount)
+        {
+            if (string.IsNullOrEmpty(str)) return string.Empty;
+            if (characterCount < 5) return str.Limit(characterCount);       // Can't do much with such a short limit
+            if (str.Trim().Length <= characterCount) return str.Trim();
+            else return str.Substring(0, characterCount - 3) + "...";
         }
     }
 }
