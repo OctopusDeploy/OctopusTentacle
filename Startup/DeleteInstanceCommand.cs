d876fcb797b0266660b1b7cf211b69980291519e
Damian Brady <dev@damianbrady.com.au>
2015-07-10 15:08:10 +1000

    High availability - register nodes on install and uninstall

--- Startup/DeleteInstanceCommand.cs
+++ Startup/DeleteInstanceCommand.cs
@@ -1,16 +1,20 @@
 using System;
+using System.Data.SqlClient;
 using Octopus.Shared.Configuration;
+using Octopus.Shared.Diagnostics;
 
 namespace Octopus.Shared.Startup
 {
     public class DeleteInstanceCommand : AbstractCommand
     {
         readonly IApplicationInstanceSelector instanceSelector;
+        readonly ILog log;
         string instanceName;
 
-        public DeleteInstanceCommand(IApplicationInstanceSelector instanceSelector)
+        public DeleteInstanceCommand(IApplicationInstanceSelector instanceSelector, ILog log)
         {
             this.instanceSelector = instanceSelector;
+            this.log = log;
             Options.Add("instance=", "Name of the instance to delete", v => instanceName = v);
         }
 
@@ -18,12 +22,35 @@ namespace Octopus.Shared.Startup
         {
             if (string.IsNullOrWhiteSpace(instanceName))
             {
+                instanceSelector.LoadDefaultInstance();
+                var connectionString = instanceSelector.Current.Configuration.Get("Octopus.Storage.ExternalDatabaseConnectionString");
+                var defaultInstanceName = instanceSelector.Current.InstanceName;
                 instanceSelector.DeleteDefaultInstance();
+                DeregisterInstance(connectionString, defaultInstanceName);
             }
             else
             {
+                instanceSelector.LoadInstance(instanceName);
+                var connectionString = instanceSelector.Current.Configuration.Get("Octopus.Storage.ExternalDatabaseConnectionString");
                 instanceSelector.DeleteInstance(instanceName);
+                DeregisterInstance(connectionString, instanceName);
             }
         }
+
+        void DeregisterInstance(string connectionString, string serverName)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                const string commandText = "DELETE FROM OctopusServerNode WHERE [Id] = @serverName";
+                using (var cmd = new SqlCommand(commandText, connection))
+                {
+                    cmd.Parameters.AddWithValue("serverName", serverName);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            log.InfoFormat("Deregistered {0} from the database", serverName);
+        }
+
     }
 }
