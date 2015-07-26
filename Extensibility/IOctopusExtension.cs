6e6ea5f35a7a4a09b948b0ae010d37efa2204ad4
Paul Stovell <paul@paulstovell.com>
2015-07-27 06:12:50 +1000

    Bring back plugins module
    Fixes OctopusDeploy/Issues#1718

---
+++ Extensibility/IOctopusExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Octopus.Shared.Extensibility
+{
+    public interface IOctopusExtension
+    {
+        void Load(ContainerBuilder builder);
+    }
+}
