e3e854dfccd1cb9575c0a320ee19c8aad0114fe1
Paul Stovell <paul@paulstovell.com>
2015-08-25 13:49:40 +1000

    Fork Azure TransientFaultHandling to our own, to get Azure out of our stack traces and to reduce Bcl.Build dependencies

---
+++ TransientFaultHandling/RetryLimitExceededException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Octopus.Shared.TransientFaultHandling
+{
+    /// <summary>
+    /// The special type of exception that provides managed exit from a retry loop. The user code can use this
+    /// exception to notify the retry policy that no further retry attempts are required.
+    /// </summary>
+    [Obsolete("You should use cancellation tokens or other means of stoping the retry loop.")]
+    public sealed class RetryLimitExceededException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Octopus.Shared.TransientFaultHandling.RetryLimitExceededException" /> class with a default error message.
+        /// </summary>
+        public RetryLimitExceededException() : this("Retry limit exceeded")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Octopus.Shared.TransientFaultHandling.RetryLimitExceededException" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public RetryLimitExceededException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Octopus.Shared.TransientFaultHandling.RetryLimitExceededException" /> class with a reference to the inner exception
+        /// that is the cause of this exception.
+        /// </summary>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        public RetryLimitExceededException(Exception innerException) : base((innerException != null) ? innerException.Message : "Retry limit exceeded", innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Octopus.Shared.TransientFaultHandling.RetryLimitExceededException" /> class with a specified error message and inner exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        public RetryLimitExceededException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ TransientFaultHandling/TransientErrorIgnoreStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Octopus.Shared.TransientFaultHandling
+{
+    /// <summary>
+    /// Implements a strategy that ignores any transient errors.
+    /// </summary>
+    sealed class TransientErrorIgnoreStrategy : ITransientErrorDetectionStrategy
+    {
+        /// <summary>
+        /// Always returns false.
+        /// </summary>
+        /// <param name="ex">The exception.</param>
+        /// <returns>Always false.</returns>
+        public bool IsTransient(Exception ex)
+        {
+            return false;
+        }
+    }
+}
