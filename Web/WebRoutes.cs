c198ae790d5bbaa7909084f53d9fb2c35de1ce40
Shane Gill <tothegills@gmail.com>
2015-08-26 14:53:20 +1000

    Use active instead of specific state when counting tasks for a node

--- Web/WebRoutes.cs
+++ Web/WebRoutes.cs
@@ -175,8 +175,8 @@ namespace Octopus.Shared.Web
 
             public static class Tasks
             {
-                public static string Template = "~/api/tasks{/id}{?skip,active,environment,project,name,node,state}";
-                public static string Index = "~/api/tasks{?skip,active,environment,project,name,node,state}";
+                public static string Template = "~/api/tasks{/id}{?skip,active,environment,project,name,node}";
+                public static string Index = "~/api/tasks{?skip,active,environment,project,name,node}";
                 public static string Get = "~/api/tasks/{id}";
                 public static string Details = "~/api/tasks/{id}/details{?verbose,tail}";
                 public static string Raw = "~/api/tasks/{id}/raw";
