797df3300168f6aded0f4e869c088d4b9c0d623b
Shane Gill <tothegills@gmail.com>
2015-09-08 12:36:02 +1000

    Add variable to turn off delta compression

--- Packages/PackageAcquisition.cs
+++ Packages/PackageAcquisition.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Octostache;
 
 namespace Octopus.Shared.Packages
 {
@@ -10,16 +8,18 @@ namespace Octopus.Shared.Packages
         readonly PackageMetadata package;
         readonly NuGetFeedProperties feed;
         readonly bool preferDownloadOnAgent;
+        readonly bool deltaCompressionEnabled;
         readonly PackageCachePolicy packageCachePolicy;
         readonly IEnumerable<string> targetRoles;
         readonly PackageAcquisitionKey key;
 
-        public PackageAcquisition(string actionId, PackageMetadata package, NuGetFeedProperties feed, bool preferDownloadOnAgent, IEnumerable<string> targetRoles, PackageCachePolicy packageCachePolicy)
+        public PackageAcquisition(string actionId, PackageMetadata package, NuGetFeedProperties feed, bool preferDownloadOnAgent, bool deltaCompressionEnabled, IEnumerable<string> targetRoles, PackageCachePolicy packageCachePolicy)
         {
             ActionId = actionId;
             this.package = package;
             this.feed = feed;
             this.preferDownloadOnAgent = preferDownloadOnAgent;
+            this.deltaCompressionEnabled = deltaCompressionEnabled;
             this.packageCachePolicy = packageCachePolicy;
             this.targetRoles = targetRoles.ToArray();
             key = new PackageAcquisitionKey(package.PackageId, package.Version, feed.Id);
@@ -33,6 +33,11 @@ namespace Octopus.Shared.Packages
         public bool PreferDownloadOnAgent
         {
             get { return preferDownloadOnAgent; }
+        }
+
+        public bool DeltaCompressionEnabled
+        {
+            get {  return deltaCompressionEnabled; }
         }
 
         public IEnumerable<string> TargetRoles
