cd55f3d0dc6bc87d3cdfe913d09a171ef30ad926
Rob <robert.erez@octopus.com>
2016-03-18 10:01:52 +1000

    Add version information to header of server installer along with warning message indicating that they shouldnt upgrade existing installs

---
+++ Util/SemanticVersionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+using Octopus.Shared.Versioning;
+
+namespace Octopus.Shared.Util
+{
+    public static class SemanticVersionInfoExtensions
+    {
+        static readonly Regex AlphaTagMatch = new Regex("^alpha\\.*[0-9]*$");
+
+        public static bool IsEarlyAccessProgram(this SemanticVersionInfo semanticVersion)
+        {
+            return AlphaTagMatch.IsMatch(semanticVersion.PreReleaseTag);
+        }
+    }
+}
