83a06cb70721cb902c3b6501ad540736834283f5
Mark Siedle <mark.siedle@octopus.com>
2016-05-03 11:08:26 +1000

    Adding integration test for key collisions on importer

--- Util/ReplaceStatus.cs
+++ Util/ReplaceStatus.cs
@@ -7,7 +7,7 @@ namespace Octopus.Shared.Util
         public static readonly ReplaceStatus Created = new ReplaceStatus("Created");
         public static readonly ReplaceStatus Deleted = new ReplaceStatus("Deleted");
         public static readonly ReplaceStatus Updated = new ReplaceStatus("Updated");
-        public static readonly ReplaceStatus SkippedDuplicate = new ReplaceStatus("Skipped (duplicate)");
+        public static readonly ReplaceStatus SkippedKeyCollision = new ReplaceStatus("Skipped (key collision)");
         public static readonly ReplaceStatus SkippedIncomplete = new ReplaceStatus("Skipped (incomplete)");
         public static readonly ReplaceStatus SkippedTooOld = new ReplaceStatus("Skipped (maxage)");
         public static readonly ReplaceStatus SkippedNoOverwrite = new ReplaceStatus("Skipped (no overwrite)");
