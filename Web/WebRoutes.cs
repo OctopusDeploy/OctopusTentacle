0aa2bffbcb7bfcb6a430e070600651cc5cba05cf
Robert Wagner <robert.wagner@octopus.com>
2016-06-28 11:48:24 +1000

    Added regardingAny parameter to events API. Fixes https://github.com/OctopusDeploy/Issues/issues/2566

--- Web/WebRoutes.cs
+++ Web/WebRoutes.cs
@@ -197,8 +197,8 @@ namespace Octopus.Shared.Web
 
             public static class Events
             {
-                public static string Template = "~/api/events{/id}{?skip,regarding,user,from,to}";
-                public static string Index = "~/api/events{?skip,regarding,user,from,to}";
+                public static string Template = "~/api/events{/id}{?skip,regarding,regardingAny,user,from,to}";
+                public static string Index = "~/api/events{?skip,regarding,regardingAny,user,from,to}";
                 public static string Get = "~/api/events/{id}";
             }
 
