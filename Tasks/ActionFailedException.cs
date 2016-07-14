ce211e7a3084d0ea237b8e13966a68c4b3ddfcc3
Michael Noonan <michaelnoonan@gmail.com>
2016-07-14 11:40:19 +1000

    This argument actually contains ActionName and MachineName.

--- Tasks/ActionFailedException.cs
+++ Tasks/ActionFailedException.cs
@@ -4,7 +4,7 @@ namespace Octopus.Shared.Tasks
 {
     public class ActionFailedException : Exception
     {
-        public ActionFailedException(string actionName, Exception innerException) : base(actionName, innerException)
+        public ActionFailedException(string actionDescription, Exception innerException) : base(actionDescription, innerException)
         {
         }
     }
