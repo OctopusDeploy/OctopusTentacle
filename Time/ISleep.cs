0ed3301cc177db7ebee793d97389ee0e48f90a60
Shannon Lewis <slewis74@gmail.com>
2016-09-05 11:23:19 +1000

    setting up UsernamePassword as an extension

--- Time/ISleep.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Octopus.Shared.Time
-{
-    public interface ISleep
-    {
-        void For(int milliseconds);
-    }
-}
--- Time/Sleep.cs
+++ Time/Sleep.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using Octopus.Server.Extensibility.Time;
 
 namespace Octopus.Shared.Time
 {
