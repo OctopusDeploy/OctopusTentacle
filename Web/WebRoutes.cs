f1b58f9f56fbc297e6b3dd44374ba09fca7edbc8
Pawel Pabich <pawel@pabich.eu>
2016-10-07 09:58:18 +1000

    Very basic update detection implemented

--- Web/WebRoutes.cs
+++ Web/WebRoutes.cs
@@ -406,6 +406,7 @@ namespace Octopus.Shared.Web
             {
                 public static string Template = "~/api/librarysteptemplates{/id}";
                 public static string Get = "~/api/librarysteptemplates/{id}";
+                public static string InstalledTemplate = "~/api/librarysteptemplates/{id}/actiontemplate";
             }
         }
 
