59c4f639daa428e88ab1b8968b09bf667b3f0d62
Michael Richardson <michael.james.richardson@gmail.com>
2016-10-31 08:57:16 +1000

    Unarchive and permanently delete certificates

--- Web/WebRoutes.cs
+++ Web/WebRoutes.cs
@@ -359,6 +359,7 @@ namespace Octopus.Shared.Web
                 public static string Index = "~/api/certificates{?skip,search,archived}";
                 public static string Get = "~/api/certificates/{id}";
                 public static string Archive = "~/api/certificates/{id}/archive";
+                public static string Unarchive = "~/api/certificates/{id}/unarchive";
                 public static string Usage = "~/api/certificates/{id}/usage";
             }
 
