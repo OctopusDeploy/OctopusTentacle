55a1fe82199b3a9ae9567fce0bd5711bf732ca51
Mark Siedle <mark.siedle@octopus.com>
2016-12-01 13:17:39 +1000

    Latest Calamari to fix some bugs and Shane's awesome safety check to make things bettera

--- Util/EnvironmentHelper.cs
+++ Util/EnvironmentHelper.cs
@@ -11,87 +11,43 @@ namespace Octopus.Shared.Util
     {
         public static string[] SafelyGetEnvironmentInformation()
         {
-            var envVars = SafelyGetEnvironmentVars()
-                .Concat(SafelyGetPathVars())
-                .Concat(SafelyGetProcessVars())
-                .Concat(SafelyGetComputerInfoVars());
+            var envVars = GetEnvironmentVars()
+                .Concat(GetPathVars())
+                .Concat(GetProcessVars());
             return envVars.ToArray();
         }
 
-        static IEnumerable<string> SafelyGetEnvironmentVars()
+        static string SafelyGet(Func<string> thingToGet)
         {
             try
             {
-                return GetEnvironmentVars();
+                return thingToGet.Invoke();
             }
-            catch
+            catch (Exception)
             {
-                // Fail silently
-                return Enumerable.Empty<string>();
+                return "Unable to retrieve environment information.";
             }
         }
+
         static IEnumerable<string> GetEnvironmentVars()
         {
-            yield return $"OperatingSystem: {Environment.OSVersion.ToString()}";
-            yield return $"OsBitVersion: {(Environment.Is64BitOperatingSystem ? "x64" : "x86")}";
-            yield return $"Is64BitProcess: {Environment.Is64BitProcess.ToString()}";
-            yield return $"CurrentUser: {System.Security.Principal.WindowsIdentity.GetCurrent().Name}";
-            yield return $"MachineName: {Environment.MachineName}";
-            yield return $"ProcessorCount: {Environment.ProcessorCount.ToString()}";
+            yield return SafelyGet(() => $"OperatingSystem: {Environment.OSVersion}");
+            yield return SafelyGet(() => $"OsBitVersion: {(Environment.Is64BitOperatingSystem ? "x64" : "x86")}");
+            yield return SafelyGet(() => $"Is64BitProcess: {Environment.Is64BitProcess}");
+            yield return SafelyGet(() => $"CurrentUser: {System.Security.Principal.WindowsIdentity.GetCurrent().Name}");
+            yield return SafelyGet(() => $"MachineName: {Environment.MachineName}");
+            yield return SafelyGet(() => $"ProcessorCount: {Environment.ProcessorCount}");
         }
 
-        static IEnumerable<string> SafelyGetPathVars()
-        {
-            try
-            {
-                return GetPathVars();
-            }
-            catch
-            {
-                // Fail silently
-                return Enumerable.Empty<string>();
-            }
-        }
         static IEnumerable<string> GetPathVars()
         {
-            yield return $"CurrentDirectory: {Directory.GetCurrentDirectory()}";
-            yield return $"TempDirectory: {Path.GetTempPath()}";
+            yield return SafelyGet(() => $"CurrentDirectory: {Directory.GetCurrentDirectory()}");
+            yield return SafelyGet(() => $"TempDirectory: {Path.GetTempPath()}");
         }
 
-        static IEnumerable<string> SafelyGetProcessVars()
-        {
-            try
-            {
-                return GetProcessVars();
-            }
-            catch
-            {
-                // Fail silently
-                return Enumerable.Empty<string>();
-            }
-        }
         static IEnumerable<string> GetProcessVars()
         {
-            yield return $"HostProcessName: {Process.GetCurrentProcess().ProcessName}";
-        }
-
-        static IEnumerable<string> SafelyGetComputerInfoVars()
-        {
-            try
-            {
-                return Enumerable.Empty<string>();
-            }
-            catch
-            {
-                // Fail silently
-                return Enumerable.Empty<string>();
-            }
-        }
-        static IEnumerable<string> GetComputerInfoVars()
-        {
-            var computerInfo = new ComputerInfo();
-            yield return $"TotalPhysicalMemory: {computerInfo.TotalPhysicalMemory.ToFileSizeString()}";
-            yield return $"AvailablePhysicalMemory: {computerInfo.AvailablePhysicalMemory.ToFileSizeString()}";
+            yield return SafelyGet(() => $"HostProcessName: {Process.GetCurrentProcess().ProcessName}");
         }
     }
 }
