13a0ab6ad8ade1af510731afd21b0909f088b359
Michael Richardson <michael.james.richardson@gmail.com>
2016-12-16 17:06:25 +1000

    Mask multi-line sensitive values

--- Security/Masking/SensitiveDataMask.cs
+++ Security/Masking/SensitiveDataMask.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Octopus.Shared.Security.Masking
@@ -11,6 +13,9 @@ namespace Octopus.Shared.Security.Masking
         public static readonly string Mask = "********";
         readonly List<string> valuesLongestToShortest = new List<string>();
         readonly List<string> valuesShortestToLongest = new List<string>();
+        readonly List<string[]> multiLineValues = new List<string[]>();
+        readonly List<PotentialMultiLineMatch> potentialMultiLineMatches = new List<PotentialMultiLineMatch>();
+        readonly Queue<KeyValuePair<string, Action<string>>> pendingActions = new Queue<KeyValuePair<string, Action<string>>>();   
         readonly StringBuilder builder = new StringBuilder();
         readonly object sync = new object();
 
@@ -35,40 +40,161 @@ namespace Octopus.Shared.Security.Masking
             if (string.IsNullOrWhiteSpace(sensitive) || sensitive.Length < 4)
                 return;
 
+            var lines = sensitive.Split(new[] {"\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries);
+
             lock (sync)
             {
-                valuesLongestToShortest.Add(sensitive);
-                valuesShortestToLongest.Add(sensitive);
+                if (lines.Length > 1)
+                {
+                    multiLineValues.Add(lines);
+                }
+                else
+                {
+                    valuesLongestToShortest.Add(sensitive);
+                    valuesShortestToLongest.Add(sensitive);
 
-                valuesLongestToShortest.Sort(StringLengthComparer.LongestToShortest);
-                valuesShortestToLongest.Sort(StringLengthComparer.ShortestToLongest);
+                    valuesLongestToShortest.Sort(StringLengthComparer.LongestToShortest);
+                    valuesShortestToLongest.Sort(StringLengthComparer.ShortestToLongest);
+                }
             }
         }
 
-        public string ApplyTo(string raw)
+        /// <summary>
+        /// Masks instances of sensitive values and invokes the supplied action with the sanitized string. 
+        /// The reason this is implemented as a callback rather than directly returning the sanitized value is
+        /// in the case of multi-line sensitive values.  If a raw string is detected as potential multi-line match,
+        /// sanitizing and invoking the callback will be delayed until it is confirmed it is/isn't a match, or until 
+        /// Flush() is called.
+        /// </summary>
+        public void ApplyTo(string raw, Action<string> action)
         {
-            if (string.IsNullOrEmpty(raw) || raw.Length < 4 || valuesLongestToShortest.Count == 0)
-                return raw;
-
             lock (sync)
             {
-                if (!HasSensitiveValues(raw))
-                    return raw;
-
-                // It's only worth allocating the new string if we actually found a match
-                builder.Clear();
-                builder.EnsureCapacity(raw.Length);
-                builder.Append(raw);
-                foreach (var sensitive in valuesLongestToShortest)
+                if (!ApplyMultiLineValuesTo(raw, action))
                 {
-                    builder.Replace(sensitive, Mask);
+                    ApplySingleLineValuesTo(raw, action);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Process any queued potential multi-line matches 
+        /// </summary>
+        public void Flush()
+        {
+            lock (sync)
+            {
+                while (pendingActions.Any())
+                {
+                    var pending = pendingActions.Dequeue();
+                    ApplySingleLineValuesTo(pending.Key, pending.Value);
+                }
+            }
+        }
+
+        void ApplySingleLineValuesTo(string raw, Action<string> action)
+        {
+            if (string.IsNullOrEmpty(raw) || raw.Length < 4 || valuesLongestToShortest.Count == 0 
+                || !ContainsSensitiveValues(raw))
+            {
+                action(raw);
+                return;
+            }
+
+            // It's only worth allocating the new string if we actually found a match
+            action(PerformMask(raw, valuesLongestToShortest));
+        }
+
+        string PerformMask(string raw, IList<string> values)
+        {
+            builder.Clear();
+            builder.EnsureCapacity(raw.Length);
+            builder.Append(raw);
+            foreach (var sensitive in values)
+            {
+                builder.Replace(sensitive, Mask);
+            }
+
+            return builder.ToString();
+        }
+
+        bool ApplyMultiLineValuesTo(string raw, Action<string> action)
+        {
+            var actionQueued = false; 
+
+            // If there are existing partial matches 
+            if (potentialMultiLineMatches.Any())
+            {
+                var rejectedPotentialMatches = new List<PotentialMultiLineMatch>();
+
+                foreach (var potentialMatch in potentialMultiLineMatches)
+                {
+                    // If the next line is still a potential match
+                    if (raw.Contains(potentialMatch.NextLine))
+                    {
+                        // Is it the final line 
+                        if (potentialMatch.Line == potentialMatch.MultilineValue.Length - 1)
+                        {
+                            // We have matched a multi-line value.  Rather than masking every-line, we will mask only the first line
+                            action(PerformMask(potentialMatch.FirstMatch, 
+                                valuesLongestToShortest.Concat(new[] {potentialMatch.MultilineValue.First()}).OrderByDescending(s => s.Length).ToList()));
+
+                            // At this point we will clear any other potential matches.
+                            // This certainly leaves some edge cases where multi-line values overlap, 
+                            // but they are not what this was designed to deal with.
+                            potentialMultiLineMatches.Clear();
+                            pendingActions.Clear();
+                            return true;
+                        }
+
+                        potentialMatch.AddMatch(raw);
+
+                        if (!actionQueued)
+                        {
+                            pendingActions.Enqueue(new KeyValuePair<string, Action<string>>(raw, action));
+                            actionQueued = true;
+                        }
+                    }
+                    else
+                    {
+                        // The multi-line match has missed, so we will remove it from the potential matches
+                        rejectedPotentialMatches.Add(potentialMatch);
+                    }
                 }
 
-                return builder.ToString();
+                // Remove all rejected matches
+                foreach (var miss in rejectedPotentialMatches)
+                    potentialMultiLineMatches.Remove(miss);
+
+                // Process any actions
+                while (pendingActions.Any() && potentialMultiLineMatches.All(potentialMatch => !potentialMatch.Matches.Contains(pendingActions.Peek().Key)))
+                {
+                    var pendingAction = pendingActions.Dequeue();
+                    ApplySingleLineValuesTo(pendingAction.Key, pendingAction.Value);
+                }
             }
+
+            // Check if the first line of any multi-line values match  
+            foreach (var multiLineValue in multiLineValues)
+            {
+                if (raw.Contains(multiLineValue.First()))
+                {
+                    var potentialMatch = new PotentialMultiLineMatch(multiLineValue, raw);
+                    potentialMultiLineMatches.Add(potentialMatch);
+                    potentialMultiLineMatches.Sort((x, y) => y.MultilineValue.Length.CompareTo(x.MultilineValue.Length));
+
+                    if (!actionQueued)
+                    {
+                        pendingActions.Enqueue(new KeyValuePair<string, Action<string>>(raw, action));
+                        actionQueued = true;
+                    }
+                }
+            }
+
+            return actionQueued;
         }
 
-        bool HasSensitiveValues(string raw)
+        bool ContainsSensitiveValues(string raw)
         {
             foreach (var sensitive in valuesShortestToLongest)
             {
@@ -77,6 +203,7 @@ namespace Octopus.Shared.Security.Masking
                     return true;
                 }
             }
+
             return false;
         }
 
@@ -95,6 +222,31 @@ namespace Octopus.Shared.Security.Masking
             {
                 return (x.Length.CompareTo(y.Length))*multiplier;
             }
+        }
+
+        class PotentialMultiLineMatch
+        {
+            public PotentialMultiLineMatch(string[] multilineValue, string initialMatch)
+            {
+                Line = 1;
+                MultilineValue = multilineValue;
+                Matches = new HashSet<string> {initialMatch};
+                FirstMatch = initialMatch;
+            } 
+
+            public int Line { get; private set;  }
+            public string[] MultilineValue { get; }
+            public string NextLine => MultilineValue[Line];
+            public string FirstMatch { get; }
+            public HashSet<string> Matches { get; }
+
+            public void AddMatch(string match)
+            {
+                Line++;
+                Matches.Add(match);
+            }
+
+
         }
     }
 }
