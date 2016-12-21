d69744bde28184bfc37c2ca1d14f607f80933a86
Michael Richardson <michael.james.richardson@gmail.com>
2016-12-21 14:32:35 +1000

    Aho-Corasick algorithm for sensitive-data-masking

--- Security/Masking/SensitiveDataMask.cs
+++ Security/Masking/SensitiveDataMask.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Octopus.Shared.Security.Masking
 {
-    // TODO: Rabin-Karp or something else might make this perform much better
-    // http://en.wikipedia.org/wiki/Rabin%E2%80%93Karp_algorithm
     public class SensitiveDataMask
     {
         public static readonly string Mask = "********";
-        readonly List<string> valuesLongestToShortest = new List<string>();
-        readonly List<string> valuesShortestToLongest = new List<string>();
-        readonly List<string[]> multiLineValues = new List<string[]>();
-        readonly List<PotentialMultiLineMatch> potentialMultiLineMatches = new List<PotentialMultiLineMatch>();
-        readonly Queue<KeyValuePair<string, Action<string>>> pendingActions = new Queue<KeyValuePair<string, Action<string>>>();   
+        static readonly object sync = new object();
         readonly StringBuilder builder = new StringBuilder();
-        readonly object sync = new object();
+        readonly AhoCorasick.Trie trie = new AhoCorasick.Trie();
+        readonly Queue<DeferredAction> deferred = new Queue<DeferredAction>();
 
         public SensitiveDataMask()
         {
@@ -28,34 +22,28 @@ namespace Octopus.Shared.Security.Masking
             MaskInstancesOf(instancesToMask);
         }
 
+        internal void MaskInstancesOf(string instanceToMask)
+        {
+            MaskInstancesOf(new[] {instanceToMask});
+        }
+
         public void MaskInstancesOf(IEnumerable<string> instancesToMask)
         {
             if (instancesToMask == null) return;
-            foreach (var instance in instancesToMask)
-                MaskInstancesOf(instance);
-        }
-
-        public void MaskInstancesOf(string sensitive)
-        {
-            if (string.IsNullOrWhiteSpace(sensitive) || sensitive.Length < 4)
-                return;
-
-            var lines = sensitive.Split(new[] {"\r\n", "\n"}, StringSplitOptions.RemoveEmptyEntries);
 
             lock (sync)
             {
-                if (lines.Length > 1)
+                foreach (var instance in instancesToMask)
                 {
-                    multiLineValues.Add(lines);
-                }
-                else
-                {
-                    valuesLongestToShortest.Add(sensitive);
-                    valuesShortestToLongest.Add(sensitive);
+                    if (string.IsNullOrWhiteSpace(instance) || instance.Length < 4)
+                        continue;
 
-                    valuesLongestToShortest.Sort(StringLengthComparer.LongestToShortest);
-                    valuesShortestToLongest.Sort(StringLengthComparer.ShortestToLongest);
+                    var normalized = instance.Replace("\r\n", "").Replace("\n", "");
+
+                    trie.Add(normalized);
                 }
+
+                trie.Build();
             }
         }
 
@@ -70,183 +58,187 @@ namespace Octopus.Shared.Security.Masking
         {
             lock (sync)
             {
-                if (!ApplyMultiLineValuesTo(raw, action))
+                var found = trie.Find(raw);
+
+                // If we are in a pending partial match then defer processing until we are sure it 
+                // is/isn't a match
+                if (found.IsPartial)
                 {
-                    ApplySingleLineValuesTo(raw, action);
+                    deferred.Enqueue(new DeferredAction(raw, action));
+                    return;
                 }
-            }
-        }
 
-        /// <summary>
-        /// Process any queued potential multi-line matches 
-        /// </summary>
-        public void Flush()
-        {
-            lock (sync)
-            {
-                while (pendingActions.Any())
+                // This result was not a partial match, so if there are any deferred actions, 
+                // add the current action to the end and process 
+                if (deferred.Any())
                 {
-                    var pending = pendingActions.Dequeue();
-                    ApplySingleLineValuesTo(pending.Key, pending.Value);
+                    deferred.Enqueue(new DeferredAction(raw, action));
+                    ProcessDeferred();
+                    return;
                 }
+
+                // If there are no deferred actions and we found no matches, invoke the action
+                // with the raw text 
+                if (!found.Found.Any())
+                {
+                    action(raw);
+                    return;
+                }
+
+                // Mask the matches and perform the action
+                MaskSectionsAndPerformAction(raw, action, GetMaskedSections(found.Found));
             }
         }
 
-        void ApplySingleLineValuesTo(string raw, Action<string> action)
-        {
-            if (string.IsNullOrEmpty(raw) || raw.Length < 4 || valuesLongestToShortest.Count == 0 
-                || !ContainsSensitiveValues(raw))
-            {
-                action(raw);
-                return;
-            }
-
-            // It's only worth allocating the new string if we actually found a match
-            action(PerformMask(raw, valuesLongestToShortest));
-        }
-
-        string PerformMask(string raw, IList<string> values)
+        void MaskSectionsAndPerformAction(string raw, Action<string> action, IEnumerable<Tuple<int, int>> maskedSections)
         {
             builder.Clear();
             builder.EnsureCapacity(raw.Length);
-            builder.Append(raw);
-            foreach (var sensitive in values)
+
+            var i = 0;
+
+            foreach (var maskedSection in maskedSections)
             {
-                builder.Replace(sensitive, Mask);
+               // If we are not at the start of the masked section, then progress to it 
+                while (i < maskedSection.Item1)
+                {
+                    builder.Append(raw[i++]);
+                }
+
+                // Apply the mask
+                builder.Append(Mask);
+                i = maskedSection.Item2 + 1;
             }
 
-            return builder.ToString();
+            // Write any remaining raw text
+            while (i < raw.Length)
+            {
+                builder.Append(raw[i++]);
+            }
+
+            action(builder.ToString());
         }
 
-        bool ApplyMultiLineValuesTo(string raw, Action<string> action)
+        /// <summary>
+        /// Processes all deferred actions.
+        /// Matches that span actions make this method tricky.
+        /// We concatenate the original text of all deferred actions and search for matches within it.
+        /// The possible scenarios then are:
+        /// 1) There were no matches within a deferred action.  Easy, just invoke the action with the original text.
+        /// 2) The action is completely spanned by a match.  In this case we want to discard the action. For example,
+        ///    a private-key in PEM format may be logged across many lines.  We don't want to write a mask for each line. 
+        /// 3) There are one or more matches that start or end within the action.  In this case we need to mask the appropriate 
+        /// // sections. 
+        /// </summary>
+        void ProcessDeferred()
         {
-            var actionQueued = false; 
+            // Concatenate all the deferred actions into one string and find matches in it
+            builder.Clear();
+            builder.EnsureCapacity(deferred.Sum(x => x.Text.Length));
 
-            // If there are existing partial matches 
-            if (potentialMultiLineMatches.Any())
+            foreach (var text in deferred.Select(x => x.Text))
             {
-                var rejectedPotentialMatches = new List<PotentialMultiLineMatch>();
-
-                foreach (var potentialMatch in potentialMultiLineMatches)
-                {
-                    // If the next line is still a potential match
-                    if (raw.Contains(potentialMatch.NextLine))
-                    {
-                        // Is it the final line 
-                        if (potentialMatch.Line == potentialMatch.MultilineValue.Length - 1)
-                        {
-                            // We have matched a multi-line value.  Rather than masking every-line, we will mask only the first line
-                            action(PerformMask(potentialMatch.FirstMatch, 
-                                valuesLongestToShortest.Concat(new[] {potentialMatch.MultilineValue.First()}).OrderByDescending(s => s.Length).ToList()));
-
-                            // At this point we will clear any other potential matches.
-                            // This certainly leaves some edge cases where multi-line values overlap, 
-                            // but they are not what this was designed to deal with.
-                            potentialMultiLineMatches.Clear();
-                            pendingActions.Clear();
-                            return true;
-                        }
-
-                        potentialMatch.AddMatch(raw);
-
-                        if (!actionQueued)
-                        {
-                            pendingActions.Enqueue(new KeyValuePair<string, Action<string>>(raw, action));
-                            actionQueued = true;
-                        }
-                    }
-                    else
-                    {
-                        // The multi-line match has missed, so we will remove it from the potential matches
-                        rejectedPotentialMatches.Add(potentialMatch);
-                    }
-                }
-
-                // Remove all rejected matches
-                foreach (var miss in rejectedPotentialMatches)
-                    potentialMultiLineMatches.Remove(miss);
-
-                // Process any actions
-                while (pendingActions.Any() && potentialMultiLineMatches.All(potentialMatch => !potentialMatch.Matches.Contains(pendingActions.Peek().Key)))
-                {
-                    var pendingAction = pendingActions.Dequeue();
-                    ApplySingleLineValuesTo(pendingAction.Key, pendingAction.Value);
-                }
+                builder.Append(text);
             }
 
-            // Check if the first line of any multi-line values match  
-            foreach (var multiLineValue in multiLineValues)
+            var matches = trie.Find(builder.ToString());
+            var maskedSections = new Queue<Tuple<int,int>>(GetMaskedSections(matches.Found));
+
+            // Process each deferred action, masking as appropriate
+            var currentEndIndex = -1;
+            var currentMaskedSection = !maskedSections.Any() ? null : maskedSections.Dequeue();
+
+            while (deferred.Any())
             {
-                if (raw.Contains(multiLineValue.First()))
+                var current = deferred.Dequeue();
+                var currentStartIndex = currentEndIndex + 1;
+                currentEndIndex += current.Text.Length;
+
+                // Scenario 1
+                // If there are no sections remaining to be masked, or the current masked section begins
+                // after this action, then simply invoke the action with the original text
+                if (currentMaskedSection == null || currentMaskedSection.Item1 > currentEndIndex)
                 {
-                    var potentialMatch = new PotentialMultiLineMatch(multiLineValue, raw);
-                    potentialMultiLineMatches.Add(potentialMatch);
-                    potentialMultiLineMatches.Sort((x, y) => y.MultilineValue.Length.CompareTo(x.MultilineValue.Length));
-
-                    if (!actionQueued)
-                    {
-                        pendingActions.Enqueue(new KeyValuePair<string, Action<string>>(raw, action));
-                        actionQueued = true;
-                    }
+                    current.Action(current.Text);
+                    continue;
                 }
-            }
 
-            return actionQueued;
-        }
+                // Scenario 2
+                // If the current masked section spans this entire action's text (i.e. this action was one line in a multi-line match)
+                // then skip this action entirely. We don't want to write the mask value for every line of a multi-line match 
+                if (currentMaskedSection.Item1 < currentStartIndex && currentMaskedSection.Item2 >= currentEndIndex)
+                    continue;
 
-        bool ContainsSensitiveValues(string raw)
-        {
-            foreach (var sensitive in valuesShortestToLongest)
-            {
-                if (raw.Contains(sensitive))
+                // Scenario 3
+                // This action has sections that need to be masked. We need to get the relative indexes for the masked sections 
+                var relativeIndexes = new List<Tuple<int, int>>();
+
+                while (currentMaskedSection != null && currentMaskedSection.Item1 <= currentEndIndex)
                 {
-                    return true;
+                    // The local start index will be either:
+                    // 0 if the masked section begins before this action 
+                    // OR the relative index of the masked section
+                    var localStartIndex = currentMaskedSection.Item1 <= currentStartIndex ? 0 : currentMaskedSection.Item1 - currentStartIndex;
+                    // Likewise the local end index will be either:
+                    // The end of this action's text if the masked section continues beyond this action
+                    // OR the relative end index
+                    var localEndIndex = currentMaskedSection.Item2 >= currentEndIndex ? current.Text.Length - 1 : currentMaskedSection.Item2 - currentStartIndex;
+                    relativeIndexes.Add(Tuple.Create(localStartIndex, localEndIndex));
+
+                    if (currentMaskedSection.Item2 > currentEndIndex)
+                        break;
+
+                    currentMaskedSection = maskedSections.Any() ? maskedSections.Dequeue() : null;
                 }
-            }
 
-            return false;
-        }
-
-        class StringLengthComparer : IComparer<string>
-        {
-            public static readonly StringLengthComparer LongestToShortest = new StringLengthComparer(-1);
-            public static readonly StringLengthComparer ShortestToLongest = new StringLengthComparer(1);
-            readonly int multiplier;
-
-            StringLengthComparer(int multiplier)
-            {
-                this.multiplier = multiplier;
-            }
-
-            public int Compare(string x, string y)
-            {
-                return (x.Length.CompareTo(y.Length))*multiplier;
+                // We have our relative indexes, so mask the text and invoke the action
+                MaskSectionsAndPerformAction(current.Text, current.Action, relativeIndexes);
             }
         }
 
-        class PotentialMultiLineMatch
+
+        // Gets the start and end indexes of the masked sections 
+        // If the matches overlap, this will combine them  
+        static IEnumerable<Tuple<int, int>> GetMaskedSections(IEnumerable<KeyValuePair<int, string>> found)
         {
-            public PotentialMultiLineMatch(string[] multilineValue, string initialMatch)
+            var sections = new List<Tuple<int, int>>();
+
+            foreach (var match in found.OrderBy(x => x.Key))
             {
-                Line = 1;
-                MultilineValue = multilineValue;
-                Matches = new HashSet<string> {initialMatch};
-                FirstMatch = initialMatch;
+                var matchStartIndex = match.Key - (match.Value.Length - 1);
+
+                if (sections.Count > 0 && matchStartIndex < sections.Last().Item2)
+                {
+                    sections[sections.Count - 1] = Tuple.Create(sections.Last().Item1, match.Key);
+                    continue;
+                }
+
+                sections.Add(Tuple.Create(matchStartIndex, match.Key));
+            }
+
+            return sections;
+        }
+
+        /// <summary>
+        /// Process any deferred potential multi-line matches 
+        /// </summary>
+        public void Flush()
+        {
+            ProcessDeferred(); 
+        }
+
+        class DeferredAction
+        {
+            public DeferredAction(string text, Action<string> action)
+            {
+                Text = text;
+                Action = action;
             } 
 
-            public int Line { get; private set;  }
-            public string[] MultilineValue { get; }
-            public string NextLine => MultilineValue[Line];
-            public string FirstMatch { get; }
-            public HashSet<string> Matches { get; }
-
-            public void AddMatch(string match)
-            {
-                Line++;
-                Matches.Add(match);
-            }
-
-
+            public string Text { get; }
+            public Action<string> Action { get; }
         }
+
+
     }
 }
