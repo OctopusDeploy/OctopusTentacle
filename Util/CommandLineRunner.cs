33d1a1c06b58d7ca9c7c3dbc9c0d559e7052a737
Matt Richardson <matt.richardson@octopus.com>
2017-04-26 16:00:08 +1000

    Add support for HSTS (Strict-Transport-Security) header (#1038)

--- Util/CommandLineRunner.cs
+++ Util/CommandLineRunner.cs
@@ -10,37 +10,44 @@ namespace Octopus.Shared.Util
         {
             foreach (var invocation in commandLineInvocations)
             {
-                try
-                {
-                    var exitCode = SilentProcessRunner.ExecuteCommand(invocation.Executable, (invocation.Arguments ?? "") + " " + (invocation.SystemArguments ?? ""),
-                        Environment.CurrentDirectory,
-                        log.Info,
-                        log.Error);
-
-                    if (exitCode != 0)
-                    {
-                        if (invocation.IgnoreFailedExitCode)
-                        {
-                            log.Info("The previous command returned a non-zero exit code of: " + exitCode);
-                            log.Info("The command that failed was: " + invocation);
-                            log.Info("The invocation is set to ignore failed exit codes, continuing...");
-                        }
-                        else
-                        {
-                            log.Error("The previous command returned a non-zero exit code of: " + exitCode);
-                            log.Error("The command that failed was: " + invocation);
-                            return false;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    log.Error(ex);
-                    log.Error("The command that caused the exception was: " + invocation);
+                if (!Execute(invocation, log))
                     return false;
-                }
             }
 
+            return true;
+        }
+
+        public bool Execute(CommandLineInvocation invocation, ILog log)
+        {
+            try
+            {
+                var exitCode = SilentProcessRunner.ExecuteCommand(invocation.Executable, (invocation.Arguments ?? "") + " " + (invocation.SystemArguments ?? ""),
+                    Environment.CurrentDirectory,
+                    log.Info,
+                    log.Error);
+
+                if (exitCode != 0)
+                {
+                    if (invocation.IgnoreFailedExitCode)
+                    {
+                        log.Info("The previous command returned a non-zero exit code of: " + exitCode);
+                        log.Info("The command that failed was: " + invocation);
+                        log.Info("The invocation is set to ignore failed exit codes, continuing...");
+                    }
+                    else
+                    {
+                        log.Error("The previous command returned a non-zero exit code of: " + exitCode);
+                        log.Error("The command that failed was: " + invocation);
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+                log.Error("The command that caused the exception was: " + invocation);
+                return false;
+            }
             return true;
         }
     }
