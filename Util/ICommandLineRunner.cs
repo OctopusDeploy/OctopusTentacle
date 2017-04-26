f2ece661a098c8056b3ad6d291db281aff16f359
Matt Richardson <matt@redgumtech.com.au>
2017-04-26 18:46:03 +1000

    Revert "Add support for HSTS (Strict-Transport-Security) header (#1038)"
    This reverts commit 30fcd438802efd3560385058d9f7a33e5bb1a0f0.

--- Util/ICommandLineRunner.cs
+++ Util/ICommandLineRunner.cs
@@ -7,6 +7,5 @@ namespace Octopus.Shared.Util
     public interface ICommandLineRunner
     {
         bool Execute(IEnumerable<CommandLineInvocation> commandLineInvocations, ILog log);
-        bool Execute(CommandLineInvocation commandLineInvocation, ILog log);
     }
 }
