d238b734a22621bc94b1f697e82c5e9c4397524d
Michael Noonan <michaelnoonan@gmail.com>
2017-06-06 10:01:45 +1000

    Make it clear what this code is for.
    It's a bit special.

--- Startup/AbstractStandardCommand.cs
+++ Startup/AbstractStandardCommand.cs
@@ -7,12 +7,13 @@ namespace Octopus.Shared.Startup
         protected AbstractStandardCommand(IApplicationInstanceSelector instanceSelector)
         {
             // The instance is actually parsed from the command-line as early as possible when the program starts to make sure logs end up in the most appropriate folder for the instance
+            // See OctopusProgram.TryLoadInstanceName()
             // Adding the common "instance=" option here so every derived command add this to their help message
             Options.Add("instance=", "Name of the instance to use", v => { });
 
             // These kinds of commands depend on being able to load the correct instance
             // Ensure this is the case by loading the current instance
-            var currentInstance = instanceSelector.Current;
+            var unused = instanceSelector.Current;
         }
     }
 }
