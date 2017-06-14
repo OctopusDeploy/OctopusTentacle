4ecc1bf26d2920191287fcc5219267ecbc6196d6
Matt Richardson <matt.richardson@octopus.com>
2017-06-14 11:58:19 +1000

    Allow new certs to be exported as pfx files (#9)
    Fixes OctopusDeploy/Issues#3564

--- source/Octopus.Tentacle/Commands/NewCertificateCommand.cs
+++ source/Octopus.Tentacle/Commands/NewCertificateCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Octopus.Diagnostics;
 using Octopus.Shared;
@@ -17,7 +18,10 @@ namespace Octopus.Tentacle.Commands
         readonly ILog log;
         readonly Lazy<ICertificateGenerator> generator;
         bool preserve;
+        [ObsoleteEx(Message="--export-file is deprecated, as it is insecure", RemoveInVersion = "4.0", ReplacementTypeOrMember="exportPfx", TreatAsErrorFromVersion="4.0")]
         string exportFile;
+        string exportPfx;
+        string password;
 
         public NewCertificateCommand(
             Lazy<ITentacleConfiguration> configuration,
@@ -30,7 +34,9 @@ namespace Octopus.Tentacle.Commands
             this.generator = generator;
 
             Options.Add("b|if-blank", "Generates a new certificate only if there is none", v => preserve = true);
-            Options.Add("e|export-file=", "Exports a new certificate to the specified file as unprotected base64 text, but does not save it to the Tentacle configuration; for use with the import-certificate command", v => exportFile = v);
+            Options.Add("e|export-file=", "DEPRECATED: Exports a new certificate to the specified file as unprotected base64 text, but does not save it to the Tentacle configuration; for use with the import-certificate command", v => exportFile = v);
+            Options.Add("export-pfx=", "Exports the new certificate to the specified file as a password protected pfx, but does not save it to the Tentacle configuration; for use with the import-certificate command", v => exportPfx = v);
+            Options.Add("pfx-password=", "The password to use for the exported pfx file", v => password = v);
         }
 
         protected override void Start()
@@ -38,6 +44,10 @@ namespace Octopus.Tentacle.Commands
 
             if (preserve && !string.IsNullOrWhiteSpace(exportFile))
                 throw new ControlledFailureException("Invalid command: --if-blank and --export-file cannot be specified together");
+            if (preserve && !string.IsNullOrWhiteSpace(exportPfx))
+                throw new ControlledFailureException("Invalid command: --if-blank and --export-pfx cannot be specified together");
+            if (!string.IsNullOrWhiteSpace(exportFile)  && !string.IsNullOrWhiteSpace(exportPfx))
+                throw new ControlledFailureException("Invalid command: --export-file and --export-pfx cannot be specified together");
 
             if (!string.IsNullOrWhiteSpace(exportFile))
             {
@@ -45,6 +55,14 @@ namespace Octopus.Tentacle.Commands
                 var base64 = Convert.ToBase64String(CertificateEncoder.Export(cert));
                 File.WriteAllText(exportFile, base64, Encoding.UTF8);
                 log.Info($"A new certificate has been generated and written to {exportFile}. Thumbprint:");
+                log.Info(cert.Thumbprint);
+            }
+            else if (!string.IsNullOrWhiteSpace(exportPfx))
+            {
+                var cert = generator.Value.GenerateNew(CertificateExpectations.TentacleCertificateFullName);
+                var pfxBytes = cert.Export(X509ContentType.Pkcs12, password);
+                File.WriteAllBytes(exportPfx, pfxBytes);
+                log.Info($"A new certificate has been generated and written to {exportPfx}. Thumbprint:");
                 log.Info(cert.Thumbprint);
             }
             else
@@ -55,7 +73,7 @@ namespace Octopus.Tentacle.Commands
                     return;
                 }
 
-                log.Verbose("Generating and installing a new cetificate...");
+                log.Verbose("Generating and installing a new certificate...");
                 var certificate = configuration.Value.GenerateNewCertificate();
                 log.Info("A new certificate has been generated and installed. Thumbprint:");
                 log.Info(certificate.Thumbprint);
