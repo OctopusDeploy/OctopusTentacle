147f1c439ca966038b1fd13bd3e37e7e90da9de1
Michael Noonan <michaelnoonan@gmail.com>
2017-06-26 14:37:29 +1000

    Clean up these commands
    Also reduce dependence on `Octopus.Shared.Guard` it was used very little but does this logic really deserve to be shared?

--- Startup/AbstractCommand.cs
+++ Startup/AbstractCommand.cs
@@ -60,5 +60,53 @@ namespace Octopus.Shared.Startup
         {
             Stop();
         }
+
+        protected void AssertNotNullOrWhitespace(string value, string name, string errorMessage = null)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ControlledFailureException(errorMessage ?? $"--{name} must be supplied");
+        }
+
+        protected void AssertFileExists(string path, string errorMessage = null)
+        {
+            if (!File.Exists(path))
+                throw new ControlledFailureException(errorMessage ?? $"File '{path}' cannot be found");
+        }
+
+        protected void AssertFileDoesNotExist(string path, string errorMessage = null)
+        {
+            if (File.Exists(path))
+            {
+                throw new ControlledFailureException(errorMessage ?? $"A file already exists at '{path}'");
+            }
+        }
+
+        protected void AssertDirectoryExists(string path, string errorMessage = null)
+        {
+            if (!Directory.Exists(path))
+                throw new ControlledFailureException(errorMessage ?? $"Directory '{path}' cannot be found");
+        }
+
+        static readonly Lazy<List<string>> SpecialLocations = new Lazy<List<string>>(() =>
+        {
+            var result = new List<string>();
+            foreach (var specialLocation in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                var location = Environment.GetFolderPath((Environment.SpecialFolder)specialLocation, Environment.SpecialFolderOption.None);
+                result.Add(location);
+            }
+            result.Add("C:");
+            result.Add("C:\\");
+            return result;
+        });
+
+        protected void AssertNotSpecialLocation(string path)
+        {
+            foreach (var specialLocation in SpecialLocations.Value)
+            {
+                if (string.Equals(path, specialLocation, StringComparison.OrdinalIgnoreCase))
+                    throw new ControlledFailureException($"Directory '{path}' is not a good place, pick a safe subdirectory");
+            }
+        }
     }
 }
