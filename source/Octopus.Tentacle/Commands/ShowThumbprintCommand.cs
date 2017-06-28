65c4d6e6c2469c4a1a1e8c6a296ddbae115b9660
Michael Noonan <michaelnoonan@gmail.com>
2017-06-28 18:39:16 +1000

    Bring across the new flavour of these commands
    They have better validation and format options.

--- source/Octopus.Tentacle/Commands/ShowThumbprintCommand.cs
+++ source/Octopus.Tentacle/Commands/ShowThumbprintCommand.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 using Octopus.Diagnostics;
+using Octopus.Shared;
 using Octopus.Shared.Configuration;
 using Octopus.Shared.Startup;
 using Octopus.Tentacle.Configuration;
@@ -10,10 +13,17 @@ namespace Octopus.Tentacle.Commands
 {
     public class ShowThumbprintCommand : AbstractStandardCommand
     {
+        static readonly string TextFormat = "text";
+        static readonly string JsonFormat = "json";
+        static readonly string[] SupportedFormats = { TextFormat, JsonFormat };
+
         readonly Lazy<ITentacleConfiguration> tentacleConfiguration;
         readonly ILog log;
         string exportFile;
-        bool thumbprintOnly;
+
+        public string Format { get; set; } = TextFormat;
+
+        public override bool SuppressConsoleLogging => true;
 
         public ShowThumbprintCommand(Lazy<ITentacleConfiguration> tentacleConfiguration, ILog log, IApplicationInstanceSelector selector) : base(selector)
         {
@@ -21,17 +31,37 @@ namespace Octopus.Tentacle.Commands
             this.log = log;
 
             Options.Add("e|export-file=", "Exports the Tentacle thumbprint to a file", v => exportFile = v);
-            Options.Add("thumbprint-only", "Only print out the thumbprint, with no additional text", s => thumbprintOnly = true);
+            ThumbprintOnlyOption();
+            Options.Add("format=", $"The format of the output ({string.Join(",", SupportedFormats)}). Defaults to {Format}.", v => Format = v);
+        }
+
+        [ObsoleteEx(Message = "The --thumbprint-only switch has been deprecated since it is no longer needed and will be removed in 4.0", RemoveInVersion = "4.0", TreatAsErrorFromVersion = "4.0")]
+        void ThumbprintOnlyOption()
+        {
+            Options.Add("thumbprint-only", "Only print out the thumbprint, with no additional text. This switch will be removed in a future version since it is no longer needed.", s => { });
         }
 
         protected override void Start()
         {
+            base.Start();
+
+            if (!SupportedFormats.Contains(Format, StringComparer.OrdinalIgnoreCase))
+                throw new ControlledFailureException($"The format '{Format}' is not supported. Try {string.Join(" or ", SupportedFormats)}.");
+
             var thumbprint = tentacleConfiguration.Value.TentacleCertificate.Thumbprint;
-            log.Info((thumbprintOnly ? "" : "The thumbprint of this Tentacle is: ") + thumbprint);
+
+            var content = string.Equals(Format, JsonFormat, StringComparison.OrdinalIgnoreCase)
+                ? JsonConvert.SerializeObject(new { Thumbprint = thumbprint })
+                : thumbprint;
+
             if (!string.IsNullOrWhiteSpace(exportFile))
             {
-                File.WriteAllText(exportFile, thumbprint, Encoding.ASCII);
+                File.WriteAllText(exportFile, content, Encoding.ASCII);
                 log.Info($"The thumbprint has been written to {exportFile}.");
+            }
+            else
+            {
+                Console.Write(content);
             }
         }
     }
