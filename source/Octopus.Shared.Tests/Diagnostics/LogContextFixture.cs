2ad10b3e11ec8dda8d388fe03fc6ae3ae8a12bc9
Cameron MacFarland <distantcam@gmail.com>
2017-09-13 10:33:33 +0800

    Separate SensitiveDataMask for Parent/Child LogContexts

---
+++ source/Octopus.Shared.Tests/Diagnostics/LogContextFixture.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using Octopus.Shared.Diagnostics;
+
+namespace Octopus.Shared.Tests.Diagnostics
+{
+    [TestFixture]
+    public class LogContextFixture
+    {
+        [Test]
+        public void ASensitiveValueIsMasked()
+        {
+            const string sensitive = "sensitive",
+                raw = "This contains a sensitive value",
+                expected = "This contains a ******** value";
+
+            var logContext = new LogContext(sensitiveValues: new[] { sensitive });
+            string result = null;
+            logContext.SafeSanitize(raw, sanitized => result = sanitized);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ChildContextUsesParentsSensitiveMask()
+        {
+            const string sensitive = "sensitive",
+                raw = "This contains a sensitive value",
+                expected = "This contains a ******** value";
+
+            var logContext = new LogContext(sensitiveValues: new[] { sensitive });
+            var childContext = logContext.CreateChild();
+            string result = null;
+            childContext.SafeSanitize(raw, sanitized => result = sanitized);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ChildContextCanHaveOwnSensitiveValues()
+        {
+            const string sensitive = "sensitive",
+                childSensitive = "child",
+                raw = "This contains a sensitive child value",
+                expected = "This contains a ******** child value",
+                expectedChild = "This contains a ******** ******** value";
+
+            var logContext = new LogContext(sensitiveValues: new[] { sensitive });
+            var childContext = logContext.CreateChild().WithSensitiveValues(new[] { childSensitive });
+            string result = null;
+
+            logContext.SafeSanitize(raw, sanitized => result = sanitized);
+            Assert.AreEqual(expected, result);
+
+            childContext.SafeSanitize(raw, sanitized => result = sanitized);
+            Assert.AreEqual(expectedChild, result);
+        }
+    }
+}
--- source/Octopus.Shared/Diagnostics/LogContext.cs
+++ source/Octopus.Shared/Diagnostics/LogContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Newtonsoft.Json;
 using Octopus.Shared.Model;
 using Octopus.Shared.Security.Masking;
@@ -10,31 +9,28 @@ namespace Octopus.Shared.Diagnostics
     {
         readonly string[] sensitiveValues;
         readonly string correlationId;
+        readonly LogContext parent;
         readonly object sensitiveDataMaskLock = new object();
         SensitiveDataMask sensitiveDataMask;
 
         [JsonConstructor]
-        public LogContext(string correlationId = null, string[] sensitiveValues = null)
+        public LogContext(string correlationId = null, string[] sensitiveValues = null, LogContext parent = null)
         {
             this.correlationId = correlationId ?? GenerateId();
             this.sensitiveValues = sensitiveValues ?? new string[0];
+            this.parent = parent ?? this;
         }
 
-        public string CorrelationId
-        {
-            get { return correlationId; }
-        }
+        public string CorrelationId => correlationId;
 
         [Encrypted]
-        public string[] SensitiveValues
-        {
-            get { return sensitiveValues; }
-        }
+        public string[] SensitiveValues => sensitiveValues;
 
         public void SafeSanitize(string raw, Action<string> action)
         {
             try
             {
+                // JIT creation of sensitiveDataMask
                 if (sensitiveDataMask == null && sensitiveValues.Length > 0)
                     lock (sensitiveDataMaskLock)
                     {
@@ -44,10 +40,20 @@ namespace Octopus.Shared.Diagnostics
                             sensitiveDataMask.MaskInstancesOf(sensitiveValues);
                         }
                     }
+
+                // Chain action with parents SafeSanitize
+                Action<string> actionWithParent = s =>
+                {
+                    if (parent == this)
+                        action(s);
+                    else
+                        parent.SafeSanitize(s, action);
+                };
+
                 if (sensitiveDataMask != null)
-                    sensitiveDataMask.ApplyTo(raw, action);
+                    sensitiveDataMask.ApplyTo(raw, actionWithParent);
                 else
-                    action(raw);
+                    actionWithParent(raw);
             }
             catch
             {
@@ -55,40 +61,18 @@ namespace Octopus.Shared.Diagnostics
             }
         }
 
-        public LogContext CreateSibling()
-        {
-            return Parent().CreateChild();
-        }
+        public LogContext CreateSibling() => Parent().CreateChild();
 
-        public LogContext Parent()
-        {
-            var lastSlash = correlationId.LastIndexOf('/');
-            if (lastSlash < 0)
-                return new LogContext();
+        public LogContext Parent() => parent;
 
-            return new LogContext(correlationId.Substring(0, lastSlash), sensitiveValues);
-        }
-
-        public LogContext CreateChild()
-        {
-            return new LogContext((correlationId + '/' + GenerateId()), sensitiveValues);
-        }
+        public LogContext CreateChild() => new LogContext((correlationId + '/' + GenerateId()), parent: this);
 
         public LogContext WithSensitiveValues(string[] sensitiveValues)
-        {
-            return new LogContext(correlationId, this.sensitiveValues.Union(sensitiveValues).ToArray());
-        }
+            => new LogContext(correlationId, sensitiveValues, parent: parent);
 
-        static string GenerateId()
-        {
-            return Guid.NewGuid().ToString("N");
-        }
+        static string GenerateId() => Guid.NewGuid().ToString("N");
 
-        public static LogContext Null()
-        {
-            var correlationId = GenerateId();
-            return new LogContext(correlationId);
-        }
+        public static LogContext Null() => new LogContext(GenerateId());
 
         public static LogContext CreateNew(string correlationId)
         {
