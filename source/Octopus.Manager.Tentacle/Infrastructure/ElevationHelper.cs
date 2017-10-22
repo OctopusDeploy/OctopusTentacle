acb452b5d45cfaa4f5f08d74234a83fc9bc07645
Michael Noonan <michaelnoonan@gmail.com>
2017-10-23 08:27:38 +1000

    Detect and report elevation failure, but friendly
    Fixes https://github.com/OctopusDeploy/Issues/issues/2639

--- source/Octopus.Manager.Tentacle/Infrastructure/ElevationHelper.cs
+++ source/Octopus.Manager.Tentacle/Infrastructure/ElevationHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
@@ -12,13 +13,21 @@ namespace Octopus.Manager.Tentacle.Infrastructure
 
         public static void Elevate(IEnumerable<string> args)
         {
-            var info = new ProcessStartInfo(Assembly.GetEntryAssembly().Location, string.Join(" ", args))
+            try
             {
-                Verb = "runas"
-            };
+                // We removed this class in favour of using the application manifest for elevation... but then!
+                // See https://github.com/OctopusDeploy/Issues/issues/3875
+                var info = new ProcessStartInfo(Assembly.GetEntryAssembly().Location, String.Join(" ", args));
+                info.Verb = "runas";
 
-            var process = new Process {StartInfo = info};
-            process.Start();
+                var process = new Process {StartInfo = info};
+
+                process.Start();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Failed to restart as an elevated process. The Octopus Tentacle Manager requires elevated privileges on this server. Please check your user account is a member of the Administrator group on this server, and answer 'YES' to any Windows UAC prompts.", e);
+            }
         }
     }
 }
