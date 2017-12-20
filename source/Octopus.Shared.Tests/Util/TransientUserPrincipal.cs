0f9543ca7becddcac4b11cc059f09e577b855251
Michael Noonan <michaelnoonan@gmail.com>
2017-12-20 14:53:09 +1000

    Delete user profile for TransientUserPrincipal
    We've been leaving these suckers lying around all over the place! Whoops!

--- source/Octopus.Shared.Tests/Util/TransientUserPrincipal.cs
+++ source/Octopus.Shared.Tests/Util/TransientUserPrincipal.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.ComponentModel;
 using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Security.Principal;
 
 namespace Octopus.Shared.Tests.Util
@@ -22,10 +24,13 @@ namespace Octopus.Shared.Tests.Util
                 try
                 {
                     attempts++;
+                    var username = name ?? new string(Guid.NewGuid().ToString("N").ToLowerInvariant().Where(char.IsLetter).ToArray());
+                    Console.WriteLine($"Trying to create a temporary Windows User Account on the local machine called '{principal.Name}'...");
+
                     principalContext = new PrincipalContext(contextType);
                     {
                         principal = new UserPrincipal(principalContext);
-                        principal.Name = name ?? new string(Guid.NewGuid().ToString("N").ToLowerInvariant().Where(char.IsLetter).ToArray());
+                        principal.Name = username;
                         principal.SetPassword(password);
                         principal.Save();
                         this.password = password;
@@ -34,12 +39,13 @@ namespace Octopus.Shared.Tests.Util
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Failed to create the Windows User Account called '{principal.Name}': {ex.Message}");
+                    Console.WriteLine($"Failed to create the temporary Windows User Account called '{principal.Name}': {ex.Message}");
                     if (attempts >= 5) throw;
                 }
             }
         }
 
+        public SecurityIdentifier Sid => principal.Sid;
         public string NTAccountName => principal.Sid.Translate(typeof(NTAccount)).ToString();
         public string DomainName => NTAccountName.Split(new[] {'\\'}, 2)[0];
         public string UserName => NTAccountName.Split(new[] {'\\'}, 2)[1];
@@ -49,9 +55,43 @@ namespace Octopus.Shared.Tests.Util
 
         public void Dispose()
         {
+            TryDeleteProfile();
             principal.Delete();
             principal.Dispose();
             principalContext.Dispose();
+        }
+
+        public override string ToString()
+        {
+            return NTAccountName;
+        }
+
+        [DllImport("userenv.dll", SetLastError = true)]
+        public static extern bool DeleteProfile(string sidString, string profilePath, string computerName);
+
+        void TryDeleteProfile()
+        {
+            try
+            {
+                Console.WriteLine($"Deleting profile for {NTAccountName}...");
+                Invoke(() => DeleteProfile(Sid.Value, null, null), $"Failed to delete the profile for '{NTAccountName}'");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static bool Invoke(Func<bool> nativeMethod, string failureDescription)
+        {
+            try
+            {
+                return nativeMethod() ? true : throw new Win32Exception();
+            }
+            catch (Win32Exception ex)
+            {
+                throw new Exception($"{failureDescription}: {ex.Message}", ex);
+            }
         }
     }
 }
