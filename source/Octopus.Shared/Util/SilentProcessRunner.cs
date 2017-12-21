8ffd68b69e463aa478229288c75956c7b6fe48b3
Michael Noonan <michaelnoonan@gmail.com>
2017-12-21 11:31:53 +1000

    Bring WindowStationAndDesktopAccess internal
    Also handle error cases with p/invoke

--- source/Octopus.Shared/Util/SilentProcessRunner.cs
+++ source/Octopus.Shared/Util/SilentProcessRunner.cs
@@ -9,6 +9,7 @@ using System.Management;
 #endif
 using System.Net;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using System.Security.Principal;
 using System.Text;
 using System.Threading;
@@ -375,6 +376,120 @@ namespace Octopus.Shared.Util
             }
         }
 
+        // Required to allow a service to run a process as another user
+        // See http://stackoverflow.com/questions/677874/starting-a-process-with-credentials-from-a-windows-service/30687230#30687230
+        internal class WindowStationAndDesktopAccess
+        {
+            public static void GrantAccessToWindowStationAndDesktop(string username, string domainName = null)
+            {
+                var hWindowStation = Win32Helper.Invoke(() => GetProcessWindowStation(), "Failed to get a handle to the current window station for this process");
+                const int windowStationAllAccess = 0x000f037f;
+                GrantAccess(username, domainName, hWindowStation, windowStationAllAccess);
+                var hDesktop = Win32Helper.Invoke(() => GetThreadDesktop(GetCurrentThreadId()), "Failed to the a handle to the desktop for the current thread");
+                const int desktopRightsAllAccess = 0x000f01ff;
+                GrantAccess(username, domainName, hDesktop, desktopRightsAllAccess);
+            }
+
+            static void GrantAccess(string username, string domainName, IntPtr handle, int accessMask)
+            {
+                SafeHandle safeHandle = new NoopSafeHandle(handle);
+                var security =
+                    new GenericSecurity(
+                        false, ResourceType.WindowObject, safeHandle, AccessControlSections.Access);
+
+                var account = string.IsNullOrEmpty(domainName)
+                    ? new NTAccount(username)
+                    : new NTAccount(domainName, username);
+
+                security.AddAccessRule(
+                    new GenericAccessRule(
+                        account, accessMask, AccessControlType.Allow));
+                security.Persist(safeHandle, AccessControlSections.Access);
+            }
+
+            [DllImport("user32.dll", SetLastError = true)]
+            private static extern IntPtr GetProcessWindowStation();
+
+            [DllImport("user32.dll", SetLastError = true)]
+            private static extern IntPtr GetThreadDesktop(int dwThreadId);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            private static extern int GetCurrentThreadId();
+
+            // All the code to manipulate a security object is available in .NET framework,
+            // but its API tries to be type-safe and handle-safe, enforcing a special implementation
+            // (to an otherwise generic WinAPI) for each handle type. This is to make sure
+            // only a correct set of permissions can be set for corresponding object types and
+            // mainly that handles do not leak.
+            // Hence the AccessRule and the NativeObjectSecurity classes are abstract.
+            // This is the simplest possible implementation that yet allows us to make use
+            // of the existing .NET implementation, sparing necessity to
+            // P/Invoke the underlying WinAPI.
+            private class GenericAccessRule : AccessRule
+            {
+                public GenericAccessRule(
+                    IdentityReference identity, int accessMask, AccessControlType type) :
+                    base(identity, accessMask, false, InheritanceFlags.None,
+                        PropagationFlags.None, type)
+                {
+                }
+            }
+
+            private class GenericSecurity : NativeObjectSecurity
+            {
+                public GenericSecurity(
+                    bool isContainer, ResourceType resType, SafeHandle objectHandle,
+                    AccessControlSections sectionsRequested)
+                    : base(isContainer, resType, objectHandle, sectionsRequested)
+                {
+                }
+
+                public new void Persist(SafeHandle handle, AccessControlSections includeSections)
+                {
+                    base.Persist(handle, includeSections);
+                }
+
+                public new void AddAccessRule(AccessRule rule)
+                {
+                    base.AddAccessRule(rule);
+                }
+
+                public override Type AccessRightType => throw new NotImplementedException();
+
+                public override AccessRule AccessRuleFactory(
+                    IdentityReference identityReference,
+                    int accessMask, bool isInherited, InheritanceFlags inheritanceFlags,
+                    PropagationFlags propagationFlags, AccessControlType type)
+                    => throw new NotImplementedException();
+
+                public override Type AccessRuleType => typeof(AccessRule);
+
+                public override AuditRule AuditRuleFactory(
+                    IdentityReference identityReference, int accessMask,
+                    bool isInherited, InheritanceFlags inheritanceFlags,
+                    PropagationFlags propagationFlags, AuditFlags flags)
+                    => throw new NotImplementedException();
+
+                public override Type AuditRuleType => typeof(AuditRule);
+            }
+
+            // Handles returned by GetProcessWindowStation and GetThreadDesktop should not be closed
+            private class NoopSafeHandle : SafeHandle
+            {
+                public NoopSafeHandle(IntPtr handle) :
+                    base(handle, false)
+                {
+                }
+
+                public override bool IsInvalid => false;
+
+                protected override bool ReleaseHandle()
+                {
+                    return true;
+                }
+            }
+        }
+
         internal class AccessToken : IDisposable
         {
             public string Username { get; }
@@ -502,6 +617,19 @@ namespace Octopus.Shared.Util
                 try
                 {
                     return nativeMethod() ? true : throw new Win32Exception();
+                }
+                catch (Win32Exception ex)
+                {
+                    throw new Exception($"{failureDescription}: {ex.Message}", ex);
+                }
+            }
+            
+            public static IntPtr Invoke(Func<IntPtr> nativeMethod, string failureDescription)
+            {
+                try
+                {
+                    var result = nativeMethod();
+                    return result != IntPtr.Zero ? result : throw new Win32Exception();
                 }
                 catch (Win32Exception ex)
                 {
