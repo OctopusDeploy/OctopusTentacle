2e1f9692da8c3d3d4ad0e3a53ff36ed91016f8d3
Robert Wagner <robert@wagner.id.au>
2017-12-22 13:18:08 +1000

    Converted test project to new project system

--- source/Octopus.Tentacle.Tests/Commands/OptionSets/ApiEndpointOptionsFixture.cs
+++ source/Octopus.Tentacle.Tests/Commands/OptionSets/ApiEndpointOptionsFixture.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
 using Octopus.Shared;
 using Octopus.Shared.Internals.Options;
@@ -14,23 +15,23 @@ namespace Octopus.Tentacle.Tests.Commands.OptionSets
     public class ApiEndpointOptionsFixture
     {
         [Test]
-        [TestCase(null    , null  , null      , null)]
-        [TestCase("server", null  , null      , null    , ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key. You can get an API key from the Octopus web portal. E.g., --apiKey=ABC1234")]
-        [TestCase("server", "user", null      , null    , ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a password for the specified user account")]
-        [TestCase("server", "user", "password", null)]
-        [TestCase("server", null  , "password", null    , ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username for the specified password")]
-        [TestCase("server", "user", "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase("server", "user", null      , "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase("server", null  , "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase("server", null  , null      , "apikey")]
-        [TestCase(null    , "user", null      , null    , ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a password for the specified user account")]
-        [TestCase(null    , "user", "password", null    , ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
-        [TestCase(null    , null  , "password", null    , ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username for the specified password")]
-        [TestCase(null    , "user", "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase(null    , "user", null      , "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase(null    , null  , "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase(null    , null  , null      , "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
-        public void ValidationIsCorrectWhenOptional(string server, string username, string password, string apiKey)
+        [TestCase(null    , null  , null      , null, null)]
+        [TestCase("server", null  , null      , null    , "Please specify a username and password, or an Octopus API key. You can get an API key from the Octopus web portal. E.g., --apiKey=ABC1234")]
+        [TestCase("server", "user", null      , null    , "Please specify a password for the specified user account")]
+        [TestCase("server", "user", "password", null, null)]
+        [TestCase("server", null  , "password", null    , "Please specify a username for the specified password")]
+        [TestCase("server", "user", "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase("server", "user", null      , "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase("server", null  , "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase("server", null  , null      , "apikey", null)]
+        [TestCase(null    , "user", null      , null    , "Please specify a password for the specified user account")]
+        [TestCase(null    , "user", "password", null    , "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
+        [TestCase(null    , null  , "password", null    , "Please specify a username for the specified password")]
+        [TestCase(null    , "user", "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase(null    , "user", null      , "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase(null    , null  , "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase(null    , null  , null      , "apikey", "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
+        public void ValidationIsCorrectWhenOptional(string server, string username, string password, string apiKey, string expectedExceptionMessage)
         {
             var optionSet = new OptionSet();
             var api = new ApiEndpointOptions(optionSet) { Optional = true };
@@ -56,27 +57,36 @@ namespace Octopus.Tentacle.Tests.Commands.OptionSets
                 args.Add(apiKey);
             }
             optionSet.Parse(args);
-            api.Validate();
+
+            if (expectedExceptionMessage == null)
+            {
+                api.Validate();
+            }
+            else
+            {
+                Action action = api.Validate;
+                action.ShouldThrow<ControlledFailureException>().WithMessage(expectedExceptionMessage);
+            }
         }
 
         [Test]
-        [TestCase(null, null, null, null, ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
-        [TestCase("server", null, null, null, ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key. You can get an API key from the Octopus web portal. E.g., --apiKey=ABC1234")]
-        [TestCase("server", "user", null, null, ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a password for the specified user account")]
-        [TestCase("server", "user", "password", null)]
-        [TestCase("server", null, "password", null, ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username for the specified password")]
-        [TestCase("server", "user", "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase("server", "user", null, "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase("server", null, "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase("server", null, null, "apikey")]
-        [TestCase(null, "user", null, null, ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a password for the specified user account")]
-        [TestCase(null, "user", "password", null, ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
-        [TestCase(null, null, "password", null, ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username for the specified password")]
-        [TestCase(null, "user", "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase(null, "user", null, "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase(null, null, "password", "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify a username and password, or an Octopus API key - not both.")]
-        [TestCase(null, null, null, "apikey", ExpectedException = typeof(ControlledFailureException), ExpectedMessage = "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
-        public void ValidationIsCorrectWhenMandatory(string server, string username, string password, string apiKey)
+        [TestCase(null, null, null, null, "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
+        [TestCase("server", null, null, null, "Please specify a username and password, or an Octopus API key. You can get an API key from the Octopus web portal. E.g., --apiKey=ABC1234")]
+        [TestCase("server", "user", null, null, "Please specify a password for the specified user account")]
+        [TestCase("server", "user", "password", null, null)]
+        [TestCase("server", null, "password", null, "Please specify a username for the specified password")]
+        [TestCase("server", "user", "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase("server", "user", null, "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase("server", null, "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase("server", null, null, "apikey", null)]
+        [TestCase(null, "user", null, null, "Please specify a password for the specified user account")]
+        [TestCase(null, "user", "password", null, "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
+        [TestCase(null, null, "password", null, "Please specify a username for the specified password")]
+        [TestCase(null, "user", "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase(null, "user", null, "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase(null, null, "password", "apikey", "Please specify a username and password, or an Octopus API key - not both.")]
+        [TestCase(null, null, null, "apikey", "Please specify an Octopus server, e.g., --server=http://your-octopus-server")]
+        public void ValidationIsCorrectWhenMandatory(string server, string username, string password, string apiKey, string expectedExceptionMessage)
         {
             var optionSet = new OptionSet();
             var api = new ApiEndpointOptions(optionSet) { Optional = false };
@@ -102,7 +112,16 @@ namespace Octopus.Tentacle.Tests.Commands.OptionSets
                 args.Add(apiKey);
             }
             optionSet.Parse(args);
-            api.Validate();
+
+            if (expectedExceptionMessage == null)
+            {
+                api.Validate();
+            }
+            else
+            {
+                Action action = api.Validate;
+                action.ShouldThrow<ControlledFailureException>().WithMessage(expectedExceptionMessage);
+            }
         }
 
     }
