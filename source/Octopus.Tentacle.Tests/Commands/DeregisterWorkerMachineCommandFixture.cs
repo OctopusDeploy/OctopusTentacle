ff8ab04783293f51f253402d03319b42c870ebd6
Michael Compton <michael.compton@octopus.com>
2018-02-22 13:57:56 +1100

    add register-worker and deregister-worker commands

---
+++ source/Octopus.Tentacle.Tests/Commands/DeregisterWorkerMachineCommandFixture.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Octopus.Client;
+using Octopus.Client.Model;
+using Octopus.Diagnostics;
+using Octopus.Shared;
+using Octopus.Shared.Configuration;
+using Octopus.Shared.Security;
+using Octopus.Tentacle.Commands;
+using Octopus.Tentacle.Commands.OptionSets;
+using Octopus.Tentacle.Configuration;
+using Octopus.Tentacle.Tests.Support;
+
+namespace Octopus.Tentacle.Tests.Commands
+{
+    [TestFixture]
+    public class DeregisterWorkerMachineCommandFixture : CommandFixture<DeregisterWorkerMachineCommand>
+    {
+        ILog log;
+        IProxyConfigParser proxyConfig;
+        IOctopusAsyncRepository asyncRepository;
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            asyncRepository = Substitute.For<IOctopusAsyncRepository>();
+            proxyConfig = Substitute.For<IProxyConfigParser>();
+            log = Substitute.For<ILog>();
+        }
+
+        [Test]
+        public void ShouldNotContinueIfMultipleMatchesButAllowMultipleIsNotSupplied()
+        {
+            const string expectedThumbPrint1 = "ABCDEFGHIJKLMNOP";
+            const string expectedThumbPrint2 = "1234124123412344";
+
+            var configuration = new StubTentacleConfiguration
+            {
+                TrustedOctopusThumbprints = new List<string> { "NON-MATCHING-THUMBPRINT" },
+                TentacleCertificate = new CertificateGenerator().GenerateNew($"CN={Guid.NewGuid()}", new Shared.Diagnostics.NullLog())
+            };
+            Command = new DeregisterWorkerMachineCommand(new Lazy<ITentacleConfiguration>(() => configuration), 
+                log, 
+                Substitute.For<IApplicationInstanceSelector>(), 
+                proxyConfig,
+                Substitute.For<IOctopusClientInitializer>());
+
+            var matchingMachines = new List<WorkerMachineResource>
+            {
+                new WorkerMachineResource { Name = "m1", Thumbprint = expectedThumbPrint1 },
+                new WorkerMachineResource { Name = "m2", Thumbprint = expectedThumbPrint2 }
+            };
+            asyncRepository.WorkerMachines.FindByThumbprint(Arg.Any<string>())
+                .ReturnsForAnyArgs(matchingMachines.AsTask());
+
+            Func<Task> exec = () => Command.Deregister(asyncRepository);
+            exec.ShouldThrow<ControlledFailureException>().WithMessage(DeregisterWorkerMachineCommand.MultipleMatchErrorMsg);
+        }
+
+        [Test]
+        public async Task ShouldDeregisterIfServerExistsIndependOfTentacleConfiguration()
+        {
+            const string expectedThumbPrint = "ABCDEFGHIJKLMNOP";
+            var configuration = new StubTentacleConfiguration
+            {
+                TrustedOctopusThumbprints = new List<string> { "NON-MATCHING-THUMBPRINT" },
+                TentacleCertificate = new CertificateGenerator().GenerateNew($"CN={Guid.NewGuid()}", new Shared.Diagnostics.NullLog())
+            };
+
+            Command = new DeregisterWorkerMachineCommand(new Lazy<ITentacleConfiguration>(() => configuration),
+                log, 
+                Substitute.For<IApplicationInstanceSelector>(),
+                proxyConfig,
+                Substitute.For<IOctopusClientInitializer>());
+
+            const string machineName = "MachineToBeDeleted";
+            var matchingMachines = new List<WorkerMachineResource>
+            {
+                new WorkerMachineResource { Name = machineName, Thumbprint = expectedThumbPrint }
+            };
+            asyncRepository.WorkerMachines.FindByThumbprint(Arg.Any<string>())
+                .ReturnsForAnyArgs(matchingMachines.AsTask());
+
+            await Command.Deregister(asyncRepository);
+
+            log.Received().Info($"Deleting worker machine '{machineName}' from the Octopus server...");
+            log.Received().Error(DeregisterWorkerMachineCommand.ThumbprintNotFoundMsg);
+        }
+
+        [Test]
+        public async Task ShouldDeleteWhenMatchesThumbprint()
+        {
+            const string expectedThumbPrint = "ABCDEFGHIJKLMNOP";
+            var configuration = new StubTentacleConfiguration
+            {
+                TrustedOctopusThumbprints = new List<string> { expectedThumbPrint },
+                TentacleCertificate = new CertificateGenerator().GenerateNew($"CN={Guid.NewGuid()}", new Shared.Diagnostics.NullLog())
+            };
+
+            Command = new DeregisterWorkerMachineCommand(new Lazy<ITentacleConfiguration>(() => configuration),
+                log,
+                Substitute.For<IApplicationInstanceSelector>(),
+                proxyConfig,
+                Substitute.For<IOctopusClientInitializer>());
+
+            asyncRepository.CertificateConfiguration.GetOctopusCertificate()
+                .ReturnsForAnyArgs(new CertificateConfigurationResource { Thumbprint = expectedThumbPrint }.AsTask());
+
+            const string machineName = "MachineToBeDeleted";
+            var matchingMachines = new List<WorkerMachineResource>
+            {
+                new WorkerMachineResource { Name = machineName, Thumbprint = expectedThumbPrint }
+            };
+            asyncRepository.WorkerMachines.FindByThumbprint(Arg.Any<string>())
+                .ReturnsForAnyArgs(matchingMachines.AsTask());
+
+            await Command.Deregister(asyncRepository);
+
+            log.Received().Info($"Deleting entry '{expectedThumbPrint}' in tentacle.config");
+            log.Received().Info($"Deleting worker machine '{machineName}' from the Octopus server...");
+            log.Received().Info(DeregisterWorkerMachineCommand.DeregistrationSuccessMsg);
+        }
+    }
+}
---
+++ source/Octopus.Tentacle/Commands/DeregisterWorkerMachineCommand.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Octopus.Client;
+using Octopus.Diagnostics;
+using Octopus.Shared;
+using Octopus.Shared.Configuration;
+using Octopus.Shared.Startup;
+using Octopus.Tentacle.Commands.OptionSets;
+using Octopus.Tentacle.Configuration;
+
+namespace Octopus.Tentacle.Commands
+{
+    public class DeregisterWorkerMachineCommand : AbstractStandardCommand
+    {
+        readonly Lazy<ITentacleConfiguration> configuration;
+        readonly ILog log;
+        readonly ApiEndpointOptions api;
+        bool allowMultiple;
+        readonly IProxyConfigParser proxyConfig;
+        readonly IOctopusClientInitializer octopusClientInitializer;
+
+        public const string ThumbprintNotFoundMsg = "The server you supplied did not match the thumbprint stored in the configuration for this tentacle.";
+        public const string DeregistrationSuccessMsg = "Machine deregistered successfully";
+        public const string MultipleMatchErrorMsg = "The worker matches more than one machine on the server. To deregister all of these machines specify the --multiple flag.";
+
+        public DeregisterWorkerMachineCommand(Lazy<ITentacleConfiguration> configuration,
+            ILog log,
+            IApplicationInstanceSelector selector,
+            IProxyConfigParser proxyConfig,
+            IOctopusClientInitializer octopusClientInitializer)
+            : base(selector)
+        {
+            this.configuration = configuration;
+            this.log = log;
+            this.proxyConfig = proxyConfig;
+            this.octopusClientInitializer = octopusClientInitializer;
+
+            api = AddOptionSet(new ApiEndpointOptions(Options));
+            Options.Add("m|multiple", "Deregister all worker machines that use the same thumbprint", s => allowMultiple = true);
+        }
+
+        protected override void Start()
+        {
+            StartAsync().GetAwaiter().GetResult();
+        }
+
+        async Task StartAsync()
+        {
+            //if we are on a polling tentacle with a polling proxy set up, use the api through that proxy
+            var proxyOverride = proxyConfig.ParseToWebProxy(configuration.Value.PollingProxyConfiguration);
+            using (var client = await octopusClientInitializer.CreateClient(api, proxyOverride))
+            {
+                await Deregister(new OctopusAsyncRepository(client));
+            }
+        }
+
+        public async Task Deregister(IOctopusAsyncRepository repository)
+        {
+            // 1. check: do the machine count/allowMultiple checks first to prevent partial trust removal
+            var matchingMachines = await repository.WorkerMachines.FindByThumbprint(configuration.Value.TentacleCertificate.Thumbprint);
+
+            if (matchingMachines.Count == 0)
+                throw new ControlledFailureException("No worker machine was found on the server matching this Tentacle's thumbprint.");
+
+            if (matchingMachines.Count > 1 && !allowMultiple)
+                throw new ControlledFailureException(MultipleMatchErrorMsg);
+
+            // 2. contact the server and de-register, this is independant to any tentacle configuration
+            foreach (var machineResource in matchingMachines)
+            {
+                log.Info($"Deleting worker machine '{machineResource.Name}' from the Octopus server...");
+                await repository.WorkerMachines.Delete(machineResource);
+            }
+
+            // 3. remove the trust from the tentancle cconfiguration
+            var serverThumbprint = (await repository.CertificateConfiguration.GetOctopusCertificate())?.Thumbprint;
+
+            if (configuration.Value.TrustedOctopusThumbprints.Count(t => t.Equals(serverThumbprint, StringComparison.InvariantCultureIgnoreCase)) == 0)
+            {
+                log.Error(ThumbprintNotFoundMsg);
+                return;
+            }
+
+            log.Info($"Deleting entry '{serverThumbprint}' in tentacle.config");
+            configuration.Value.RemoveTrustedOctopusServersWithThumbprint(serverThumbprint);
+
+            log.Info(DeregistrationSuccessMsg);
+            VoteForRestart();
+        }
+    }
+}
