22afb4a104e327e36400d2950d2f1151fbce4e3c
Tom Peters <tom.peters@octopus.com>
2018-11-05 11:15:30 +1000

    Only consider spaces within which the current user has access.

--- source/Octopus.Tentacle/Commands/SpaceRepositoryFactory.cs
+++ source/Octopus.Tentacle/Commands/SpaceRepositoryFactory.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Octopus.Client;
+using Octopus.Client.Model;
 using Octopus.Shared;
 using Octopus.Shared.Properties;
 
@@ -22,7 +24,8 @@ namespace Octopus.Tentacle.Commands
                     throw new ControlledFailureException($"A space with name \"{spaceName}\" was requested, but this version of Octopus Server does not support spaces. Please upgrade Octopus Server to a version which supports spaces, or remove the \"space\" parameter.");
                 }
 
-                var space = await client.Repository.Spaces.FindByName(spaceName);
+                var userSpaces = await FetchUserSpaces();
+                var space = userSpaces.SingleOrDefault(s => string.Equals(s.Name.Trim(), spaceName, StringComparison.OrdinalIgnoreCase));
                 if (space == null)
                 {
                     throw new ControlledFailureException($"A space with name \"{spaceName}\" could not be found. Ensure you have spelled the space name correctly and that the user has access to this space");
@@ -33,7 +36,8 @@ namespace Octopus.Tentacle.Commands
 
             if (await SupportsSpaces(client))
             {
-                var defaultSpace = await client.Repository.Spaces.FindOne(s => s.IsDefault);
+                var userSpaces = await FetchUserSpaces();
+                var defaultSpace = userSpaces.SingleOrDefault(s => s.IsDefault);
                 if (defaultSpace == null)
                 {
                     throw new ControlledFailureException("No \"space\" was specified, and the default space is disabled or inaccessible to this user. Please select a space using the \"space\" parameter.");
@@ -44,6 +48,16 @@ namespace Octopus.Tentacle.Commands
 
             // This works in a backwards compatible way for the endpoints that tentacle cares about
             return client.Repository;
+
+            // We only want to return the spaces within which this user has access, rather than the spaces that this user can see.
+            // This means if a user can see a space because they have the SpaceView permission, they still might not have access within that specific space
+            // This allows us to fail earlier in that scenario with a more helpful message.
+            async Task<SpaceResource[]> FetchUserSpaces()
+            {
+                var userRepository = client.ForSystem().Users;
+                var currentUser = await userRepository.GetCurrent();
+                return await userRepository.GetSpaces(currentUser);
+            }
         }
 
         Task<bool> SupportsSpaces(IOctopusAsyncClient client)
