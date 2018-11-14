0eeb7484b72e21f9a88f40e9e49b4dd8bdf6cbb7
Tom Peters <tom.peters@octopus.com>
2018-11-14 13:04:47 +1000

    Updated help text for space options

--- source/Octopus.Tentacle/Commands/DeregisterWorkerCommand.cs
+++ source/Octopus.Tentacle/Commands/DeregisterWorkerCommand.cs
@@ -41,7 +41,7 @@ namespace Octopus.Tentacle.Commands
 
             api = AddOptionSet(new ApiEndpointOptions(Options));
             Options.Add("m|multiple", "Deregister all workers that use the same thumbprint", s => allowMultiple = true);
-            Options.Add("space=", "The space which this worker will be deregistered from, - e.g. 'Default' where Default is the name of an existing space; the default is the default space", s => spaceName = s);
+            Options.Add("space=", "The space which this worker will be deregistered from, - e.g. 'Finance Department' where Finance Department is the name of an existing space; the default value is the Default space, if one is designated.", s => spaceName = s);
         }
 
         protected override void Start()
