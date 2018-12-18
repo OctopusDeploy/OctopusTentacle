2b20b3ea7180a8acfce81e0bd42b3e1a471bb211
Rob <robert.erez@gmail.com>
2018-12-18 10:49:02 +1000

    Non-interactive host should not shutdown after start. Shutdown signal will be picked up by new signal handlers.

--- source/Octopus.Shared/Startup/NoninteractiveHost.cs
+++ source/Octopus.Shared/Startup/NoninteractiveHost.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Octopus.Shared.Startup
 {
     class NoninteractiveHost : ICommandHost, ICommandRuntime
     {
+        ManualResetEvent mre = new ManualResetEvent(false); 
         public void Run(Action<ICommandRuntime> start, Action shutdown)
         {
             start(this);
-            shutdown();
+            mre.WaitOne();
         }
 
         public void Stop(Action shutdown)
         {
             shutdown();
+            mre.Set();
         }
 
         public void OnExit(int exitCode)
