a997f62349a819a5ae18c9687930386fad65f74e
Rob <robert.erez@gmail.com>
2019-03-08 09:41:32 +1000

    expose GetInstance on IApplicationInstanceStore so we can more easily clean up after E2E tests

--- source/Octopus.Shared/Configuration/IApplicationInstanceStore.cs
+++ source/Octopus.Shared/Configuration/IApplicationInstanceStore.cs
@@ -5,6 +5,7 @@ namespace Octopus.Shared.Configuration
 {
     public interface IApplicationInstanceStore
     {
+        ApplicationInstanceRecord GetInstance(ApplicationName name, string instanceName);
         IList<ApplicationInstanceRecord> ListInstances(ApplicationName name);
         void SaveInstance(ApplicationInstanceRecord instanceRecord);
         void DeleteInstance(ApplicationInstanceRecord instanceRecord);
