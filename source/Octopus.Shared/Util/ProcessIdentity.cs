b3bdedf1126b2b482d7a4d956573d05b13322c40
Tom Williams <twilliams1711@gmail.com>
2019-03-24 23:24:18 +1100

    fix netcore errors

--- source/Octopus.Shared/Util/ProcessIdentity.cs
+++ source/Octopus.Shared/Util/ProcessIdentity.cs
@@ -6,7 +6,9 @@ namespace Octopus.Shared.Util
     public static class ProcessIdentity
     {
         public static string CurrentUserName => PlatformDetection.IsRunningOnWindows ?
+#pragma warning disable PC001 // API not supported on all platforms
             WindowsIdentity.GetCurrent().Name :
+#pragma warning restore PC001 // API not supported on all platforms
             Environment.UserName;
     }
 }
