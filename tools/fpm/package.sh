5cb66fc98f88455dad9a16dbd5cc1d513fee7552
Cameron MacFarland <distantcam@gmail.com>
2019-04-09 11:56:46 +0800

    Deb packaging now with versioning and dependencies

--- tools/fpm/package.sh
+++ tools/fpm/package.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-fpm -n tentacle-service -s pleaserun -t dir /opt/octopus/tentacle/Tentacle agent
+fpm -v $VERSION \
+  -n tentacle-service \
+  -s pleaserun \
+  -t dir \
+  /opt/octopus/tentacle/Tentacle agent
 
-fpm -n tentacle -s dir -t deb --deb-no-default-config-files --after-install setup.sh --before-remove uninstall.sh /app/publish/=/opt/octopus/tentacle ./tentacle-service.dir/usr/share/pleaserun/=/usr/share/pleaserun
+# Dependencies based on https://github.com/dotnet/dotnet-docker/blob/master/2.1/runtime-deps/bionic/amd64/Dockerfile
+fpm -v $VERSION \
+  -n tentacle \
+  -s dir \
+  -t deb \
+  -m '<support@octopus.com>' \
+  --vendor 'Octopus Deploy' \
+  --url 'https://octopus.com/' \
+  --description 'Octopus Tentacle package' \
+  -d 'libc6' \
+  -d 'libgcc1' \
+  -d 'libgssapi-krb5-2' \
+  -d 'libicu60' \
+  -d 'liblttng-ust0' \
+  -d 'libssl1.0.0' \
+  -d 'libstdc++6' \
+  -d 'zlib1g' \
+  --deb-no-default-config-files \
+  --after-install setup.sh \
+  --before-remove uninstall.sh \
+  /app/publish/=/opt/octopus/tentacle \
+  ./tentacle-service.dir/usr/share/pleaserun/=/usr/share/pleaserun
 
 rm -rf tentacle-service.dir
 
-cp tentacle_1.0_amd64.deb /app
+cp *.deb /app
