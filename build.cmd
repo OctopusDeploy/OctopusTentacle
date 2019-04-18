3eee4f5d243df02160936cb3b1367f53424733b5
Cameron MacFarland <distantcam@gmail.com>
2019-04-18 11:38:32 +0800

    Reverting build.cmd change as it breaks existing calls

--- build.cmd
+++ build.cmd
@@ -10,8 +10,6 @@ PowerShell.exe -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Unrestricted 
 EXIT /B %ERRORLEVEL%
 
 :ESCAPE_ARGS
-REM Trim surrounding quotes
-FOR /f "useback tokens=*" %%a IN ('%ARGS%') DO SET ARGS=%%~a
 SET ARGS=%ARGS:"=\"%
 SET ARGS=%ARGS:`=``%
 SET ARGS=%ARGS:'=`'%
@@ -22,7 +20,5 @@ SET ARGS=%ARGS:(=`(%
 SET ARGS=%ARGS:)=`)%
 SET ARGS=%ARGS:,=`,%
 SET ARGS=%ARGS:^%=%
-REM Add surrounding quotes
-SET ARGS="%ARGS%"
 
 GOTO POWERSHELL
