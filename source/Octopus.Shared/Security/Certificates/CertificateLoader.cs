9d431fff4f5c77b5183750523d020ea896d8531d
johnsimons <john.simons@octopus.com>
2019-05-14 13:41:28 +1000

    Adding CertificateLoader

---
+++ source/Octopus.Shared/Security/Certificates/CertificateLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Octopus.Shared.Util;
+
+namespace Octopus.Shared.Security.Certificates
+{
+    public static class CertificateLoader
+    {
+        public static X509Certificate2 FromBase64String(string certificateString)
+        {
+            return FromBase64String(null, certificateString);
+        }
+        public static X509Certificate2 FromBase64String(string thumbprint, string certificateString)
+        {
+            if (PlatformDetection.IsRunningOnWindows)
+            {
+                return CertificateEncoder.FromBase64String(thumbprint, certificateString);
+            }
+
+            return CertificateEncoder.FromBase64String(thumbprint, certificateString, StoreName.My);
+        }
+    }
+}
