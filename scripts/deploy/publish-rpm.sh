84c50d667aea4fd0d11457a6ef90c27008267a78
Shane <tothegills@gmail.com>
2019-07-22 12:21:08 +1000

    Refactor deploy script variables

--- scripts/deploy/publish-rpm.sh
+++ scripts/deploy/publish-rpm.sh
@@ -4,8 +4,8 @@
 #artifacts: this is the directory containing the rpm file(s)
 
 # Publishes built RPMs to an s3-backed RPM repo.
-export AWS_ACCESS_KEY_ID=$(get_octopusvariable "S3:ACCESS_KEY_ID")
-export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "S3:SECRET_ACCESS_KEY")
+export AWS_ACCESS_KEY_ID=$(get_octopusvariable "Publish.RPM.S3.AccessKeyId")
+export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "Publish.RPM.S3.SecretAccessKey")
 
 set -e
 if [ ! -z "${DEBUG}" ]; then
@@ -16,7 +16,6 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SRC_BASE="${SCRIPT_DIR}/../.."
 
 DEPENDENCIES=("aws" "createrepo")
-REGION="us-west-2"
 SOURCE_DIR=""
 TARGET_BUCKET=""
 
@@ -53,7 +52,7 @@ TARGET_DIR="/tmp/${TARGET_BUCKET}"
 
 # make sure we're operating on the latest data in the target bucket
 mkdir -p $TARGET_DIR
-aws --region "${REGION}" s3 sync "s3://${TARGET_BUCKET}" $TARGET_DIR
+aws s3 sync "s3://${TARGET_BUCKET}" $TARGET_DIR
 
 # copy the RPM in and update the repo
 mkdir -pv $TARGET_DIR/x86_64/
@@ -65,4 +64,4 @@ fi
 for a in $TARGET_DIR/x86_64 ; do createrepo -v $UPDATE --deltas $a/ ; done
 
 # sync the repo state back to s3
-aws --region "${REGION}" s3 sync $TARGET_DIR s3://$TARGET_BUCKET
+aws s3 sync $TARGET_DIR s3://$TARGET_BUCKET
