e02b9ee34a20f9ccdfcbcbcba1db8c6ae70af9e5
thekatstevens <andrew.katsivas@gmail.com>
2019-08-28 17:37:54 +0930

    Update outbound links to modern URLs

--- source/Octopus.Shared/Util/OutboundLinks.cs
+++ source/Octopus.Shared/Util/OutboundLinks.cs
@@ -5,28 +5,27 @@ namespace Octopus.Shared.Util
     public class OutboundLinks
     {
         public static readonly string WhatIsIncludedInAnonymousUsageStatistics = "http://octopusdeploy.com/documentation/security/outbound";
-        public static readonly string NuGetPackageHostingOptions = "http://octopusdeploy.com/documentation/packaging";
-        public static readonly string HowDoIGetALicense = "http://octopusdeploy.com/purchase";
-        public static readonly string HowDoIUpgradeMyLicense = "http://octopusdeploy.com/licenses/upgrade";
-        public static readonly string HowDoIGetATrialLicense = "http://octopusdeploy.com/licenses/trial";
-        public static readonly string ProductHomePage = "http://octopusdeploy.com";
-        public static readonly string ProductDownloadPage = "http://octopusdeploy.com/downloads";
+        public static readonly string NuGetPackageHostingOptions = "https://octopus.com/docs/packaging-applications";
+        public static readonly string HowDoIGetALicense = "https://octopus.com/pricing";
+        public static readonly string HowDoIUpgradeMyLicense = "https://octopus.com/upgrade";
+        public static readonly string ProductHomePage = "https://octopus.com";
+        public static readonly string ProductDownloadPage = "https://octopus.com/downloads";
         public static readonly string ProductBacklog = "https://trello.com/board/octopus/4e907de70880ba000079b75c";
-        public static readonly string DocumentationHome = "http://octopusdeploy.com/documentation";
-        public static readonly string DocumentationPackaging = "http://octopusdeploy.com/documentation/packaging";
-        public static readonly string DocumentationVariables = "http://octopusdeploy.com/documentation/features/variables";
-        public static readonly string DocumentationPowerShell = "http://octopusdeploy.com/documentation/features/powershell";
-        public static readonly string ExternalToolOctoPack = "http://octopusdeploy.com/documentation/packaging/octopack";
+        public static readonly string DocumentationHome = "https://octopus.com/docs";
+        public static readonly string DocumentationPackaging = "https://octopus.com/docs/packaging-applications";
+        public static readonly string DocumentationVariables = "https://octopus.com/docs/deployment-process/variables";
+        public static readonly string DocumentationPowerShell = "https://octopus.com/docs/deployment-examples/custom-scripts";
+        public static readonly string ExternalToolOctoPack = "https://octopus.com/docs/packaging-applications/create-packages/octopack";
         public static readonly string ExternalToolOctoTools = "https://github.com/OctopusDeploy/Octopus-Tools";
-        public static readonly string HelpGeneral = "http://help.octopusdeploy.com";
-        public static readonly string HelpReportAProblem = "http://help.octopusdeploy.com/discussions/problems";
-        public static readonly string HelpReportAProblemNewTopic = "http://help.octopusdeploy.com/discussions/problems#new_topic_form";
-        public static readonly string HelpAskQuestion = "http://help.octopusdeploy.com/discussions/questions";
-        public static readonly string HelpMakeSuggestion = "http://help.octopusdeploy.com/discussions/suggestions";
+        public static readonly string HelpGeneral = "https://help.octopus.com";
+        public static readonly string HelpReportAProblem = "https://help.octopus.com";
+        public static readonly string HelpReportAProblemNewTopic = "https://help.octopus.com";
+        public static readonly string HelpAskQuestion = "https://help.octopus.com";
+        public static readonly string HelpMakeSuggestion = "https://help.octopus.com";
         public static readonly string WhatIsMarkdown = "http://en.wikipedia.org/wiki/Markdown";
-        public static readonly string CheckForUpgrades = "https://octopusdeploy.com/download/upgrade/v3";
-        public static readonly string WhyAmINotAllowedToUseDependencies = "http://octopusdeploy.com/documentation/packaging";
-        public static readonly string HowDoIInstallPowerShell = "http://octopusdeploy.com/documentation/install/tentacle";
+        public static readonly string CheckForUpgrades = "https://octopus.com/download/upgrade/v3";
+        public static readonly string WhyAmINotAllowedToUseDependencies = "https://octopus.com/docs/packaging-applications";
+        public static readonly string HowDoIInstallPowerShell = "https://octopus.com/docs/infrastructure/deployment-targets/windows-targets";
         public static readonly string WhereDoIGetMyApiKey = "http://g.octopushq.com/ApiKey";
     }
 }
