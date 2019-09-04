8b0de13bd0a0854b823cba6094d24489184b049f
Shane Gill <tothegills@users.noreply.github.com>
2019-09-04 13:24:56 +1000

    Set executable path on linux (#161)
    * Set appropriate executable path for linux
    
    * Linux executable path needs to be trimmed of dll
    
    * Linux exe path does not have an extension at all

--- source/Octopus.Tentacle/Commands/RunAgentCommand.cs
+++ source/Octopus.Tentacle/Commands/RunAgentCommand.cs
@@ -87,7 +87,9 @@ namespace Octopus.Tentacle.Commands
             Environment.SetEnvironmentVariable(EnvironmentVariables.TentacleApplications, configuration.Value.ApplicationDirectory);
             Environment.SetEnvironmentVariable(EnvironmentVariables.TentacleJournal, configuration.Value.JournalFilePath);
             Environment.SetEnvironmentVariable(EnvironmentVariables.TentacleInstanceName, selector.GetCurrentInstance().InstanceName);
-            var exePath = Path.ChangeExtension(typeof(RunAgentCommand).Assembly.FullLocalPath(), "exe");
+            var exePath = PlatformDetection.IsRunningOnWindows
+                ? Path.ChangeExtension(typeof(RunAgentCommand).Assembly.FullLocalPath(), "exe")
+                : Path.GetFileNameWithoutExtension(typeof(RunAgentCommand).Assembly.FullLocalPath());
             Environment.SetEnvironmentVariable(EnvironmentVariables.TentacleExecutablePath, exePath);
             Environment.SetEnvironmentVariable(EnvironmentVariables.TentacleProgramDirectoryPath, Path.GetDirectoryName(exePath));
             Environment.SetEnvironmentVariable(EnvironmentVariables.AgentProgramDirectoryPath, Path.GetDirectoryName(exePath));
