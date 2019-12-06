0503397324c3e48398d06b4e5e986cd133354b98
Matt Richardson <matt.richardson@octopus.com>
2019-12-06 12:47:24 +1100

    Show more info when powershell check fails
    Adds a "more info" link that shows the command line executed and the output it received to aid debugging.
    
    Relates to OctopusDeploy/Issues#6052
    Fixes https://github.com/OctopusDeploy/OctopusTentacle/issues/179

--- source/Octopus.Manager.Tentacle/PreReq/PowerShellPrerequisite.cs
+++ source/Octopus.Manager.Tentacle/PreReq/PowerShellPrerequisite.cs
@@ -1,6 +1,6 @@
+using System;
 using System.IO;
 using Octopus.Shared.Diagnostics;
-using Octopus.Shared.Util;
 
 namespace Octopus.Manager.Tentacle.PreReq
 {
@@ -10,35 +10,45 @@ namespace Octopus.Manager.Tentacle.PreReq
 
         public PrerequisiteCheckResult Check()
         {
-            return CheckPowerShellIsInstalled()
+            return CheckPowerShellIsInstalled(out var commandLineOutput)
                 ? PrerequisiteCheckResult.Successful()
-                : PrerequisiteCheckResult.Failed("Windows PowerShell 2.0 or above does not appear to be installed and on the System Path on this machine. Please install Windows PowerShell or add it to the System Path then re-run this setup tool.", helpLink: "http://g.octopushq.com/HowDoIInstallPowerShell", helpLinkText: "Download and install Windows PowerShell");
+                : PrerequisiteCheckResult.Failed("Windows PowerShell 2.0 or above does not appear to be installed and on the System Path on this machine. Please install Windows PowerShell or add it to the System Path then re-run this setup tool.",
+                    helpLink: "http://g.octopushq.com/HowDoIInstallPowerShell",
+                    helpLinkText: "Download and install Windows PowerShell",
+                    commandLineOutput: commandLineOutput);
         }
 
-        static bool CheckPowerShellIsInstalled()
+        static bool CheckPowerShellIsInstalled(out string commandLineOutput)
         {
+            var stdOut = new StringWriter();
+            var stdErr = new StringWriter();
+
+            const string powerShellExe = "powershell.exe";
+            const string arguments = "-NonInte3ractive -NoProfile -Command \"Write-Output $PSVersionTable.PSVersion\"";
+            commandLineOutput = $"{powerShellExe} {arguments}";
+        
             // Despite our old check conforming to Microsoft's recommendations
             // for PS version checking around the 1.0/2.0 era, and extending
             // to detect 3.0, it failed to detect 4. Going the direct route:
             try
             {
-                var output = new StringWriter();
-
-                SilentProcessRunner.ExecuteCommand(
-                    "powershell.exe",
-                    "-NonInteractive -NoProfile -Command \"Write-Output $PSVersionTable.PSVersion\"",
+                Octopus.Shared.Util.SilentProcessRunner.ExecuteCommand(
+                    powerShellExe,
+                    arguments,
                     ".",
-                    output.WriteLine,
-                    err =>
-                    {
-                    });
+                    stdOut.WriteLine,
+                    s => stdErr.WriteLine($"ERR: {s}"));
 
-                var outputText = output.ToString();
+                var outputText = stdOut.ToString();
                 Log.Octopus().Verbose("PowerShell prerequisite check output: " + outputText);
-                return outputText.Contains("Major");
+
+                commandLineOutput = $"{commandLineOutput}{Environment.NewLine}{stdOut}{Environment.NewLine}{stdErr}";
+
+                return false;// outputText.Contains("Major");
             }
-            catch
+            catch (Exception ex)
             {
+                commandLineOutput = $"{commandLineOutput}{Environment.NewLine}{ex}";
                 return false;
             }
         }
