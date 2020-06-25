728fd922eae99b72c6e22c180953a37edb998fbc
David Young <thedewi@users.noreply.github.com>
2020-06-26 00:45:06 +1000

    Use common create-linux-packages.sh, tighten up quoting & errors

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
+# Package tentacle from BINARIES_PATH, with executable permission into .deb and .rpm packages (with a /usr/bin symlink)
+# and a .tar.gz archive, placed in PACKAGES_PATH.
 
-# Remove existing packages, fpm doesnt like to overwrite
-rm *.{deb,rpm}
+if [[ -z "$VERSION" ]]; then
+  echo 'This script requires the environment variable VERSION - the version being packaged.' >&2
+  exit 1
+fi
+if [[ -z "$BINARIES_PATH" ]]; then
+  echo 'This script requires the environment variable BINARIES_PATH - the path containing binaries and related files to package.' >&2
+  exit 1
+fi
+if [[ -z "$PACKAGES_PATH" ]]; then
+  echo 'This script requires the environment variable PACKAGES_PATH - the path where packages should be written.' >&2
+  exit 1
+fi
 
-fpm --version $VERSION \
-  --name tentacle \
-  --input-type dir \
-  --output-type deb \
-  --maintainer '<support@octopus.com>' \
-  --vendor 'Octopus Deploy' \
-  --url 'https://octopus.com/' \
-  --description 'Octopus Tentacle package' \
-  --deb-no-default-config-files \
-  --after-install setup.sh \
-  --before-remove uninstall.sh \
-  $TENTACLE_BINARIES=/opt/octopus/tentacle
+which fpm >/dev/null || {
+  echo 'This script requires fpm and related tools, and is intended to be run in the container "octopusdeploy/package-linux-docker".' >&2
+  exit 1
+}
+if [[ ! -e /opt/linux-package-feeds ]]; then
+  echo "This script requires tools in '/opt/linux-package-feeds'. If running inside a container, check the volume mounts." >&2
+  exit 1
+fi
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-fpm --version $VERSION \
-  --name tentacle \
-  --input-type dir \
-  --output-type rpm \
-  --maintainer '<support@octopus.com>' \
-  --vendor 'Octopus Deploy' \
-  --url 'https://octopus.com/' \
-  --description 'Octopus Tentacle package' \
-  --after-install setup.sh \
-  --before-remove uninstall.sh \
-  $TENTACLE_BINARIES=/opt/octopus/tentacle
 
-mkdir tentacle
+if [[ ! -d "$PACKAGES_PATH" ]]; then
+  mkdir -p "$PACKAGES_PATH" || exit
+fi
 
-cp -a $TENTACLE_BINARIES/. tentacle/
+# Create .deb and .rpm packages
+COMMAND_FILE=Tentacle
+INSTALL_PATH=/opt/octopus/tentacle
+PACKAGE_NAME=tentacle
+PACKAGE_DESC='Octopus Tentacle package'
+FPM_OPTS=(
+  --after-install "$SCRIPT_DIR/setup.sh"
+  --before-remove "$SCRIPT_DIR/uninstall.sh"
+)
+FPM_DEB_OPTS=(
+)
+FPM_RPM_OPTS=(
+)
 
-tar -czvf tentacle-$VERSION-linux_x64.tar.gz tentacle
+source /opt/linux-package-feeds/create-linux-packages.sh || exit
 
-mkdir -p $ARTIFACTS
-
-cp -f *.tar.gz $ARTIFACTS
-
-cp -f *.{deb,rpm} $ARTIFACTS
+# Create .tar.gz archive
+rm -rf tentacle || exit
+mkdir tentacle || exit
+cp -a "$BINARIES_PATH/." tentacle/ || exit
+tar czvf "tentacle-$VERSION-linux_x64.tar.gz" tentacle || exit
+mv -f "tentacle-$VERSION-linux_x64.tar.gz" "$PACKAGES_PATH" || exit
