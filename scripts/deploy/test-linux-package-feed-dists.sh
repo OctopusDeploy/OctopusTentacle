13a2b0deb5bdd8d5b2eccf82619b62296da5bb3e
David Young <thedewi@users.noreply.github.com>
2020-06-26 16:48:19 +1000

    Add test-linux-package-feed-dists.sh and its helper.

---
+++ scripts/deploy/test-linux-package-feed-dists.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Smoke test our apt and rpm feeds in various dockerized distros.
+
+if [[ ! -e "$LPF_PATH" ]]; then
+  echo 'This script requires the environment variable LPF_PATH - the location of "linux-package-feeds" tools to use.' >&2
+  exit 1
+fi
+if [[ -z "$PUBLISH_LINUX_EXTERNAL" ]]; then
+  echo 'This script requires the environment variable PUBLISH_LINUX_EXTERNAL - specify "true" to test the external public feed.' >&2
+  exit 1
+fi
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+
+for DOCKER_IMAGE in $(cat "$LPF_PATH/test-env-docker-images.conf" | grep -o '^[^#]*' | tr -d '\r' | head -n2) # ZZDY HEAD ONLY
+do
+  echo "== Testing in '$DOCKER_IMAGE' =="
+  docker pull "$DOCKER_IMAGE" >/dev/null || exit
+  docker run --rm \
+    --hostname "tentacletestfeedpkg$RANDOM" \
+    --volume "$(pwd):/working" --volume "$SCRIPT_DIR/test-linux-package-feed.sh:/test-linux-package-feed.sh" \
+    --volume "$(realpath "$LPF_PATH"):/opt/linux-package-feeds" \
+    --env PUBLISH_LINUX_EXTERNAL \
+    --env REDHAT_SUBSCRIPTION_USERNAME --env REDHAT_SUBSCRIPTION_PASSWORD \
+    "$DOCKER_IMAGE" bash -c 'cd /working && bash /test-linux-package-feed.sh' || exit
+done
