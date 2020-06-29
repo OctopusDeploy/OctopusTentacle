9a8cc600496ee365cee32f2f64e3ba577f182f2b
David Young <thedewi@users.noreply.github.com>
2020-06-29 17:11:24 +1000

    Expand error messages to explain where to get linux-package-feeds

--- scripts/test-linux-package-in-dists.sh
+++ scripts/test-linux-package-in-dists.sh
@@ -2,7 +2,9 @@
 # Test that .deb and .rpm packages in the working directory install a Tentacle command that runs successfully.
 
 if [[ ! -e "$LPF_PATH" ]]; then
-  echo 'This script requires the environment variable LPF_PATH - the location of "linux-package-feeds" tools to use.' >&2
+  echo "This script requires the environment variable LPF_PATH - the location of 'linux-package-feeds' scripts to use." >&2
+  echo "They come from https://github.com/OctopusDeploy/linux-package-feeds/tree/master/source, distributed in TeamCity" >&2
+  echo "  via 'Infrastructure / Linux Package Feeds'." >&2
   exit 1
 fi
 
