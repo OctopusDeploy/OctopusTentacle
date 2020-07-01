a53bc6d6ff189bfac85643bd61eee01331d0b623
David Young <thedewi@users.noreply.github.com>
2020-07-01 10:51:12 +1000

    Restrict sensitive RedHat variables to RedHat containers

--- scripts/deploy/test-linux-package-from-feed-in-dists.sh
+++ scripts/deploy/test-linux-package-from-feed-in-dists.sh
@@ -17,6 +17,12 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 for DOCKER_IMAGE in $(cat "$LPF_PATH/test-env-docker-images.conf" | grep -o '^[^#]*' | tr -d '\r')
 do
+  if [[ "$DOCKER_IMAGE" == *RHEL* ]]; then
+    RHEL_OPTS='--env REDHAT_SUBSCRIPTION_USERNAME --env REDHAT_SUBSCRIPTION_PASSWORD'
+  else
+    RHEL_OPTS=''
+  fi
+
   echo "== Testing in '$DOCKER_IMAGE' =="
   docker pull "$DOCKER_IMAGE" >/dev/null || exit
   docker run --rm \
@@ -24,6 +30,6 @@ do
     --volume "$(pwd):/working" --volume "$SCRIPT_DIR/test-linux-package-from-feed.sh:/test-linux-package-from-feed.sh" \
     --volume "$(realpath "$LPF_PATH"):/opt/linux-package-feeds" \
     --env PUBLISH_LINUX_EXTERNAL \
-    --env REDHAT_SUBSCRIPTION_USERNAME --env REDHAT_SUBSCRIPTION_PASSWORD \
+    $RHEL_OPTS \
     "$DOCKER_IMAGE" bash -c 'cd /working && bash /test-linux-package-from-feed.sh' || exit
 done
--- scripts/test-linux-package-in-dists.sh
+++ scripts/test-linux-package-in-dists.sh
@@ -17,12 +17,18 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 for DOCKER_IMAGE in $(cat "$LPF_PATH/test-env-docker-images.conf" | grep -o '^[^#]*' | tr -d '\r')
 do
+  if [[ "$DOCKER_IMAGE" == *RHEL* ]]; then
+    RHEL_OPTS='--env REDHAT_SUBSCRIPTION_USERNAME --env REDHAT_SUBSCRIPTION_PASSWORD'
+  else
+    RHEL_OPTS=''
+  fi
+
   echo "== Testing in '$DOCKER_IMAGE' =="
   docker pull "$DOCKER_IMAGE" >/dev/null || exit
   docker run --rm \
     --hostname "tentacletestpkg$RANDOM" \
     --volume "$(pwd):/working" --volume "$SCRIPT_DIR/test-linux-package.sh:/test-linux-package.sh" \
     --volume "$(realpath "$LPF_PATH"):/opt/linux-package-feeds" \
-    --env REDHAT_SUBSCRIPTION_USERNAME --env REDHAT_SUBSCRIPTION_PASSWORD \
+    $RHEL_OPTS \
     "$DOCKER_IMAGE" bash -c 'cd /working && bash /test-linux-package.sh' || exit
 done
