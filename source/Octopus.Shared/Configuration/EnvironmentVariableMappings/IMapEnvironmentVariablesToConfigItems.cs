a9037931087a252090ea07d3d46fece2b24a0981
slewis74 <slewis74@gmail.com>
2020-09-01 09:16:33 +1000

    Added MapEnvironmentVariablesToConfigItems

---
+++ source/Octopus.Shared/Configuration/EnvironmentVariableMappings/IMapEnvironmentVariablesToConfigItems.cs
@@ -0,0 +1,13 @@
+﻿namespace Octopus.Shared.Configuration.EnvironmentVariableMappings
+{
+    public interface IMapEnvironmentVariablesToConfigItems
+    {
+        string[] SupportedConfigurationKeys { get; }
+        
+        string[] SupportedEnvironmentVariables { get; }
+
+        void SetEnvironmentVariableValue(string key, string value);
+
+        string GetConfigurationValue(string key);
+    }
+}
---
+++ source/Octopus.Shared/Configuration/EnvironmentVariableMappings/MapEnvironmentVariablesToConfigItems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Octopus.Shared.Configuration.EnvironmentVariableMappings
+{
+    public abstract class MapEnvironmentVariablesToConfigItems : IMapEnvironmentVariablesToConfigItems
+    {
+        readonly Dictionary<string, string> environmentVariableValues;
+
+        public MapEnvironmentVariablesToConfigItems(string[] supportedConfigurationKeys, string[] supportedEnvironmentVariables)
+        {
+            SupportedConfigurationKeys = supportedConfigurationKeys;
+            SupportedEnvironmentVariables = supportedEnvironmentVariables;
+            environmentVariableValues = new Dictionary<string, string>();
+        }
+
+        public string[] SupportedConfigurationKeys { get; }
+        public string[] SupportedEnvironmentVariables { get; }
+
+        protected IReadOnlyDictionary<string, string> EnvironmentVariableValues => environmentVariableValues;
+
+        public void SetEnvironmentVariableValue(string key, string value)
+        {
+            environmentVariableValues[key] = value;
+        }
+
+        public abstract string GetConfigurationValue(string key);
+    }
+}
