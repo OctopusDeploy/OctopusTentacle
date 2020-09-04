8ba750b8329347d43f3724180d6752fa2b433cc0
Andrew Harcourt <andrew.harcourt@octopus.com>
2020-09-04 18:50:40 +1000

    Move EULA check from run step to configure step to prevent the tentacle from even registering if the EULA hasn't been accepted.

--- docker/linux/scripts/run-tentacle.sh
+++ docker/linux/scripts/run-tentacle.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -eux
 
-if [[ "$ENABLE_DIND" -eq "Y" ]]; then
+if [[ "$ENABLE_DIND" == "Y" ]]; then
     echo "Starting Docker-in-Docker daemon. This requires that this container be run in privileged mode."
     nohup /usr/local/bin/dockerd-entrypoint.sh dockerd &
 else
     echo Docker-in-Docker is disabled.
-fi
-
-if [[ "$ACCEPT_EULA" -ne "Y" ]]; then
-    echo "ERROR: You must accept the EULA at https://octopus.com/company/legal by passing an environment variable 'ACCEPT_EULA=Y'"
-    exit 1
 fi
 
 tentacle agent --instance "$OCTOPUS_TENTACLE_INSTANCE_NAME"
