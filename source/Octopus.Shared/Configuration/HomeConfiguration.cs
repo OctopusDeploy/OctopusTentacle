74137104d32db7744c85fb4ebc32a29a98ee0a57
slewis74 <slewis74@gmail.com>
2020-09-07 09:15:29 +1000

    made constaint names clearer

--- source/Octopus.Shared/Configuration/HomeConfiguration.cs
+++ source/Octopus.Shared/Configuration/HomeConfiguration.cs
@@ -6,8 +6,8 @@ namespace Octopus.Shared.Configuration
 {
     public class HomeConfiguration : IHomeConfiguration
     {
-        internal const string OctopusHome = "Octopus.Home";
-        internal const string OctopusNodeCache = "Octopus.Node.Cache";
+        internal const string OctopusHomeSettingName = "Octopus.Home";
+        internal const string OctopusNodeCacheSettingName = "Octopus.Node.Cache";
 
         readonly ApplicationName application;
         readonly IKeyValueStore settings;
@@ -24,19 +24,19 @@ namespace Octopus.Shared.Configuration
         {
             get
             {
-                var value = settings.Get<string?>(OctopusHome, null);
+                var value = settings.Get<string?>(OctopusHomeSettingName, null);
                 if (value != null && !Path.IsPathRooted(value))
                     value = PathHelper.ResolveRelativeDirectoryPath(value);
                 return value;
             }
-            set => settings.Set<string?>(OctopusHome, value);
+            set => settings.Set<string?>(OctopusHomeSettingName, value);
         }
         
         public string? CacheDirectory
         {
             get
             {
-                var value = settings.Get<string?>(OctopusNodeCache, null);
+                var value = settings.Get<string?>(OctopusNodeCacheSettingName, null);
                 if (value == null)
                     return ApplicationSpecificHomeDirectory;
                 
@@ -45,7 +45,7 @@ namespace Octopus.Shared.Configuration
                 
                 return value;
             }
-            set => settings.Set<string?>(OctopusNodeCache, value);
+            set => settings.Set<string?>(OctopusNodeCacheSettingName, value);
         }
     }
 }
