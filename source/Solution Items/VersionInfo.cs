4628a8d80c17f7daf2910e30a66ce5ccd3402de6
Andrew Harcourt <andrew.harcourt@octopus.com>
2020-09-17 18:48:03 +1000

    Make versioning more amenable to being adjusted by non-GitVersion tooling.

--- source/Solution Items/VersionInfo.cs
+++ source/Solution Items/VersionInfo.cs
@@ -1,16 +1,21 @@
-// Versioned by GitVersion
+// This file will be updated by our build process at compile time.
 
+using System;
 using System.Reflection;
+using Octopus.Tentacle;
 
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0-local")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion(VersionInformation.AssemblyVersion)]
+[assembly: AssemblyInformationalVersion(VersionInformation.AssemblyInformationalVersion)]
+[assembly: AssemblyFileVersion(VersionInformation.AssemblyFileVersion)]
 
 namespace Octopus.Tentacle
 {
-    static class GitVersionInformation
+    static class VersionInformation
     {
-        public static string BranchName = "UNKNOWNBRANCH";
-        public static string NuGetVersion = "0.0.0-local";
+        public const string AssemblyVersion = "0.0.0.0";
+        public const string AssemblyFileVersion = "0.0.0.0";
+        public const string AssemblyInformationalVersion = "0.0.0-local";
+        public const string BranchName = "UNKNOWNBRANCH";
+        public const string NuGetVersion = "0.0.0-local";
     }
 }
