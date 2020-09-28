389d5112ae2e47322ed2b462c51be06be3587415
Andrew Harcourt <andrew.harcourt@octopus.com>
2020-09-28 20:27:08 +1000

    Define assembly version attributes so that we can reference them from multiple projects

--- source/Solution Items/VersionInfo.cs
+++ source/Solution Items/VersionInfo.cs
@@ -2,10 +2,32 @@
 
 using System;
 using System.Reflection;
-using Octopus.Tentacle.Versioning;
 
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("0.0.0-local")]
 [assembly: AssemblyGitBranch("UNKNOWNBRANCH")]
 [assembly: AssemblyNuGetVersion("0.0.0-local")]
+
+#if DEFINE_VERSION_ATTRIBUTES
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public sealed class AssemblyGitBranchAttribute : Attribute
+    {
+        public AssemblyGitBranchAttribute(string branchName)
+        {
+            BranchName = branchName;
+        }
+
+        public string BranchName { get; }
+    }
+
+    public sealed class AssemblyNuGetVersionAttribute : Attribute
+    {
+        public AssemblyNuGetVersionAttribute(string nuGetVersion)
+        {
+            NuGetVersion = nuGetVersion;
+        }
+
+        public string NuGetVersion { get; }
+    }
+#endif
