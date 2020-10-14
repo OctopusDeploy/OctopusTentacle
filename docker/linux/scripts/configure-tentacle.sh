2392fbb7b4464ecf9c75866bde6f1acc11eb0ba9
Matthew Casperson <matthewcasperson@gmail.com>
2020-10-15 08:44:21 +1000

    Working on expanding the Linux docker container

--- docker/linux/scripts/configure-tentacle.sh
+++ docker/linux/scripts/configure-tentacle.sh
@@ -3,8 +3,18 @@ set -eux
 
 # This is very much a work in progress. Please do not view this as production-ready. It's in Octopus-internal use at
 # the moment and will be made more robust in due course.
-#
-# This script only currently supports configuring polling tentacles.
+
+function getPublicHostName() {
+	if [[ $1 == 'PublicIp' ]]; then
+		curl https://api.ipify.org/
+	elif [[ $1 == 'FQDN' ]]; then
+		
+	elif [[ $1 == 'ComputerName' ]]; then
+		hostname
+	else
+		echo $CustomPublicHostName
+	fi
+}
 
 if [[ "$ACCEPT_EULA" != "Y" ]]; then
     echo "ERROR: You must accept the EULA at https://octopus.com/company/legal by passing an environment variable 'ACCEPT_EULA=Y'"
@@ -26,7 +36,66 @@ applicationsDirectory=/home/Octopus/Applications
 mkdir -p $configurationDirectory
 mkdir -p $applicationsDirectory
 
+ARGS=()
+
+if [[ ! -z "$WorkerPool" ]]; then
+	ARGS+=('register-worker')
+			
+	IFS=',' read -ra WORKER_POOLS <<< "$WorkerPool"
+	for i in "${WORKER_POOLS[@]}"; do
+		ARGS+=('--workerpool', $i)
+	done
+else
+	  ARGS+=('register-with')
+fi
+
+ARGS+=(
+	'--console',
+	'--instance', $instanceName,
+	'--server', $ServerUrl,
+	'--force')
+		
+if [[ ! -z "$ServerPort" ]]; then
+	ARGS+=(
+		'--comms-style', 'TentacleActive',
+		'--server-comms-port', $ServerPort)
+else
+	ARGS+=(
+		'--comms-style', 'TentaclePassive',
+		'--publicHostName', $(getPublicHostName),
+		'--tentacle-comms-port', $ListeningPort)
+fi
+
+if [[ ! -z "$ServerApiKey" ]]; then
+	ARGS+=('apiKey', $ServerApiKey)
+else
+	ARGS+=(
+		'--username', $ServerUsername,
+    	'--password', $ServerPassword)
+fi
+
+if [[ ! -z "$TargetName" ]]; then
+	ARGS+=('--name', $TargetName)
+fi
+
+if [[ ! -z "$TargetEnvironment" ]]; then
+	IFS=',' read -ra ENVIRONMENTS <<< "$TargetEnvironment"
+	for i in "${ENVIRONMENTS[@]}"; do
+		ARGS+=('--environment', $i)
+	done
+fi
+
+if [[ ! -z "$TargetRole" ]]; then
+	IFS=',' read -ra ROLES <<< "$TargetRole"
+	for i in "${ROLES[@]}"; do
+		ARGS+=('--role', $i)
+	done
+fi
+
 tentacle create-instance --instance "$instanceName" --config "$configurationDirectory/tentacle.config"
 tentacle new-certificate --instance "$instanceName" --if-blank
 tentacle configure --instance "$instanceName" --app "$applicationsDirectory" --noListen "True" --reset-trust
-tentacle register-worker --instance "$instanceName" --server "$ServerUrl" --name "$HOSTNAME" --comms-style "$CommunicationsStype" --server-comms-port $ServerPort --apiKey $ServerApiKey --space "$Space" --workerpool="$WorkerPool" --policy="$MachinePolicy" --force
+
+tentacle "${ARGS[@]}"
+
+#tentacle register-worker --instance "$instanceName" --server "$ServerUrl" --name "$HOSTNAME" --comms-style "$CommunicationsStype" --server-comms-port $ServerPort --apiKey $ServerApiKey --space "$Space" --workerpool="$WorkerPool" --policy="$MachinePolicy" --force
