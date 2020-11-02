ad00602c2f118d3fe7727ae6e3c8b1929fb02b7d
slewis74 <slewis74@gmail.com>
2020-11-02 17:38:33 +1000

    added tests around MapsTentacleEnvironmentValuesToConfigItems

---
+++ source/Octopus.Tentacle.Tests/Configuration/EnvironmentValuesFixture.cs
@@ -0,0 +1,57 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using System.Linq;
+using Assent;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Octopus.Shared.Configuration.EnvironmentVariableMappings;
+using Octopus.Shared.Startup;
+using Octopus.Tentacle.Configuration;
+
+namespace Octopus.Tentacle.Tests.Configuration
+{
+    [TestFixture]
+    public class EnvironmentValuesFixture
+    {
+        MapsTentacleEnvironmentValuesToConfigItems mapper = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mapper = new MapsTentacleEnvironmentValuesToConfigItems(Substitute.For<ILogFileOnlyLogger>());
+        }
+
+        [Test]
+        public void HomeDirectoryCanBeMapped()
+        {
+            mapper.SetEnvironmentValues(new Dictionary<string, string?> { { "OCTOPUS_HOME", @"c:\MyHome" }});
+            var result = mapper.GetConfigurationValue("Octopus.Home");
+            result.Should().Be(@"c:\MyHome", because: "Values provided by Shared are mapped");
+        }
+
+        [Test]
+        public void ValueCanBeMappedDirectly()
+        {
+            mapper.SetEnvironmentValues(new Dictionary<string, string?> { { MapsTentacleEnvironmentValuesToConfigItems.ProxyHost.Name, "Data Source=.;Initial Catalog=OctopusDeploy-master;Trusted_connection=true" }});
+            var result = mapper.GetConfigurationValue(PollingProxyConfiguration.ProxyHostSettingName);
+            result.Should().Be("Data Source=.;Initial Catalog=OctopusDeploy-master;Trusted_connection=true", because: "Raw SQL connection string gets passed directly through");
+        }
+
+        [Test]
+        public void SupportedEnvironmentValueNames()
+        {
+            var text = @"===================================================================================
+This list contains the supported environment value names, which is a public contract.
+===================================================================================
+" + string.Join("\n",
+                MapsTentacleEnvironmentValuesToConfigItems.SupportedEnvironmentValues.Select(x =>
+                {
+                    if (x is SensitiveEnvironmentVariable)
+                        return x.Name + " (sensitive)";
+                    return x.Name;
+                }).OrderBy(x => x));
+            this.Assent(text);
+        }
+    }
+}
