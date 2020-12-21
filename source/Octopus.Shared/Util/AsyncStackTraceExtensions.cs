9a01e33af2e9fb042b264000d9680a8537c78628
Matt Richardson <matt.richardson@octopus.com>
2020-12-22 10:02:39 +1100

    Dont try and work with a null stack trace (#124)

--- source/Octopus.Shared/Util/AsyncStackTraceExtensions.cs
+++ source/Octopus.Shared/Util/AsyncStackTraceExtensions.cs
@@ -38,86 +38,89 @@ namespace Octopus.Shared.Util
 
             var sb = new StringBuilder();
 
-            foreach (var stackTrace in ex.StackTrace!.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+            if (ex.StackTrace != null)
             {
-                var s = stackTrace;
-
-                // Get rid of stack-frames that are part of the BCL async machinery
-                if (s.StartsWith("   at "))
-                    s = s.Substring(6);
-                else
-                    continue;
-
-                if (s == "System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)")
-                    continue;
-
-                if (s == "System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)")
-                    continue;
-
-                if (s == "System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()")
-                    continue;
-
-                if (s == "System.Runtime.CompilerServices.TaskAwaiter.GetResult()")
-                    continue;
-
-                // Get rid of stack-frames that are part of the runtime exception handling machinery
-                if (s == "System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()")
-                    continue;
-
-                // Get rid of stack-frames that are part of .NET Native machiner
-                if (s.Contains("!<BaseAddress>+0x"))
-                    continue;
-
-                // Get rid of stack frames from VB and C# compiler-generated async state machine
-                s = Re1.Replace(s, "$1");
-                s = Re2.Replace(s, "$1");
-
-                // If the stack trace had PDBs, "Alpha.Beta.GammaAsync in c:\code\module1.vb:line 53"
-                var re3Match = Re3.Match(s);
-                s = re3Match.Success ? re3Match.Groups[1].Value : s;
-                var pdbfile = re3Match.Success ? re3Match.Groups[2].Value : null;
-                var pdbline = re3Match.Success ? (int?)int.Parse(Convert.ToString(re3Match.Groups[3].Value)) : null;
-
-                // Get rid of stack frames from AsyncStackTrace
-                if (s.EndsWith("AsyncStackTraceExtensions.Log`1"))
-                    continue;
-
-                if (s.EndsWith("AsyncStackTraceExtensions.Log"))
-                    continue;
-
-                if (s.Contains("AsyncStackTraceExtensions.Log<"))
-                    continue;
-
-                // Extract the method name, "Alpha.Beta.GammaAsync"
-                var re4Match = Re4.Match(s);
-                var fullyQualified = s;
-                var member = re4Match.Success ? re4Match.Groups[1].Value : "";
-
-                // Now attempt to relate this to the log
-                // We'll assume that every logged call is in the stack (Q. will this assumption be violated by inlining?)
-                // We'll assume that not every call in the stack was logged, since users might chose not to log
-                // We'll assume that the bottom-most stack frame wasn't logged
-                if (logs.Count > 0 && logs.Peek().Member == member && sb.Length > 0)
+                foreach (var stackTrace in ex.StackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    var log = logs.Dequeue();
-                    sb.AppendFormat("   at {1}{2} in {3}:line {4}{0}",
-                        Environment.NewLine,
-                        fullyQualified,
-                        log.LabelAndArg,
-                        Path.GetFileName(log.Path),
-                        log.Line);
-                }
-                else if (pdbfile != null)
-                {
-                    sb.AppendFormat("   at {1} in {2}:line {3}{0}",
-                        Environment.NewLine,
-                        fullyQualified,
-                        Path.GetFileName(pdbfile),
-                        pdbline);
-                }
-                else
-                {
-                    sb.AppendFormat("   at {1}{0}", "\r\n", fullyQualified);
+                    var s = stackTrace;
+
+                    // Get rid of stack-frames that are part of the BCL async machinery
+                    if (s.StartsWith("   at "))
+                        s = s.Substring(6);
+                    else
+                        continue;
+
+                    if (s == "System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)")
+                        continue;
+
+                    if (s == "System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)")
+                        continue;
+
+                    if (s == "System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()")
+                        continue;
+
+                    if (s == "System.Runtime.CompilerServices.TaskAwaiter.GetResult()")
+                        continue;
+
+                    // Get rid of stack-frames that are part of the runtime exception handling machinery
+                    if (s == "System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()")
+                        continue;
+
+                    // Get rid of stack-frames that are part of .NET Native machiner
+                    if (s.Contains("!<BaseAddress>+0x"))
+                        continue;
+
+                    // Get rid of stack frames from VB and C# compiler-generated async state machine
+                    s = Re1.Replace(s, "$1");
+                    s = Re2.Replace(s, "$1");
+
+                    // If the stack trace had PDBs, "Alpha.Beta.GammaAsync in c:\code\module1.vb:line 53"
+                    var re3Match = Re3.Match(s);
+                    s = re3Match.Success ? re3Match.Groups[1].Value : s;
+                    var pdbfile = re3Match.Success ? re3Match.Groups[2].Value : null;
+                    var pdbline = re3Match.Success ? (int?)int.Parse(Convert.ToString(re3Match.Groups[3].Value)) : null;
+
+                    // Get rid of stack frames from AsyncStackTrace
+                    if (s.EndsWith("AsyncStackTraceExtensions.Log`1"))
+                        continue;
+
+                    if (s.EndsWith("AsyncStackTraceExtensions.Log"))
+                        continue;
+
+                    if (s.Contains("AsyncStackTraceExtensions.Log<"))
+                        continue;
+
+                    // Extract the method name, "Alpha.Beta.GammaAsync"
+                    var re4Match = Re4.Match(s);
+                    var fullyQualified = s;
+                    var member = re4Match.Success ? re4Match.Groups[1].Value : "";
+
+                    // Now attempt to relate this to the log
+                    // We'll assume that every logged call is in the stack (Q. will this assumption be violated by inlining?)
+                    // We'll assume that not every call in the stack was logged, since users might chose not to log
+                    // We'll assume that the bottom-most stack frame wasn't logged
+                    if (logs.Count > 0 && logs.Peek().Member == member && sb.Length > 0)
+                    {
+                        var log = logs.Dequeue();
+                        sb.AppendFormat("   at {1}{2} in {3}:line {4}{0}",
+                            Environment.NewLine,
+                            fullyQualified,
+                            log.LabelAndArg,
+                            Path.GetFileName(log.Path),
+                            log.Line);
+                    }
+                    else if (pdbfile != null)
+                    {
+                        sb.AppendFormat("   at {1} in {2}:line {3}{0}",
+                            Environment.NewLine,
+                            fullyQualified,
+                            Path.GetFileName(pdbfile),
+                            pdbline);
+                    }
+                    else
+                    {
+                        sb.AppendFormat("   at {1}{0}", "\r\n", fullyQualified);
+                    }
                 }
             }
 
