1e13eddc61dcddd6128614d2c832178460c751f0
Shane <tothegills@gmail.com>
2021-01-21 14:12:51 +1000

    Use well known SID rather than group name

--- scripts/Harden-InstallationDirectory.ps1
+++ scripts/Harden-InstallationDirectory.ps1
@@ -1,14 +1,19 @@
 ﻿$path = $PSScriptRoot
-Write-Host "Applying permissions to directory $path"
+
+[System.Security.Principal.WellKnownSidType] $wellKnownSid = [System.Security.Principal.WellKnownSidType]::BuiltinUsersSid
+$identifier = New-Object System.Security.Principal.SecurityIdentifier -ArgumentList $wellKnownSid, $null
+$user = $identifier.Translate( [System.Security.Principal.NTAccount])
+
+Write-Host "Removing write permissions for $user"
 
 # Remove inheritance but preserve existing entries
 $acl = (Get-Item $path).GetAccessControl('Access')
 $acl.SetAccessRuleProtection($true,$true)
 Set-Acl  $path -AclObject $acl
 
-# Remove write access rules for BUILTIN\Users
+# Remove write access rules
 $acl = (Get-Item $path).GetAccessControl('Access')
-$writeRules = $acl.Access | ? {($_.IdentityReference -eq 'BUILTIN\Users') -and ($_.AccessControlType -eq 'Allow')} | ? {($_.FileSystemRights -match 'CreateFiles') -or ($_.FileSystemRights -match 'AppendData')}
+$writeRules = $acl.Access | ? {($_.IdentityReference -eq $user.Value) -and ($_.AccessControlType -eq 'Allow')} | ? {($_.FileSystemRights -match 'CreateFiles') -or ($_.FileSystemRights -match 'AppendData')}
 foreach ($rule in $writeRules) {
   $acl.RemoveAccessRule($rule)
 }
