54f56206cf63b4a9433e5a89bdf2d506b6ec200a
Robert Wagner <robert.wagner@octopus.com>
2021-02-15 09:52:26 +1000

    Added nullable

--- source/Octopus.CoreUtilities/Maybe.cs
+++ source/Octopus.CoreUtilities/Maybe.cs
@@ -6,6 +6,7 @@ namespace Octopus.CoreUtilities
     {
         Maybe()
         {
+            value = default!;
         }
 
         public static readonly Maybe<T> None = new Maybe<T>();
@@ -38,19 +39,19 @@ namespace Octopus.CoreUtilities
         public static Maybe<T> AsSome<T>(this T value) => Maybe<T>.Some(value);
         public static bool None<T>(this Maybe<T> maybe) => maybe == null || maybe == Maybe<T>.None;
         public static bool Some<T>(this Maybe<T> maybe) => !None(maybe);
-        
-        public static T SomeOrDefault<T>(this Maybe<T> maybe)
+
+        public static T? SomeOrDefault<T>(this Maybe<T> maybe)
             => maybe.Some() ? maybe.Value : default(T);
-        
+
         public static T SomeOr<T>(this Maybe<T> maybe, T ifNone)
             => maybe.Some() ? maybe.Value : ifNone;
 
         public static Maybe<R> Select<T, R>(this Maybe<T> maybe, Func<T, R> selector)
             => maybe.Some() ? selector(maybe.Value).AsSome() : Maybe<R>.None;
 
-        public static R SelectValueOrDefault<T, R>(this Maybe<T> maybe, Func<T, R> selector)
+        public static R? SelectValueOrDefault<T, R>(this Maybe<T> maybe, Func<T, R> selector)
             => maybe.Some() ? selector(maybe.Value) : default(R);
-          
+
         public static R SelectValueOr<T, R>(this Maybe<T> maybe, Func<T, R> selector, R ifNone)
             => maybe.Some() ? selector(maybe.Value) : ifNone;
 
