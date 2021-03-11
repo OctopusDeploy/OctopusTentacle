25636de60c65ed3a5b47714523c6f3f4c4236f1f
slewis74 <slewis74@gmail.com>
2021-03-11 19:36:27 +1000

    fix for custom handling of PrettyPrinting exceptions

--- source/Octopus.Shared/Diagnostics/LoggingModule.cs
+++ source/Octopus.Shared/Diagnostics/LoggingModule.cs
@@ -1,4 +1,9 @@
-﻿using System.Linq;
+﻿#if FULL_FRAMEWORK
+using System.Data.SqlClient;
+#else
+using Microsoft.Data.SqlClient;
+#endif
+using System.Linq;
 using Autofac;
 using Octopus.Diagnostics;
 using Octopus.Shared.Startup;
@@ -9,12 +14,28 @@ namespace Octopus.Shared.Diagnostics
     {
         protected override void Load(ContainerBuilder builder)
         {
+            ConfigurePrettyPrint();
             // Only add the NLogAppender if it isn't already - otherwise we get twice the logs for the price of one
             if (!Log.Appenders.Any(a => a is NLogAppender))
                 Log.Appenders.Add(new NLogAppender());
 
             builder.RegisterType<SystemLog>().As<ISystemLog>().SingleInstance();
             builder.Register(c => new LogFileOnlyLogger()).As<ILogFileOnlyLogger>().InstancePerLifetimeScope();
+        }
+
+        void ConfigurePrettyPrint()
+        {
+            ExceptionExtensions.AddCustomExceptionHandler<SqlException>((sb, ex) =>
+            {
+                var number = ((SqlException)ex).Number;
+                sb.AppendLine($"SQL Error {number} - {ex.Message}");
+                return true;
+            });
+            ExceptionExtensions.AddCustomExceptionHandler<ControlledFailureException>((sb, ex) =>
+            {
+                sb.AppendLine(ex.Message);
+                return false;
+            });
         }
     }
 }
