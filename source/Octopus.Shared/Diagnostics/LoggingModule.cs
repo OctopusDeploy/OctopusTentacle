5a852aee3402968981f3bb6ab967efde9d6e2f20
slewis74 <slewis74@gmail.com>
2021-03-15 11:08:28 +1000

    updated because PrettyPrint now looks for implementors of ICustomPrettyPrintHandler<TException>

--- source/Octopus.Shared/Diagnostics/LoggingModule.cs
+++ source/Octopus.Shared/Diagnostics/LoggingModule.cs
@@ -3,7 +3,9 @@ using System.Data.SqlClient;
 #else
 using Microsoft.Data.SqlClient;
 #endif
+using System;
 using System.Linq;
+using System.Text;
 using Autofac;
 using Octopus.Diagnostics;
 using Octopus.Shared.Startup;
@@ -14,7 +16,6 @@ namespace Octopus.Shared.Diagnostics
     {
         protected override void Load(ContainerBuilder builder)
         {
-            ConfigurePrettyPrint();
             // Only add the NLogAppender if it isn't already - otherwise we get twice the logs for the price of one
             if (!Log.Appenders.Any(a => a is NLogAppender))
                 Log.Appenders.Add(new NLogAppender());
@@ -22,20 +23,24 @@ namespace Octopus.Shared.Diagnostics
             builder.RegisterType<SystemLog>().As<ISystemLog>().SingleInstance();
             builder.Register(c => new LogFileOnlyLogger()).As<ILogFileOnlyLogger>().InstancePerLifetimeScope();
         }
+    }
 
-        void ConfigurePrettyPrint()
+    class SqlPrettyPrintHandler : ICustomPrettyPrintHandler<SqlException>
+    {
+        public bool Handle(StringBuilder sb, Exception ex)
         {
-            ExceptionExtensions.AddCustomExceptionHandler<SqlException>((sb, ex) =>
-            {
-                var number = ((SqlException)ex).Number;
-                sb.AppendLine($"SQL Error {number} - {ex.Message}");
-                return true;
-            });
-            ExceptionExtensions.AddCustomExceptionHandler<ControlledFailureException>((sb, ex) =>
-            {
-                sb.AppendLine(ex.Message);
-                return false;
-            });
+            var number = ((SqlException)ex).Number;
+            sb.AppendLine($"SQL Error {number} - {ex.Message}");
+            return true;
         }
     }
+    class ControlledFailureExceptionPrettyPrintHandler : ICustomPrettyPrintHandler<ControlledFailureException>
+    {
+        public bool Handle(StringBuilder sb, Exception ex)
+        {
+            sb.AppendLine(ex.Message);
+            return false;
+        }
+    }
+
 }
