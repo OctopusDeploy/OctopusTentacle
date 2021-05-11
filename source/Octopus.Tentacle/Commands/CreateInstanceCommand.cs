37a68e9964524721231da707038e5574d0f36dfe
Rob <robert.erez@gmail.com>
2021-05-11 14:15:01 +1000

    remove unecessary cto parameter

--- source/Octopus.Tentacle/Commands/CreateInstanceCommand.cs
+++ source/Octopus.Tentacle/Commands/CreateInstanceCommand.cs
@@ -1,30 +1,24 @@
 using System;
-using Octopus.Shared;
 using Octopus.Shared.Configuration.Instances;
 using Octopus.Shared.Startup;
-using Octopus.Shared.Util;
-using Octopus.Tentacle.Configuration;
 
 namespace Octopus.Tentacle.Commands
 {
     public class CreateInstanceCommand : AbstractCommand
     {
         readonly IApplicationInstanceManager instanceManager;
-        private readonly Lazy<IWritableTentacleConfiguration> writableTentacleConfiguration;
         string instanceName;
         string config;
         string home;
 
-        public CreateInstanceCommand(IApplicationInstanceManager instanceManager,
-            Lazy<IWritableTentacleConfiguration> writableTentacleConfiguration)
+        public CreateInstanceCommand(IApplicationInstanceManager instanceManager)
         {
             this.instanceManager = instanceManager;
-            this.writableTentacleConfiguration = writableTentacleConfiguration;
             Options.Add("instance=", "Name of the instance to create", v => instanceName = v);
             Options.Add("config=", "Path to configuration file to create", v => config = v);
             Options.Add("home=", "[Optional] Path to the home directory - defaults to the same directory as the config file", v => home = v);
         }
-
+        
         protected override void Start()
         {
             if (string.IsNullOrWhiteSpace(instanceName))
