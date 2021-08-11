ebddca813a0160818f491e7d12482b54041b9a92
Andrew Watson <andrew.watson@octopus.com>
2021-08-11 23:41:34 +1000

    Refactor AutoServiceFactory

---
+++ source/Octopus.Shared.Tests/Communications/AutofacServiceFactoryFixture.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+using FluentAssertions;
+using Halibut.ServiceModel;
+using NUnit.Framework;
+using Octopus.Shared.Communications;
+
+namespace Octopus.Shared.Tests.Communications
+{
+    public class AutofacServiceFactoryFixture
+    {
+        [Test]
+        public void NoSources_WorksAsExpected()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AutofacServiceFactory>().AsImplementedInterfaces().SingleInstance();
+            
+            var container = builder.Build();
+            
+            var factory = container.Resolve<IServiceFactory>();
+            factory.RegisteredServiceTypes.Count.Should().Be(0);
+        }
+        
+        [Test]
+        public void SingleServiceSource_RegistersCorrectNamedTypes()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AutofacServiceFactory>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<SimpleServiceSource>().AsImplementedInterfaces().SingleInstance();
+            
+            var container = builder.Build();
+            
+            var factory = container.Resolve<IServiceFactory>();
+            factory.RegisteredServiceTypes.Count.Should().Be(1);
+            factory.RegisteredServiceTypes.Select(x => x.Name).Should().Contain(nameof(ISimpleService));
+
+            var service = factory.CreateService(nameof(ISimpleService));
+            var greeting = (service.Service as ISimpleService)?.Greet("Fred");
+            greeting.Should().Be("Hello Fred!");
+        }
+
+        [Test]
+        public void MultipleServiceSources_RegisterCorrectTypes()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AutofacServiceFactory>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<SimpleServiceSource>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<PieServiceSource>().AsImplementedInterfaces().SingleInstance();
+            
+            var container = builder.Build();
+            
+            var factory = container.Resolve<IServiceFactory>();
+            factory.RegisteredServiceTypes.Count.Should().Be(2);
+            var types = factory.RegisteredServiceTypes.Select(x => x.Name).ToList();
+            types.Should().Contain(nameof(ISimpleService));
+            types.Should().Contain(nameof(IPieService));
+            
+            var service = factory.CreateService(nameof(ISimpleService));
+            var greeting = (service.Service as ISimpleService)?.Greet("Fred");
+            greeting.Should().Be("Hello Fred!");
+
+            var pieService = factory.CreateService(nameof(IPieService));
+            var pie = (pieService.Service as IPieService)?.GetPie();
+            pie.Should().Be(3.14159f);
+        }
+
+        [Test]
+        public void Source_ReturningNull_DoesNotThrow()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AutofacServiceFactory>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<NullServiceSource>().AsImplementedInterfaces().SingleInstance();
+            var container = builder.Build();
+            
+            var factory = container.Resolve<IServiceFactory>();
+            factory.RegisteredServiceTypes.Count.Should().Be(0);
+        }
+        
+        [Test]
+        public void Source_ReturningEmpty_DoesNotThrow()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AutofacServiceFactory>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<EmptyServiceSource>().AsImplementedInterfaces().SingleInstance();
+            var container = builder.Build();
+            
+            var factory = container.Resolve<IServiceFactory>();
+            factory.RegisteredServiceTypes.Count.Should().Be(0);
+        }
+        
+        [Test]
+        public void InvalidSource_ThrowsExpectedException()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AutofacServiceFactory>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<InvalidServiceSource>().AsImplementedInterfaces().SingleInstance();
+            var container = builder.Build();
+
+            Assert.Throws<DependencyResolutionException>(() => container.Resolve<IServiceFactory>());
+        }
+        
+        public interface IPieService
+        {
+            float GetPie();
+        }
+
+        public class PieService : IPieService
+        {
+            public float GetPie()
+            {
+                return 3.14159f;
+            }
+        }
+        
+        public class PieServiceSource : IAutofacServiceSource
+        {
+            public IEnumerable<Type> GetServices()
+            {
+                yield return typeof(PieService);
+            }
+        }
+        
+        public interface ISimpleService
+        {
+            string Greet(string name);
+        }
+
+        public class SimpleService : ISimpleService
+        {
+            public string Greet(string name)
+            {
+                return $"Hello {name}!";
+            }
+        }
+
+        public class SimpleServiceSource : IAutofacServiceSource
+        {
+            public IEnumerable<Type> GetServices()
+            {
+                yield return typeof(SimpleService);
+            }
+        }
+        
+        public class NullServiceSource : IAutofacServiceSource
+        {
+            public IEnumerable<Type> GetServices()
+            {
+                return null;
+            }
+        }
+        
+        public class EmptyServiceSource : IAutofacServiceSource
+        {
+            public IEnumerable<Type> GetServices()
+            {
+                return new Type[] {};
+            }
+        }
+        
+        public class InvalidServiceSource  : IAutofacServiceSource
+        {
+            public IEnumerable<Type> GetServices()
+            {
+                yield return typeof(ISimpleService);
+            }
+        }
+    }
+}
--- source/Octopus.Shared/Communications/AutofacServiceFactory.cs
+++ source/Octopus.Shared/Communications/AutofacServiceFactory.cs
@@ -1,16 +1,48 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Autofac;
 using Halibut.ServiceModel;
+using Octopus.CoreUtilities.Extensions;
 
 namespace Octopus.Shared.Communications
 {
-    public class AutofacServiceFactory : IServiceFactory
+    public interface IAutofacServiceSource
+    {
+        IEnumerable<Type> GetServices();
+    }
+    
+    public class AutofacServiceFactory : IServiceFactory, IDisposable
     {
         readonly ILifetimeScope scope;
+        readonly HashSet<Type> serviceTypes = new HashSet<Type>();
 
-        public AutofacServiceFactory(ILifetimeScope scope)
+        public AutofacServiceFactory(ILifetimeScope scope, IEnumerable<IAutofacServiceSource> sources)
         {
-            this.scope = scope;
+            this.scope = scope.BeginLifetimeScope(b =>
+            {
+                // ReSharper disable once ConstantNullCoalescingCondition : if GetServices returns null, this will throw
+                foreach (var service in sources.SelectMany(x => x.GetServices() ?? new Type[] {}))
+                {
+                    BuildService(b, service);
+                }
+            });
+        }
+
+        void BuildService(ContainerBuilder builder, Type serviceType)
+        {
+            var reg = builder.RegisterType(serviceType).SingleInstance();
+            var interfaces = serviceType.GetInterfaces();
+            if (serviceType.IsInterface || interfaces.IsNullOrEmpty())
+            {
+                throw new Exception("Service type must be a class that implements an interface");
+            }
+            
+            foreach (var face in interfaces)
+            {
+                reg = reg.Named(face.Name, typeof(object));
+                serviceTypes.Add(face);
+            }
         }
 
         public IServiceLease CreateService(string serviceName)
@@ -25,6 +57,8 @@ namespace Octopus.Shared.Communications
                 throw new Exception("The Tentacle service is shutting down and cannot process this request.");
             }
         }
+
+        public IReadOnlyList<Type> RegisteredServiceTypes => serviceTypes.ToList();
 
         class Lease : IServiceLease
         {
@@ -41,6 +75,11 @@ namespace Octopus.Shared.Communications
                 if (disposable != null)
                     disposable.Dispose();
             }
+        }
+
+        public void Dispose()
+        {
+            scope.Dispose();
         }
     }
 }
