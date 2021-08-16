5f2453c615c3362acb56bf5461c1b196c6da822a
Andrew Watson <andrew.watson@octopus.com>
2021-08-16 10:06:07 +1000

    Moving AutofacScriptService to Tentacle

--- source/Octopus.Shared.Tests/Contracts/ServiceFixture.cs
+++ source/Octopus.Shared.Tests/Contracts/ServiceFixture.cs
@@ -1,56 +1,19 @@
 ﻿using System;
-using System.Linq;
-using Autofac;
-using FluentAssertions;
-using Halibut.ServiceModel;
+using System.Security.Cryptography.X509Certificates;
+using Halibut;
+using Halibut.Transport.Protocol;
 using NUnit.Framework;
-using Octopus.Shared.Communications;
 using Octopus.Shared.Contracts;
 
 namespace Octopus.Shared.Tests.Contracts
 {
     public class ServiceFixture
     {
-        class DummyScriptService : IScriptService
-        {
-            public ScriptTicket StartScript(StartScriptCommand command)
-            {
-                throw new NotImplementedException();
-            }
-
-            public ScriptStatusResponse GetStatus(ScriptStatusRequest request)
-            {
-                throw new NotImplementedException();
-            }
-
-            public ScriptStatusResponse CancelScript(CancelScriptCommand command)
-            {
-                throw new NotImplementedException();
-            }
-
-            public ScriptStatusResponse CompleteScript(CompleteScriptCommand command)
-            {
-                throw new NotImplementedException();
-            }
-        }
-        
-        readonly IAutofacServiceSource serviceSource = new KnownServiceSource(typeof(DummyScriptService));
-        
         [Test]
-        public void IScriptService_CanBeRegistered()
+        public void IScriptService_CanCreateClient()
         {
-            var builder = new ContainerBuilder();
-            builder.RegisterType<AutofacServiceFactory>().AsImplementedInterfaces().SingleInstance();
-            builder.RegisterInstance(serviceSource).AsImplementedInterfaces();
-            
-            var container = builder.Build();
-            
-            var factory = container.Resolve<IServiceFactory>();
-            factory.RegisteredServiceTypes.Count.Should().Be(1);
-            factory.RegisteredServiceTypes.Select(x => x.Name).Should().Contain(nameof(IScriptService));
-
-            var service = factory.CreateService(nameof(IScriptService));
-            Assert.Throws<NotImplementedException>(() => (service.Service as IScriptService)?.CancelScript(new CancelScriptCommand(new ScriptTicket("ticket"), 0)));
+            // Constructing binder verifies that the interface does not have any disallowed types
+            var binder = new RegisteredSerializationBinder(new[] { typeof(IScriptService) });
         }
     }
 }
