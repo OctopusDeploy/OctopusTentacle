4c420215a186194181f835369af1e943680b7342
Andrew Watson <andrew.watson@octopus.com>
2021-08-16 10:28:10 +1000

    Update Halibut and move AutofacServiceFactory to Tentacle

---
+++ source/Octopus.Tentacle/Communications/KnownServiceSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Octopus.Tentacle.Communications
+{
+    public class KnownServiceSource : IAutofacServiceSource
+    {
+        public KnownServiceSource(params Type[] serviceTypes)
+        {
+            ServiceTypes = serviceTypes;
+        }
+        
+        public IEnumerable<Type> ServiceTypes { get; }
+    }
+}
---
+++ source/Octopus.Tentacle/Communications/UnknownServiceNameException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Octopus.Tentacle.Communications
+{
+    public class UnknownServiceNameException : Exception
+    {
+        public UnknownServiceNameException(string serviceName)
+            : base($"Error: {serviceName} has not been registered through an IAutofacServiceSource")
+        {
+            ServiceName = serviceName;
+        }
+        
+        public string ServiceName { get; }
+    }
+}
