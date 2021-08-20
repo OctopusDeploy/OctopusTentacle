2958492a3b5eb4f147c98e9cc0ca109cd58da4ed
Trent Mohay <trent.mohay@octopus.com>
2021-08-20 17:04:27 +1000

    Remove strip

--- linux-packages/packaging-scripts/create-linux-packages.sh
+++ linux-packages/packaging-scripts/create-linux-packages.sh
@@ -67,8 +67,6 @@ if [[ -d tmp_usr_bin ]]; then
 fi
 
 # Create executable symlink to include in package
-# Remove the build-id from the executable, to prevent collisions
-strip --remove-section=.note.gnu.build-id $INPUT_PATH/$COMMAND_FILE
 mkdir tmp_usr_bin && ln -s "$INSTALL_PATH/$COMMAND_FILE" tmp_usr_bin/ || exit
 
 # Make sure the command has execute permissions
