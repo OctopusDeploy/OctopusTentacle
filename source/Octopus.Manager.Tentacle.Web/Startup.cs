c8d7bb352a0ddb5b517a398aec14a780ec992bcd
Shaun Hevey <shaun.hevey@octopus.com>
2021-10-25 19:51:49 +1100

    adding initial web project

---
+++ source/Octopus.Manager.Tentacle.Web/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Octopus.Diagnostics;
+using Octopus.Shared.Configuration;
+using Octopus.Shared.Configuration.Instances;
+using Octopus.Shared.Diagnostics;
+using Octopus.Shared.Util;
+
+namespace Octopus.Manager.Tentacle.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<ISystemLog, SystemLog>();
+            services.AddSingleton<IOctopusFileSystem, OctopusPhysicalFileSystem>();
+            services.AddSingleton<IApplicationInstanceStore>(provider => new ApplicationInstanceStore(ApplicationName.Tentacle, provider.GetRequiredService<ISystemLog>(), provider.GetRequiredService<IOctopusFileSystem>(), provider.GetRequiredService<IRegistryApplicationInstanceStore>()));
+            if (PlatformDetection.IsRunningOnWindows)
+            {
+                services.AddSingleton<IRegistryApplicationInstanceStore, WindowsRegistryApplicationInstanceStore>();
+            }
+            else
+            {
+                services.AddSingleton<IRegistryApplicationInstanceStore, NullRegistryApplicationInstanceStore>();
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
+
