fc1027d048f0ed5ae5e99947fdfebb7ae4142700
Adrian Codrington <adrian.codrington@octopus.com>
2021-12-02 15:41:00 +0800

    Move SlugGenerator from OctopusDeploy/Octopus.Core

---
+++ source/Octopus.Shared/Util/SlugGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Octopus.Shared.Util
+{
+    public class SlugGenerator
+    {
+        public static string GenerateSlug(string input, Func<string, bool> isInUse)
+        {
+            input = ConvertToLowercaseAndDashes(input);
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                input = "blue-ring";
+            }
+
+            return Uniquifier.UniquifyString(input, isInUse);
+        }
+
+        static string ConvertToLowercaseAndDashes(string value)
+        {
+            value = value ?? string.Empty;
+            value = value.ToLower();
+            value = Regex.Replace(value, "\\s", "-");
+            value = new string(value.Select(x => char.IsLetterOrDigit(x) || x == '-' ? x : '-').ToArray());
+            value = Regex.Replace(value, "-+", "-");
+            value = value.Trim('-', '/');
+            return value;
+        }
+    }
+}
