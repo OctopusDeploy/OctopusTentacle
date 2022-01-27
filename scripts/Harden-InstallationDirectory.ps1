7d9f19fe7a0746c76b3694b921c4b78879f9f653
Nelson Susanto <nelson.susanto@octopus.com>
2022-01-28 10:11:09 +1100

    Revert Harden executable to script, sign script during build (#351)
    * Harden-InstallationDirectory.ps1 is now copied to the output during the build step, and is then signed along with the other files.
    
    * Removed unused using directives
    
    * Re-add Harden ps1 script, remove Hardener project
    
    * Don't bypass execution policy on harden script
    
    * Remove ignore return
    
    * Revert "Remove ignore return"
    
    This reverts commit 86c8a0138c0cbff8dfe9774adbf5614f74292046.
    
    * Bypass execution policy where allowed
    
    Co-authored-by: MissedTheMark <mark.coafield@octopus.com>
    Co-authored-by: Mark Coafield <46470837+MissedTheMark@users.noreply.github.com>

---
+++ scripts/Harden-InstallationDirectory.ps1
@@ -0,0 +1,25 @@
+$path = $PSScriptRoot
+
+try {
+  [System.Security.Principal.WellKnownSidType] $wellKnownSid = [System.Security.Principal.WellKnownSidType]::BuiltinUsersSid
+  $identifier = New-Object System.Security.Principal.SecurityIdentifier -ArgumentList $wellKnownSid, $null
+  $user = $identifier.Translate( [System.Security.Principal.NTAccount])
+
+  Write-Output "Removing write permissions on $path for $user"
+
+  # Remove inheritance but preserve existing entries
+  $acl = (Get-Item $path).GetAccessControl('Access')
+  $acl.SetAccessRuleProtection($true,$true)
+  Set-Acl $path -AclObject $acl
+
+  # Remove write access rules
+  $acl = (Get-Item $path).GetAccessControl('Access')
+  $writeRules = $acl.Access | ? {($_.IdentityReference -eq $user.Value) -and ($_.AccessControlType -eq 'Allow')} | ? {($_.FileSystemRights -match 'CreateFiles') -or ($_.FileSystemRights -match 'AppendData')}
+  foreach ($rule in $writeRules) {
+    $acl.RemoveAccessRule($rule)
+  }
+  Set-Acl $path -AclObject $acl
+}
+catch {
+  Write-Output "Something went wrong while securing $($path): $($PSItem.ToString())"
+}
