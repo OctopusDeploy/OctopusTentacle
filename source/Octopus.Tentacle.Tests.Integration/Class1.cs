a42724d8d814c2e732a08e4a2b75445a7bfddd23
Shane Gill <tothegills@gmail.com>
2022-08-16 09:34:01 +1000

    Break out integration tests that don't run on docker

---
+++ source/Octopus.Tentacle.Tests.Integration/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Octopus.Tentacle.Tests.Integration
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/Octopus.Tentacle.Tests.Integration/Support/TaskHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+namespace Octopus.Tentacle.Tests.Support
+{
+    public static class TaskHelper
+    {
+        public static Task<T> AsTask<T>(this T result)
+        {
+            return Task.FromResult(result);
+        }
+    }
+}
