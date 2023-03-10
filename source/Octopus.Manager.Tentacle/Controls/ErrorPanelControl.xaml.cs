c0cf3ab4c9f643691e2b117b5c192ecb998cdb93
Ben Pearce <ben.pearce@octopus.com>
2023-03-10 13:21:04 +1000

    cleanup usings

--- source/Octopus.Manager.Tentacle/Controls/ErrorPanelControl.xaml.cs
+++ source/Octopus.Manager.Tentacle/Controls/ErrorPanelControl.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
 using Octopus.Manager.Tentacle.Properties;
-using Octopus.Manager.Tentacle.Util;
 
 namespace Octopus.Manager.Tentacle.Controls
 {
