c8a65a68d7a72bdcab0926c62911f7a397259732
Luke Butters <luke.butters@octopus.com>
2023-05-18 09:41:42 +1000

    Adds the Port Forwarder test util to tentacle.

---
+++ source/Octopus.Tentacle.Tests.Integration/Util/TcpUtils/PortForwarder.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Octopus.Tentacle.Tests.Integration.Util.TcpUtils
+{
+    public class PortForwarder : IDisposable
+    {
+        readonly Uri originServer;
+        readonly Socket listeningSocket;
+        readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        readonly List<TcpPump> pumps = new List<TcpPump>();
+        readonly ILogger logger;
+        readonly TimeSpan sendDelay;
+        
+        public int ListeningPort { get; }
+
+        public PortForwarder(Uri originServer, TimeSpan sendDelay)
+        {
+            logger = new SerilogLoggerBuilder().Build().ForContext<PortForwarder>();
+            this.originServer = originServer;
+            this.sendDelay = sendDelay;
+            var scheme = originServer.Scheme;
+
+            listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listeningSocket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+            listeningSocket.Listen(0);
+            logger.Information("Listening on {LoadBalancerEndpoint}", listeningSocket.LocalEndPoint?.ToString());
+
+            ListeningPort = ((IPEndPoint)listeningSocket.LocalEndPoint).Port;
+            PublicEndpoint = new UriBuilder(scheme, "localhost", ListeningPort).Uri;
+
+            Task.Factory.StartNew(() => WorkerTask(cancellationTokenSource.Token).ConfigureAwait(false), TaskCreationOptions.LongRunning);
+        }
+
+        public Uri PublicEndpoint { get; }
+
+        async Task WorkerTask(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Yield();
+
+                try
+                {
+                    var clientSocket = await listeningSocket.AcceptAsync();
+
+                    var originEndPoint = new DnsEndPoint(originServer.Host, originServer.Port);
+                    var originSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+                    var pump = new TcpPump(clientSocket, originSocket, originEndPoint, sendDelay, logger);
+                    pump.Stopped += OnPortForwarderStopped;
+                    lock (pumps)
+                    {
+                        pumps.Add(pump);
+                    }
+
+                    pump.Start();
+                }
+                catch (SocketException ex)
+                {
+                    // This will occur normally on teardown.
+                    logger.Verbose(ex, "Socket Error accepting new connection {Message}", ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, "Error accepting new connection {Message}", ex.Message);
+                }
+            }
+        }
+
+        void OnPortForwarderStopped(object sender, EventArgs e)
+        {
+            if (sender is TcpPump portForwarder)
+            {
+                portForwarder.Stopped -= OnPortForwarderStopped;
+                lock (pumps)
+                {
+                    pumps.Remove(portForwarder);
+                }
+
+                portForwarder.Dispose();
+            }
+        }
+
+        public void PauseExistingConnections()
+        {
+            lock (pumps)
+            {
+                foreach (var pump in pumps)
+                {
+                    pump.Pause();
+                }
+            }
+        }
+
+        public void CloseExistingConnections()
+        {
+            DisposePumps();
+        }
+
+        List<Exception> DisposePumps()
+        {
+            var exceptions = new List<Exception>();
+
+            lock (pumps)
+            {
+                var clone = pumps.ToArray();
+                pumps.Clear();
+                foreach (var pump in clone)
+                {
+                    try
+                    {
+                        pump.Dispose();
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e);
+                    }
+                }
+            }
+
+            return exceptions;
+        }
+
+        public void Dispose()
+        {
+            if(!cancellationTokenSource.IsCancellationRequested) cancellationTokenSource.Cancel();
+            listeningSocket.Close();
+
+            var exceptions = DisposePumps();
+
+            try
+            {
+                listeningSocket.Dispose();
+            }
+            catch (Exception e)
+            {
+                exceptions.Add(e);
+            }
+
+            try
+            {
+                cancellationTokenSource.Dispose();
+            }
+            catch (Exception e)
+            {
+                exceptions.Add(e);
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
---
+++ source/Octopus.Tentacle.Tests.Integration/Util/TcpUtils/TcpPump.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Octopus.Tentacle.Tests.Integration.Util.TcpUtils
+{
+    public class TcpPump : IDisposable
+    {
+        readonly Socket clientSocket;
+        readonly EndPoint clientEndPoint;
+        readonly Socket originSocket;
+        readonly EndPoint originEndPoint;
+        readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        readonly ILogger logger;
+        readonly TimeSpan sendDelay;
+        bool isDisposing;
+        bool isDisposed;
+        public bool IsPaused { get; set; }
+        
+
+        public TcpPump(Socket clientSocket, Socket originSocket, EndPoint originEndPoint, TimeSpan sendDelay, ILogger logger)
+        {
+            this.logger = logger.ForContext<TcpPump>(); 
+            this.clientSocket = clientSocket ?? throw new ArgumentNullException(nameof(clientSocket));
+            this.originSocket = originSocket ?? throw new ArgumentNullException(nameof(originSocket));
+            this.originEndPoint = originEndPoint ?? throw new ArgumentNullException(nameof(originEndPoint));
+            this.sendDelay = sendDelay;
+            clientEndPoint = clientSocket.RemoteEndPoint ?? throw new ArgumentException("Remote endpoint is null", nameof(clientSocket));
+        }
+
+        public event EventHandler<EventArgs> Stopped;
+
+        public void Start()
+        {
+            var cancellationToken = cancellationTokenSource.Token;
+
+            Task.Run(
+                async () =>
+                {
+                    logger.Verbose("Forwarding connection from {ClientEndPoint} to {OriginEndPoint}.", clientEndPoint.ToString(), originEndPoint.ToString());
+
+                    try
+                    {
+                        await originSocket.ConnectAsync(originEndPoint).ConfigureAwait(false);
+
+                        // If the connection was ok, then set-up a pump both ways
+                        var pump1 = Task.Run(async () => await PumpBytes(clientSocket, originSocket, new SocketPump(() => this.IsPaused, sendDelay), cancellationToken).ConfigureAwait(false), cancellationToken);
+                        var pump2 = Task.Run(async () => await PumpBytes(originSocket, clientSocket, new SocketPump(() => this.IsPaused, sendDelay), cancellationToken).ConfigureAwait(false), cancellationToken);
+
+                        // When one is finished, they are both "done" so stop them
+                        await Task.WhenAny(pump1, pump2).ConfigureAwait(false);
+                        logger.Verbose("Stopping connection forwarding from {ClientEndPoint} to {OriginEndPoint}.", clientEndPoint.ToString(), originEndPoint.ToString());
+                        if (!cancellationTokenSource.IsCancellationRequested)
+                        {
+                            cancellationTokenSource.Cancel();
+                        }
+
+                        // Wait for both pumps to be complete
+                        await Task.WhenAll(pump1, pump2).ConfigureAwait(false);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Verbose(ex, "Forwarding between {ClientEndPoint} and {OriginEndPoint} failed.", clientEndPoint.ToString(), originEndPoint.ToString());
+                    }
+
+                    // We are done. Close everything.
+                    logger.Verbose("Stopped connection forwarding from {ClientEndPoint} to {OriginEndPoint}.", clientEndPoint.ToString(), originEndPoint.ToString());
+                    clientSocket.Close();
+                    originSocket.Close();
+
+                    // Let the users know we are done
+                    Stopped?.Invoke(this, EventArgs.Empty);
+                },
+                cancellationToken);
+        }
+
+        async Task PumpBytes(Socket readFrom, Socket writeTo, SocketPump socketPump, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                await Task.Yield();
+
+                if (cancellationToken.IsCancellationRequested) break;
+                if (!readFrom.Connected) break;
+                if (!writeTo.Connected) break;
+
+                try
+                {
+
+                    var socketStatus = await socketPump.PumpBytes(readFrom, writeTo, cancellationToken);
+                    if(socketStatus == SocketPump.SocketStatus.SOCKET_CLOSED) break;
+                }
+                catch (SocketException socketException)
+                {
+                    logger.Verbose(socketException, "Received socket error {SocketErrorCode} {ReadEndPoint}.", socketException.SocketErrorCode, readFrom.ToString());
+                }
+                catch (OperationCanceledException canceledException)
+                {
+                    logger.Verbose(canceledException, "Received pump cancellation {ReadEndPoint}.", readFrom.ToString());
+                }
+                catch (Exception ex)
+                {
+                    logger.Warning(ex, "Received pump exception: {Message}.", ex.Message);
+                }
+            }
+        }
+
+
+        public void Dispose()
+        {
+            if (isDisposing || isDisposed) return;
+            isDisposing = true;
+
+            logger.Verbose("Port forwarder disposed.");
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+            isDisposed = true;
+            
+            clientSocket.Close();
+            originSocket.Close();
+        }
+
+        public void Pause()
+        {
+            IsPaused = true;
+        }
+    }
+}
