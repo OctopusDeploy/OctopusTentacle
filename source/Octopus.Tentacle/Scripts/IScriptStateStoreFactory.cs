e1f05ea45f1d2c377fcfd40b2b63cdc4304fa1fc
Nathan Willoughby <nathan.willoughby@octopus.com>
2023-05-17 23:16:48 +1000

    Added ScriptServiceV2

---
+++ source/Octopus.Tentacle/Scripts/IScriptStateStoreFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Octopus.Tentacle.Contracts;
+
+namespace Octopus.Tentacle.Scripts
+{
+    public interface IScriptStateStoreFactory
+    {
+        ScriptStateStore Create(ScriptTicket ticket, IScriptWorkspace workspace);
+    }
+}
