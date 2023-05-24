92b5a60afe80c26f8dc8ced68ff8b02df05e518e
Luke Butters <luke.butters@octopus.com>
2023-05-24 15:21:23 +1000

    .

--- source/Octopus.Tentacle.Tests.Integration/Util/TcpUtils/IDataTransferObserver.cs
+++ source/Octopus.Tentacle.Tests.Integration/Util/TcpUtils/IDataTransferObserver.cs
@@ -5,16 +5,5 @@ namespace Octopus.Tentacle.Tests.Integration.Util.TcpUtils
     public interface IDataTransferObserver
     {
         public void WritingData(TcpPump tcpPump, MemoryStream buffer);
-
-        public static IDataTransferObserver Combine(params IDataTransferObserver[] dataTransferObservers)
-        {
-            var builder = new DataTransferObserverBuilder();
-            foreach (var dataTransferObserver in dataTransferObservers)
-            {
-                builder.WithWritingDataObserver(dataTransferObserver.WritingData);
-            }
-
-            return builder.Build();
-        }
     }
 }
