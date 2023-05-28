dc03cf8b835e8dc8c9237bdd0ec8181b7a13b7ca
Luke Butters <luke.butters@octopus.com>
2023-05-28 10:01:09 +1000

    Is this faster?

--- source/Octopus.Tentacle.Tests.Integration/Support/TentacleBinaryCache.cs
+++ source/Octopus.Tentacle.Tests.Integration/Support/TentacleBinaryCache.cs
@@ -3,8 +3,11 @@ using System.Collections.Concurrent;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Nito.AsyncEx;
 using NUnit.Framework;
+using Octopus.Tentacle.Tests.Integration.Util;
 using Octopus.Tentacle.Util;
+using Serilog;
 
 namespace Octopus.Tentacle.Tests.Integration.Support
 {
@@ -25,7 +28,8 @@ namespace Octopus.Tentacle.Tests.Integration.Support
 
         public async Task<string> GetTentacleVersion(string tmp, string version)
         {
-            using var _ = versionLock.GetOrAdd(version, s => new SemaphoreSlim(1, 1)).Lock();
+            var logger = new SerilogLoggerBuilder().Build().ForContext<TentacleBinaryCache>();
+            
             var cachDirName = "TentacleBinaryCache";
             if (TentacleExeFinder.IsRunningInTeamCity())
             {
@@ -39,35 +43,47 @@ namespace Octopus.Tentacle.Tests.Integration.Support
 
             var tentacleVersionCacheDir = Path.Combine(cacheDir, version);
             if (Directory.Exists(tentacleVersionCacheDir)) return Path.Combine(tentacleVersionCacheDir, TentacleExeFinder.AddExeExtension("Tentacle"));
-
-            var tentacleExe = await tentacleFetcher.GetTentacleVersion(tmp, version);
-
-            var parentDir = new DirectoryInfo(tentacleExe).Parent;
-
+            
+            
+            logger.Information("Will download tentacle: {Version}", version);
+            var semaphore = versionLock.GetOrAdd(version, s => new SemaphoreSlim(1, 1));
+            await semaphore.WaitAsync(CancellationToken.None);
             try
             {
-                lock (CacheMutex)
+                var tentacleExe = await tentacleFetcher.GetTentacleVersion(tmp, version);
+                var parentDir = new DirectoryInfo(tentacleExe).Parent;
+
+                try
                 {
-                    try
+                    lock (CacheMutex)
                     {
-                        parentDir.MoveTo(tentacleVersionCacheDir);
-                    }
-                    catch (Exception e)
-                    {
-                        TestContext.Error.WriteLine($"Could not move {parentDir.FullName} to {tentacleVersionCacheDir} attempting to copy files instead. {e}");
-                        parentDir.CopyTo(tentacleVersionCacheDir);
-                        Directory.Delete(parentDir.FullName, true);
+                        try
+                        {
+                            parentDir.MoveTo(tentacleVersionCacheDir);
+                        }
+                        catch (Exception e)
+                        {
+                            TestContext.Error.WriteLine($"Could not move {parentDir.FullName} to {tentacleVersionCacheDir} attempting to copy files instead. {e}");
+                            parentDir.CopyTo(tentacleVersionCacheDir);
+                            Directory.Delete(parentDir.FullName, true);
+                        }
                     }
                 }
+                catch (Exception e)
+                {
+                    TestContext.Error.WriteLine($"Could not copy {parentDir.FullName} to {tentacleVersionCacheDir} tentacle will not be cached. {e}");
+                }
+                
+                logger.Information("Download tentacle: {Version}", version);
+
+                if (Directory.Exists(tentacleVersionCacheDir)) return Path.Combine(tentacleVersionCacheDir, TentacleExeFinder.AddExeExtension("Tentacle"));
+
+                return tentacleExe;
             }
-            catch (Exception e)
+            finally
             {
-                TestContext.Error.WriteLine($"Could not copy {parentDir.FullName} to {tentacleVersionCacheDir} tentacle will not be cached. {e}");
+                semaphore.Release(1);
             }
-
-            if (Directory.Exists(tentacleVersionCacheDir)) return Path.Combine(tentacleVersionCacheDir, TentacleExeFinder.AddExeExtension("Tentacle"));
-
-            return tentacleExe;
         }
     }
 }
