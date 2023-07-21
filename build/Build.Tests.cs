23ca81744d9fdb65a5b5061b4be6c31871807fb0
Ray Nham <ray.nham@octopus.com>
2023-07-21 11:14:24 +1000

    fixed test assembly path

--- build/Build.Tests.cs
+++ build/Build.Tests.cs
@@ -211,7 +211,7 @@ partial class Build
         // By doing things this way, we can have a seamless experience between local and remote builds.
         var octopusTentacleTestsDirectory = BuildDirectory / "Octopus.Tentacle.Tests" / testFramework / testRuntime;
         var testAssembliesPath = octopusTentacleTestsDirectory.GlobFiles("*.Tests.dll");
-        var testResultsPath = ArtifactsDirectory / "teamcity" / $"TestResults-Tests-{testFramework}-{testRuntime}";
+        var testResultsPath = ArtifactsDirectory / "teamcity" / $"TestResults-Tests-{testFramework}-{testRuntime}.xml";
         
         try
         {
@@ -242,8 +242,8 @@ partial class Build
         // have to shunt them all around the place.
         // By doing things this way, we can have a seamless experience between local and remote builds.
         var octopusTentacleTestsDirectory = BuildDirectory / "Octopus.Tentacle.Tests.Integration" / testFramework / testRuntime;
-        var testAssembliesPath = octopusTentacleTestsDirectory.GlobFiles("*.Tests*.dll");
-        var testResultsPath = ArtifactsDirectory / "teamcity" / $"TestResults-Tests-Integration-{testFramework}-{testRuntime}";
+        var testAssembliesPath = octopusTentacleTestsDirectory.GlobFiles("*.TestsIntegration.dll");
+        var testResultsPath = ArtifactsDirectory / "teamcity" / $"TestResults-Tests-Integration-{testFramework}-{testRuntime}.xml";
 
         try
         {
