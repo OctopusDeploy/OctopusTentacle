8ee9f3b037cdb3bfef7c045bf8a995cbe17005c8
Nathan Willoughby <nathan.willoughby@octopus.com>
2023-08-07 09:54:35 +1000

    Add async client service interfaces

---
+++ source/Octopus.Tentacle.Contracts/ClientServices/IAsyncClientScriptService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Halibut.ServiceModel;
+
+namespace Octopus.Tentacle.Contracts.ClientServices
+{
+    public interface IAsyncClientScriptService
+    {
+        Task<ScriptTicket> StartScriptAsync(StartScriptCommand command, HalibutProxyRequestOptions halibutProxyRequestOptions);
+        Task<ScriptStatusResponse> GetStatusAsync(ScriptStatusRequest request, HalibutProxyRequestOptions halibutProxyRequestOptions);
+        Task<ScriptStatusResponse> CancelScriptAsync(CancelScriptCommand command, HalibutProxyRequestOptions halibutProxyRequestOptions);
+        Task<ScriptStatusResponse> CompleteScriptAsync(CompleteScriptCommand command, HalibutProxyRequestOptions halibutProxyRequestOptions);
+    }
+}
---
+++ source/Octopus.Tentacle.Contracts/ClientServices/IAsyncClientScriptServiceV2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Halibut.ServiceModel;
+using Octopus.Tentacle.Contracts.ScriptServiceV2;
+
+namespace Octopus.Tentacle.Contracts.ClientServices
+{
+    public interface IAsyncClientScriptServiceV2
+    {
+        Task<ScriptStatusResponseV2> StartScriptAsync(StartScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions);
+        Task<ScriptStatusResponseV2> GetStatusAsync(ScriptStatusRequestV2 request, HalibutProxyRequestOptions proxyRequestOptions);
+        Task<ScriptStatusResponseV2> CancelScriptAsync(CancelScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions);
+        Task CompleteScriptAsync(CompleteScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions);
+    }
+}
