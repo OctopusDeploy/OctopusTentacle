d1cb3b7b4466ef88ec653ab069f470cb153910b9
Nathan Willoughby <86938706+nathanwoctopusdeploy@users.noreply.github.com>
2023-08-07 18:50:59 +1000

    Add async client service interfaces (#558)

---
+++ source/Octopus.Tentacle.Contracts/ClientServices/IAsyncClientCapabilitiesServiceV2.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Halibut.ServiceModel;
+using Halibut.Transport.Caching;
+using Octopus.Tentacle.Contracts.Capabilities;
+
+namespace Octopus.Tentacle.Contracts.ClientServices
+{
+    public interface IAsyncClientCapabilitiesServiceV2
+    {
+        [CacheResponse(600)]
+        Task<CapabilitiesResponseV2> GetCapabilitiesAsync(HalibutProxyRequestOptions halibutProxyRequestOptions);
+    }
+}
---
+++ source/Octopus.Tentacle.Contracts/ClientServices/IAsyncClientFileTransferService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Halibut;
+using Halibut.ServiceModel;
+
+namespace Octopus.Tentacle.Contracts.ClientServices
+{
+    public interface IAsyncClientFileTransferService
+    {
+        Task<UploadResult> UploadFileAsync(string remotePath, DataStream upload, HalibutProxyRequestOptions halibutProxyRequestOptions);
+        Task<DataStream> DownloadFileAsync(string remotePath, HalibutProxyRequestOptions halibutProxyRequestOptions);
+    }
+}
