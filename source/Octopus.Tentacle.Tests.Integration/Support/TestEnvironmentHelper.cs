831ea9bb12ce9b2e3302f0ba7ac4722c8f48f355
Stephen Burman <stephen.burman@octopus.com>
2023-09-11 16:40:08 +1000

    Fixing tests relating to usernames for running on old OS's (like 2012)

---
+++ source/Octopus.Tentacle.Tests.Integration/Support/TestEnvironmentHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+using System;
+using System.Linq;
+using Octopus.Tentacle.Util;
+
+namespace Octopus.Tentacle.Tests.Integration.Support
+{
+    public static class TestEnvironmentHelper
+    {
+#pragma warning disable CA1416
+        public static string CurrentFullUserName => PlatformDetection.IsRunningOnWindows ? WindowsIdentity.GetCurrent().Name : Environment.UserName;
+        public static string CurrentUserName => PlatformDetection.IsRunningOnWindows ? WindowsIdentity.GetCurrent().Name.Split('\\').Last() : Environment.UserName;
+#pragma warning restore CA1416
+    }
+}
