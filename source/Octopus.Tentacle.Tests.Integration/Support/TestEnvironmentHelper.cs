681f56359dba48ce8c1b27cf238a5e5de9e4626d
Stephen Burman <stephen.burman@octopus.com>
2023-09-12 11:13:57 +1000

    Dealing with Linux

--- source/Octopus.Tentacle.Tests.Integration/Support/TestEnvironmentHelper.cs
+++ source/Octopus.Tentacle.Tests.Integration/Support/TestEnvironmentHelper.cs
@@ -11,7 +11,7 @@ namespace Octopus.Tentacle.Tests.Integration.Support
 #pragma warning restore CA1416
 
         // These "Environment" versions are required for testing older operating systems on TeamCity, as "CurrentUserName" gives different values to what is actually set in the environment...
-        public static string EnvironmentUserName => Environment.GetEnvironmentVariable("username") ?? throw new Exception("Environment variable 'username' not set");
-        public static string EnvironmentDomain => Environment.GetEnvironmentVariable("userdomain") ?? throw new Exception("Environment variable 'userdomain' not set");
+        public static string EnvironmentUserName => Environment.GetEnvironmentVariable("username") ?? Environment.UserName;
+        public static string EnvironmentDomain => Environment.GetEnvironmentVariable("userdomain") ?? string.Empty;
     }
 }
