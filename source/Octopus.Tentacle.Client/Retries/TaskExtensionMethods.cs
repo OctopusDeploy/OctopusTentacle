98feda192f3e45b4b6c2d4c1b976d1df2fab4414
Stephen Burman <stephen.burman@octopus.com>
2023-09-19 16:54:08 +1000

    Removing usage of CancellationToken.Register, as it can cause memory issues

--- source/Octopus.Tentacle.Client/Retries/TaskExtensionMethods.cs
+++ source/Octopus.Tentacle.Client/Retries/TaskExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Octopus.Tentacle.Client.Retries
 {
@@ -22,6 +24,47 @@ namespace Octopus.Tentacle.Client.Retries
                     var observedException = t.Exception;
                 },
                 TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+        }
+
+        public static async Task<bool> WaitTillCompletedOrAbandoned(this Task actionTask, TimeSpan abandonAfter, CancellationToken cancellationToken)
+        {
+            var actionTaskCompleted = await actionTask.WaitTillCompletedOrCancelled(cancellationToken);
+
+            if (!actionTaskCompleted)
+            {
+                actionTaskCompleted = await actionTask.WaitTillCompletedOrTimeout(abandonAfter).ConfigureAwait(false);
+
+                if (!actionTaskCompleted)
+                {
+                    actionTask.IgnoreUnobservedExceptions();
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static async Task<bool> WaitTillCompletedOrCancelled(this Task taskToWaitFor, CancellationToken cancellationToken)
+        {
+            using var cleanupCancellationTokenSource = new CancellationTokenSource();
+            using var linkedCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, cleanupCancellationTokenSource.Token);
+
+            var completedTask = await Task.WhenAny(taskToWaitFor, Task.Delay(-1, linkedCancellationTokenSource.Token)).ConfigureAwait(false);
+
+            cleanupCancellationTokenSource.Cancel();
+
+            return taskToWaitFor == completedTask;
+        }
+
+        public static async Task<bool> WaitTillCompletedOrTimeout(this Task taskToWaitFor, TimeSpan delay)
+        {
+            using var cleanupCancellationTokenSource = new CancellationTokenSource();
+
+            var completedTask = await Task.WhenAny(taskToWaitFor, Task.Delay(delay, cleanupCancellationTokenSource.Token)).ConfigureAwait(false);
+
+            cleanupCancellationTokenSource.Cancel();
+
+            return taskToWaitFor == completedTask;
         }
     }
 }
