ac3d537bbe2b687e5b6709ee19bb86399cdf3ca6
Samdanae Imran <117602873+samdanaei@users.noreply.github.com>
2023-09-29 09:27:29 +1300

    Add support for .Net 6 tentacles

--- source/Octopus.Tentacle.Upgrader/GlobalSuppressions.cs
+++ source/Octopus.Tentacle.Upgrader/GlobalSuppressions.cs
@@ -3,7 +3,10 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "PC001:API not supported on all platforms", Justification = "Upgrader only support windows anyway", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.Program.PerformUpgrade(System.String[])~System.Int32")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "PC001:API not supported on all platforms", Justification = "Upgrader only support windows anyway", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.ServiceBouncer.GetRegistryValue(System.String,System.String)~System.String")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "PC001:API not supported on all platforms", Justification = "Upgrader only support windows anyway", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.SoftwareInstaller.IsPendingServerRestart()~System.Boolean")]
+using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("Usage", "PC001:API not supported on all platforms", Justification = "Upgrader only supports windows", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.Program.PerformUpgrade(System.String[])~System.Int32")]
+[assembly: SuppressMessage("Usage", "PC001:API not supported on all platforms", Justification = "Upgrader only supports windows", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.ServiceBouncer.GetRegistryValue(System.String,System.String)~System.String")]
+[assembly: SuppressMessage("Usage", "PC001:API not supported on all platforms", Justification = "Upgrader only supports windows", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.SoftwareInstaller.IsPendingServerRestart()~System.Boolean")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Upgrader only supports windows", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.ServiceBouncer.EnsureServiceExecutablePathIsCorrect(System.ServiceProcess.ServiceController)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Upgrader only supports windows", Scope = "member", Target = "~M:Octopus.Tentacle.Upgrader.SoftwareInstaller.IsPendingServerRestart~System.Boolean")]
