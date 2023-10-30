ba3e0c1a305a4d4f5a588bc54f309ef0974ca906
Alastair Pitts <alastair.pitts@octopus.com>
2023-10-30 12:29:25 +1100

    Add unit test for TentacleClientOptionsBuilder

---
+++ source/Octopus.Tentacle.Tests/Client/TentacleClientOptionsBuilderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Octopus.Tentacle.Client;
+using Octopus.Tentacle.Contracts;
+
+namespace Octopus.Tentacle.Tests.Client
+{
+    [TestFixture]
+    public class TentacleClientOptionsBuilderTests
+    {
+        [Test]
+        public void CannotDisableScriptServiceV1()
+        {
+            // Arrange
+            var builder = new TentacleClientOptionsBuilder();
+
+            // Act + Assert
+            Action x = () => builder.DisableService(nameof(IScriptService));
+
+            x.Should().Throw<ArgumentException>();
+        }
+    }
+}
