25bd84fa5cf2ee1ae202d4adb462743aca1766aa
Stephen Burman <stephen.burman@octopus.com>
2023-11-14 15:36:22 +1100

    Updating Azure testing scripts

--- testing/compatability/azure/deploy.ps1
+++ testing/compatability/azure/deploy.ps1
@@ -8,11 +8,16 @@ $env:octopusServerUrl = $octopusServerUrl
 $env:octopusServerApiKey = $octopusServerApiKey
 $env:adminPassword = $adminPassword
 
-$env:octopusServerRole = 'OsTestingTentacle'
+$env:octopusServerRole = 'OsTestingListening'
 $env:octopusServerEnvironment = 'OsTesting'
 $env:tentacleVersion = '7.0.201'
 
+
 az group create --name Tentacle --location australiaeast
+
+# .NET 6.0
+#---------------------------------
+$env:tentacleUri = "https://octopus-downloads-staging.s3.amazonaws.com/octopus/Octopus.Tentacle.$env:tentacleVersion-net6.0-win-x64.msi"
 
 # Windows 2012
 $env:virtualMachineName = 'Net6Tentacle-Window2012'
@@ -42,4 +47,39 @@ az deployment group create --name Tentacle --resource-group Tentacle --template-
 $env:virtualMachineName = 'Net6Tentacle-Window2022'
 $env:virtualMachineSku = '2022-datacenter-azure-edition'
 $env:os = 'windows2022'
+az deployment group create --name Tentacle --resource-group Tentacle --template-file vm.bicep --parameters vm.bicepparam
+
+
+# .NET 4.8
+#---------------------------------
+$env:tentacleUri = "https://octopus-downloads-staging.s3.amazonaws.com/octopus/Octopus.Tentacle.$env:tentacleVersion-x64.msi"
+
+# Windows 2012
+$env:virtualMachineName = 'Net48Tentacle-Window2012'
+$env:virtualMachineSku = '2012-datacenter-gensecond'
+$env:os = 'windows2012-48'
+az deployment group create --name Tentacle --resource-group Tentacle --template-file vm.bicep --parameters vm.bicepparam
+
+# Windows 2012R2
+$env:virtualMachineName = 'Net48Tentacle-Window2012R2'
+$env:virtualMachineSku = '2012-r2-datacenter-gensecond'
+$env:os = 'windows2012R2-48'
+az deployment group create --name Tentacle --resource-group Tentacle --template-file vm.bicep --parameters vm.bicepparam
+
+# Windows 2016
+$env:virtualMachineName = 'Net48Tentacle-Window2016'
+$env:virtualMachineSku = '2016-datacenter-gensecond'
+$env:os = 'windows2016-48'
+az deployment group create --name Tentacle --resource-group Tentacle --template-file vm.bicep --parameters vm.bicepparam
+
+# Windows 2019
+$env:virtualMachineName = 'Net48Tentacle-Window2019'
+$env:virtualMachineSku = '2019-datacenter-gensecond'
+$env:os = 'windows2019-48'
+az deployment group create --name Tentacle --resource-group Tentacle --template-file vm.bicep --parameters vm.bicepparam
+
+# Windows 2022
+$env:virtualMachineName = 'Net48Tentacle-Window2022'
+$env:virtualMachineSku = '2022-datacenter-azure-edition'
+$env:os = 'windows2022-48'
 az deployment group create --name Tentacle --resource-group Tentacle --template-file vm.bicep --parameters vm.bicepparam
--- testing/compatability/azure/readme.md
+++ testing/compatability/azure/readme.md
@@ -14,11 +14,49 @@ Install Bicep
 
 ## Deploying
 
+
+### Login
 > az login
 
-> az account set --subscription {subscriptionId}
+It may not install the correct tenant (subscription). In which case, do this:
 
-> ./deploy.ps1 -octopusServerThumbprint xyz -octopusServerUrl http://abc.octopus.app -octopusServerApiKey 123 -adminPassword qwerty123!@#
+> az login -t {Tentant Domain}
+For example:
+> az login -t octopusdeploysandbox.onmicrosoft.com
+
+Choose the appropriate subsription. 
+E.g. In Octopus Sandbox, Subscriptions -> Team Core Platform - Sandbox (c772d4b7-5ac7-4e9c-a259-9f84743d4ae5)
+
+> az account set --subscription {Subscription Id}
+For example, for Team Core Platform - Sandbox:
+> az account set --subscription c772d4b7-5ac7-4e9c-a259-9f84743d4ae5
+
+
+### Change Scripts
+Ensure the variables in `deploy.ps1` are correct. Some variables of note:
+- octopusServerRole - The role that the Tentacles will be tagged with.
+- octopusServerEnvironment - The environment the Tentacles will be registered against.
+- tentacleVersion - The version of the Tentacle being used.
+
+### Deploy
+Now, install the VMs and install Tentacle:
+> ./deploy.ps1 -octopusServerThumbprint {OctopusServerThumbprint} -octopusServerUrl {OctopusServerUri} -octopusServerApiKey {OctopusServerApiKey} -adminPassword qwerty123!@#
+Where:
+- OctopusServerUri - The URI of the instance you wish to register against. E.g. https://sast-rpc-retires.testoctopus.app
+- OctopusServerThumbprint - The thumbprint from the same Octopus Server (found in Settings => Thumbprint)
+- OctopusServerApiKey - The API key to use from the same Octopus Server (create via your profile on the Octopus Server instance)
+
+This script will take a while. 
+
+There may be limitations reached in Azure. Be sure to increase these limitations, or work around them by choosing other resources. E.g. choosing the better `hardwareProfile.vmSize: 'Standard_D2as_v5'` in `vm.bicep`.
+
+You can view progress via Azure itself via the Resource Group for the subscription used above.
+
+Ensure that it installs the tentacles within the instance of Octopus Server chosen above.
+
+Note that for the .NET 4.8 instances, some will require a restart of the VM. This can be done by RDPing into them via Azure, and restarting.
+
+
 
 ## Deleting
 
