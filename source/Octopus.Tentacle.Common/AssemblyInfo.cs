24510b05854bcc7050200852c2d50516ba9a9c4e
Alastair Pitts <APErebus@users.noreply.github.com>
2023-11-27 13:07:46 +1100

    Remove Octopus.Tentacle.Common and move files back into Octopus.Tentacle (#691)

--- source/Octopus.Tentacle.Common/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Octopus.Manager.Tentacle")]
-[assembly: InternalsVisibleTo("Octopus.Tentacle.Tests")]
-[assembly: InternalsVisibleTo("Octopus.Tentacle.Tests.Integration")]
