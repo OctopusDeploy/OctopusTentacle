9812b3531352fbf9975f4591790e112278d74131
Adrian Codrington <adrian.codrington@octopus.com>
2024-01-30 15:57:57 +0800

    Create Octopus.Manager.Tentacle.Tests project

---
+++ source/Octopus.Manager.Tentacle.Tests/ApplicationStartupSanityCheckFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Octopus.Manager.Tentacle.Tests
+{
+    [TestFixture]
+    public class ApplicationStartupSanityCheckFixture
+    {
+        [Test]
+        public void ApplicationCanStartWithoutCrashing()
+        {
+            Assert.Fail("insta-fail");
+        }
+    }
+}
---
+++ source/Octopus.Manager.Tentacle.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Octopus.Manager.Tentacle.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Octopus.Manager.Tentacle.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("F37F3918-D14E-4F0D-ADDC-931C872C6078")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
