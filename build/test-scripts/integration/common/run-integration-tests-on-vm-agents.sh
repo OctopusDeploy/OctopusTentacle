a66f805c7ffb35c66fca484fccd297f4969f3309
Luke Butters <luke.butters@octopus.com>
2024-01-31 15:25:07 +1100

    .

--- build/test-scripts/integration/common/run-integration-tests-on-vm-agents.sh
+++ build/test-scripts/integration/common/run-integration-tests-on-vm-agents.sh
@@ -8,7 +8,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-$SCRIPT_DIR/setup-vm-agents.sh
+$SCRIPT_DIR/setup-vm-agent.sh
 
 itdll=`pwd`/build/outputs/integrationtests/net6.0/linux-x64/Octopus.Tentacle.Tests.Integration.dll
 dotnet vstest $itdll /testcasefilter:TestCategory!=RequiresSudoOnLinux /logger:logger://teamcity /TestAdapterPath:/opt/TeamCity/BuildAgent/plugins/dotnet/tools/vstest15 /logger:console;verbosity=detailed
