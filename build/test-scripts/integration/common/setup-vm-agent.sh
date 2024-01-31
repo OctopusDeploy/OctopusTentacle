07c47a0744843685501d8237971b0230019b468f
Luke Butters <luke.butters@octopus.com>
2024-01-31 15:33:13 +1100

    .

--- build/test-scripts/integration/common/setup-vm-agent.sh
+++ build/test-scripts/integration/common/setup-vm-agent.sh
@@ -2,5 +2,7 @@
 
 # VMs can be out dated so install the latest dotnet to ensure the dll to run
 # was not compiled with a newer version
-source /tmp/dotnet-install.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source $SCRIPT_DIR/dotnet-install.sh
 install_dotnet 6.0
