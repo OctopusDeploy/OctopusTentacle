9e6ae8cb3e81e6071e4fcd5f43a4ec0d0aa6522c
Luke Butters <luke.butters@octopus.com>
2024-02-01 14:55:42 +1100

    Run Integration Tests Script for VMs supports sudo (#784)
    * Run Integration Tests Script for VMs supports sudo

--- build/test-scripts/integration/common/run-integration-tests-on-vm-agents.sh
+++ build/test-scripts/integration/common/run-integration-tests-on-vm-agents.sh
@@ -14,4 +14,11 @@ $SCRIPT_DIR/setup-vm-agent.sh
 source $SCRIPT_DIR/set-dotnet-envvars.sh
 
 itdll=`pwd`/build/outputs/integrationtests/net6.0/linux-x64/Octopus.Tentacle.Tests.Integration.dll
+
+
+if [ "$TENTACLE_IT_WITH_SUDO" = "1" ]; then
+sudo -i dotnet vstest $itdll /logger:logger://teamcity /TestAdapterPath:/opt/TeamCity/BuildAgent/plugins/dotnet/tools/vstest15 /logger:console;verbosity=detailed
+else
 dotnet vstest $itdll /testcasefilter:TestCategory!=RequiresSudoOnLinux /logger:logger://teamcity /TestAdapterPath:/opt/TeamCity/BuildAgent/plugins/dotnet/tools/vstest15 /logger:console;verbosity=detailed
+fi
+
