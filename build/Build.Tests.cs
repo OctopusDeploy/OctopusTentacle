b87f1f86426668a2b3966b6480882f82ce46f014
Ray Nham <ray.nham@octopus.com>
2024-02-15 20:18:30 +1100

    added: debug log to monitor TC build

--- build/Build.Tests.cs
+++ build/Build.Tests.cs
@@ -1,4 +1,4 @@
-﻿// ReSharper disable RedundantUsingDirective
+// ReSharper disable RedundantUsingDirective
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -273,7 +273,9 @@ partial class Build
         var octopusTentacleManagerTestsDirectory = BuildDirectory / "Octopus.Manager.Tentacle.Tests" / testFramework / testRuntime;
         var testAssembliesPath = octopusTentacleTestsDirectory.GlobFiles("*.Tests.dll")
             .Union(octopusTentacleClientTestsDirectory.GlobFiles("*.Tests.dll"))
-            .Union(octopusTentacleManagerTestsDirectory.GlobFiles("*.Tests.dll"));
+            .Union(octopusTentacleManagerTestsDirectory.GlobFiles("*.Tests.dll")).ToArray();
+        
+        Log.Warning(string.Join(Environment.NewLine, testAssembliesPath.Select(z => z.ToString())));
         
         try
         {
