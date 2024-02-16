8b5ebac68207407124eb18bb4072c2668c703572
Ray Nham <79076870+raynhamdev@users.noreply.github.com>
2024-02-16 15:25:01 +1100

    Added some wizard model tests for Tentacle Manager (#808)

---
+++ source/Octopus.Manager.Tentacle.Tests/Utils/CommandLineInvocationExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Octopus.Tentacle.Util;
+
+namespace Octopus.Manager.Tentacle.Tests.Utils
+{
+    public static class CommandLineInvocationExtension
+    {
+        public static string ToCommandLineString(this IEnumerable<CommandLineInvocation> source)
+        {
+            return string.Join(Environment.NewLine, source.Select(z => z.ToString()));
+        }
+    }
+}
