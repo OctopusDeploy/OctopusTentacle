2e4287cd6fb72d08b5df3a92906ac93cc44c7c89
Alastair Pitts <alastair.pitts@octopus.com>
2024-02-23 12:46:50 +1100

    Truncate podname to 63 chars

--- source/Octopus.Tentacle/Kubernetes/KubernetesScriptPodNameExtensions.cs
+++ source/Octopus.Tentacle/Kubernetes/KubernetesScriptPodNameExtensions.cs
@@ -1,9 +1,10 @@
 ﻿using Octopus.Tentacle.Contracts;
+using Octopus.Tentacle.Util;
 
 namespace Octopus.Tentacle.Kubernetes
 {
     public static class KubernetesScriptPodNameExtensions
     {
-        public static string ToKubernetesScriptPobName(this ScriptTicket scriptTicket) => $"octopus-script-{scriptTicket.TaskId}".ToLowerInvariant();
+        public static string ToKubernetesScriptPobName(this ScriptTicket scriptTicket) => $"octopus-script-{scriptTicket.TaskId}".ToLowerInvariant().Truncate(63);
     }
 }
