f608667b358aaa959d36b4fadb85a5177f726859
Adrian Codrington <adrian.codrington@octopus.com>
2024-03-07 21:07:08 +0800

    Add telemetry tests

---
+++ source/Octopus.Manager.Tentacle.Tests/Builders/TelemetryServiceBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NSubstitute;
+using Octopus.Manager.Tentacle.Infrastructure;
+using Octopus.Manager.Tentacle.Util;
+
+namespace Octopus.Manager.Tentacle.Tests.Builders
+{
+    public class TelemetryServiceBuilder
+    {
+        bool sendingResult = true;
+
+        public TelemetryServiceBuilder WithSendingResult(bool result)
+        {
+            sendingResult = result;
+            return this;
+        }
+
+        public ITelemetryService Build()
+        {
+            var telemetryService = Substitute.For<ITelemetryService>();
+            telemetryService.SendTelemetryEvent(Arg.Any<Uri>(), Arg.Any<TelemetryEvent>()).Returns(sendingResult);
+            return telemetryService;
+        }
+    }
+}
--- source/Octopus.Manager.Tentacle.Tests/WizardFixtures/SetupTentacleWizardFixture.cs
+++ source/Octopus.Manager.Tentacle.Tests/WizardFixtures/SetupTentacleWizardFixture.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 using Octopus.Client.Model;
-using Octopus.Manager.Tentacle.Shell;
+using Octopus.Manager.Tentacle.Infrastructure;
 using Octopus.Manager.Tentacle.TentacleConfiguration.SetupWizard;
+using Octopus.Manager.Tentacle.Tests.Builders;
 using Octopus.Manager.Tentacle.Tests.Utils;
 using Octopus.Manager.Tentacle.Util;
-using Octopus.Tentacle.Configuration;
-using Octopus.Tentacle.Configuration.Instances;
 
 namespace Octopus.Manager.Tentacle.Tests.WizardFixtures
 {
@@ -18,7 +18,7 @@ namespace Octopus.Manager.Tentacle.Tests.WizardFixtures
         public void WhenSettingUpListeningTentacle_ScriptShouldBeCorrectlyGenerated()
         {
             // Arrange
-            var model = CreateTestSetupTentacleWizardModel();
+            var model = new SetupTentacleWizardModelBuilder().Build();
             model.CommunicationStyle = CommunicationStyle.TentaclePassive;
             model.ListenPort = "10933";
             model.OctopusThumbprint = "TestThumbprint";
@@ -31,20 +31,20 @@ namespace Octopus.Manager.Tentacle.Tests.WizardFixtures
             // Assert
             var pathToTentacleExe = $"\"{model.PathToTentacleExe ?? CommandLine.PathToTentacleExe()}\"";
             var expectedOutput = $"{pathToTentacleExe} create-instance --instance \"{model.InstanceName}\" --config \"C:\\Octopus\\{model.InstanceName}\\Tentacle-{model.InstanceName}.config\"" +
-                    Environment.NewLine + $"{pathToTentacleExe} new-certificate --instance \"{model.InstanceName}\" --if-blank" +
-                    Environment.NewLine + $"{pathToTentacleExe} configure --instance \"{model.InstanceName}\" --reset-trust" +
-                    Environment.NewLine + $"{pathToTentacleExe} configure --instance \"{model.InstanceName}\" --app \"C:\\Octopus\\Applications\\{model.InstanceName}\" --port \"{model.ListenPort}\" --noListen \"False\"" +
-                    Environment.NewLine + $"{pathToTentacleExe} configure --instance \"{model.InstanceName}\" --trust \"{model.OctopusThumbprint}\"" +
-                    Environment.NewLine + $"\"netsh\" advfirewall firewall add rule \"name=Octopus Deploy Tentacle\" dir=in action=allow protocol=TCP localport={model.ListenPort}" +
-                    Environment.NewLine + $"{pathToTentacleExe} service --instance \"{model.InstanceName}\" --install --stop --start";
+                Environment.NewLine + $"{pathToTentacleExe} new-certificate --instance \"{model.InstanceName}\" --if-blank" +
+                Environment.NewLine + $"{pathToTentacleExe} configure --instance \"{model.InstanceName}\" --reset-trust" +
+                Environment.NewLine + $"{pathToTentacleExe} configure --instance \"{model.InstanceName}\" --app \"C:\\Octopus\\Applications\\{model.InstanceName}\" --port \"{model.ListenPort}\" --noListen \"False\"" +
+                Environment.NewLine + $"{pathToTentacleExe} configure --instance \"{model.InstanceName}\" --trust \"{model.OctopusThumbprint}\"" +
+                Environment.NewLine + $"\"netsh\" advfirewall firewall add rule \"name=Octopus Deploy Tentacle\" dir=in action=allow protocol=TCP localport={model.ListenPort}" +
+                Environment.NewLine + $"{pathToTentacleExe} service --instance \"{model.InstanceName}\" --install --stop --start";
             script.Should().Be(expectedOutput);
         }
-        
+
         [Test]
         public void WhenSettingUpPollingTentacle_ScriptShouldBeCorrectlyGenerated()
         {
             // Arrange
-            var model = CreateTestSetupTentacleWizardModel();
+            var model = new SetupTentacleWizardModelBuilder().Build();
             model.CommunicationStyle = CommunicationStyle.TentacleActive;
             model.OctopusServerUrl = "localhost";
             model.AuthMode = AuthMode.APIKey;
@@ -69,17 +69,32 @@ namespace Octopus.Manager.Tentacle.Tests.WizardFixtures
                 Environment.NewLine + $"{pathToTentacleExe} service --instance \"{model.InstanceName}\" --install --stop --start";
             script.Should().Be(expectedOutput);
         }
-        
-        static SetupTentacleWizardModel CreateTestSetupTentacleWizardModel()
-        {
-            var instanceStore = Substitute.For<IApplicationInstanceStore>();
-            var instanceSelectionModel = new InstanceSelectionModel(ApplicationName.Tentacle, instanceStore);
-            const string tentacleInstanceName = "TestInstance";
-            instanceSelectionModel.New(tentacleInstanceName);
 
-            var model = new SetupTentacleWizardModel(instanceSelectionModel);
-            
-            return model;
+        [Test]
+        public async Task WhenSettingUpPollingTentacle_TelemetryEventShouldBeSent()
+        {
+            var telemetryService = new TelemetryServiceBuilder().Build();
+
+            var model = new SetupTentacleWizardModelBuilder()
+                .WithTelemetryService(telemetryService)
+                .Build();
+
+            model.OctopusServerUrl = "http://localhost";
+            model.CommunicationStyle = CommunicationStyle.TentacleActive;
+            model.MachineType = MachineType.DeploymentTarget;
+
+            _ = await model.ReviewAndRunScriptTabViewModel.GenerateAndExecuteScript();
+
+            // TODO: Replace with a less ugly assertion
+            await telemetryService
+                .Received(1)
+                .SendTelemetryEvent(
+                    new Uri("http://localhost"),
+                    Arg.Is<TelemetryEvent>(t =>
+                        t.EventProperties.ContainsKey("Communication Style") &&
+                        t.EventProperties["Communication Style"] == CommunicationStyle.TentacleActive.ToString() &&
+                        t.EventProperties.ContainsKey("Machine Type") &&
+                        t.EventProperties["Machine Type"] == MachineType.DeploymentTarget.ToString()));
         }
     }
 }
