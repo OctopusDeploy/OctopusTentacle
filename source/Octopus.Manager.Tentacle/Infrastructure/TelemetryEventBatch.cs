004fe9703e095d9729c2f8bd31487296361f1ac3
Adrian Codrington <adrian.codrington@octopus.com>
2024-03-07 16:26:34 +0800

    Create TelemetryService

---
+++ source/Octopus.Manager.Tentacle/Infrastructure/TelemetryEventBatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Octopus.Manager.Tentacle.Infrastructure
+{
+    public class TelemetryEventBatch
+    {
+        [JsonProperty("client_upload_time")]
+        public long Timestamp { get; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        [JsonProperty("events")]
+        public IEnumerable<TelemetryEvent> Events { get; }
+
+        public TelemetryEventBatch(TelemetryEvent eventObj)
+        {
+            Events = new List<TelemetryEvent> { eventObj };
+        }
+    }
+}
---
+++ source/Octopus.Manager.Tentacle/Util/TelemetryService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Octopus.Manager.Tentacle.Infrastructure;
+
+namespace Octopus.Manager.Tentacle.Util
+{
+    public interface ITelemetryService
+    {
+        Task<bool> SendTelemetryEvent(Uri serverUri, TelemetryEvent eventObj);
+    }
+    
+    public class TelemetryService: ITelemetryService
+    {
+        public async Task<bool> SendTelemetryEvent(Uri serverUri, TelemetryEvent eventObj)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = serverUri;
+
+                var eventBatch = new TelemetryEventBatch(eventObj);
+                var stringPayload = JsonConvert.SerializeObject(eventBatch);
+
+                var response = await httpClient.PostAsync("api/telemetry/process", new StringContent(stringPayload, Encoding.UTF8, "application/json"));
+                return response.IsSuccessStatusCode;
+            }
+        }
+    }
+}
