75e74f459e5157e45893648df68b5ca13c1c6314
Alastair Pitts <alastair.pitts@octopus.com>
2024-03-21 15:57:17 +1100

    Change the returned services based on if running as kubernetes agent

--- source/Octopus.Tentacle.Tests/Capabilities/CapabilitiesServiceV2Fixture.cs
+++ source/Octopus.Tentacle.Tests/Capabilities/CapabilitiesServiceV2Fixture.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using Octopus.Tentacle.Kubernetes;
 using Octopus.Tentacle.Services.Capabilities;
 
 namespace Octopus.Tentacle.Tests.Capabilities
@@ -9,7 +11,7 @@ namespace Octopus.Tentacle.Tests.Capabilities
     public class CapabilitiesServiceV2Fixture
     {
         [Test]
-        public async Task CapabilitiesAreReturned()
+        public async Task NonKubernetesScriptServicesAreReturnedForNonKubernetesAgent()
         {
             var capabilities = (await new CapabilitiesServiceV2()
                 .GetCapabilitiesAsync(CancellationToken.None))
@@ -18,8 +20,26 @@ namespace Octopus.Tentacle.Tests.Capabilities
             capabilities.Should().Contain("IScriptService");
             capabilities.Should().Contain("IFileTransferService");
             capabilities.Should().Contain("IScriptServiceV2");
-            capabilities.Should().Contain("IScriptServiceV3Alpha");
-            capabilities.Count.Should().Be(4);
+            capabilities.Count.Should().Be(3);
+
+            capabilities.Should().NotContainMatch("IKubernetesScriptService*");
+        }
+
+        [Test]
+        public async Task OnlyKubernetesScriptServicesAreReturnedWhenRunningAsKubernetesAgent()
+        {
+            Environment.SetEnvironmentVariable(KubernetesConfig.NamespaceVariableName, "ABC");
+
+            var capabilities = (await new CapabilitiesServiceV2()
+                    .GetCapabilitiesAsync(CancellationToken.None))
+                .SupportedCapabilities;
+
+            capabilities.Should().Contain("IFileTransferService");
+            capabilities.Should().Contain("IKubernetesScriptServiceV1Alpha");
+            capabilities.Count.Should().Be(2);
+
+
+            capabilities.Should().NotContainMatch("IScriptService*");
         }
     }
 }
