d584b3fbbacaa9efcd8e70f7930cc1eafc1ad891
Alastair Pitts <alastair.pitts@octopus.com>
2024-03-26 11:37:13 +1100

    Log?

--- source/Octopus.Tentacle/Util/PlatformDetection.cs
+++ source/Octopus.Tentacle/Util/PlatformDetection.cs
@@ -15,7 +15,16 @@ namespace Octopus.Tentacle.Util
             /// <summary>
             /// Indicates if the Tentacle is running inside a Kubernetes cluster as the Kubernetes Agent. This is done by checking if the namespace environment variable is set
             /// </summary>
-            public static bool IsRunningAsKubernetesAgent => !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(KubernetesConfig.NamespaceVariableName));
+            public static bool IsRunningAsKubernetesAgent
+            {
+                get
+                {
+                    var envVar = Environment.GetEnvironmentVariable(KubernetesConfig.NamespaceVariableName);
+                    Console.Error.WriteLine($"{KubernetesConfig.NamespaceVariableName}: '{envVar}'");
+
+                    return !string.IsNullOrWhiteSpace(envVar);
+                }
+            }
         }
     }
 }
