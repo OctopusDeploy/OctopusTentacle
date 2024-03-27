c54fa2be7253fd9697cf3909f0eafda32d704ef6
Rhys Parry <rhys.parry@octopus.com>
2024-03-27 15:13:07 +1000

    fix: simplify configure-and-run.sh

--- docker/linux/scripts/configure-and-run.sh
+++ docker/linux/scripts/configure-and-run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-set -u
+set -eu
 
 /scripts/configure-tentacle.sh
-CONFIGURE_RESULT=$?
-if [ "$CONFIGURE_RESULT" != "0" ]; then
-  exit $CONFIGURE_RESULT
-fi
 
 exec /scripts/run-tentacle.sh
