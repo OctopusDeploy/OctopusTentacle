f38c65585081b6e89768b89de2dc0a4b8be3c759
Rhys Parry <rhys.parry@octopus.com>
2024-03-27 13:40:34 +1000

    fix: ensure docker and tentacle terminate together

--- docker/linux/scripts/run-tentacle.sh
+++ docker/linux/scripts/run-tentacle.sh
@@ -1,35 +1,59 @@
 #!/bin/bash
-set -eux
+set -ux
 
 if [[ "$DISABLE_DIND" == "Y" ]]; then
     echo Docker-in-Docker is disabled.
-    DIND_PID=""
-else
-    echo "Starting Docker-in-Docker daemon. This requires that this container be run in privileged mode."
-    nohup /usr/local/bin/dockerd-entrypoint.sh dockerd &
-    DIND_PID=$!
+    echo "Starting Tentacle"
+    exec tentacle agent --instance Tentacle --noninteractive
 fi
 
-function handle_interrupt() {
-    echo "Received SIGINT. Forwarding..."
-    kill -INT $TENTACLE_PID $DIND_PID
-}
+echo "Starting Docker-in-Docker daemon. This requires that this container be run in privileged mode."
+nohup /usr/local/bin/dockerd-entrypoint.sh dockerd &
+DIND_PID=$!
 
-function handle_term() {
-    echo "Received SIGTERM. Forwarding..."
-    kill -TERM $TENTACLE_PID $DIND_PID
-}
-
-function handle_kill() {
-    echo "Received SIGKILL. Forwarding..."
-    kill -KILL $TENTACLE_PID $DIND_PID
-}
-
+echo "Starting Tentacle"
 tentacle agent --instance Tentacle --noninteractive &
 TENTACLE_PID=$!
 
+EXTERNAL_SIGNAL=""
+
+function handle_interrupt() {
+    EXTERNAL_SIGNAL="INT"
+    echo "Received SIGINT. Forwarding..."
+    kill -s INT $TENTACLE_PID $DIND_PID
+}
+
+function handle_term() {
+    EXTERNAL_SIGNAL="TERM"
+    echo "Received SIGTERM. Forwarding..."
+    kill -s TERM $TENTACLE_PID $DIND_PID
+}
+
 trap handle_interrupt SIGINT
 trap handle_term SIGTERM
-trap handle_kill SIGKILL
 
-wait
+exited_process=0
+
+wait -n -p exited_process $TENTACLE_PID $DIND_PID
+waited_exit_code=$?
+if [ -n "$EXTERNAL_SIGNAL" ]; then
+    echo "Exiting due to external signal: $EXTERNAL_SIGNAL"
+    wait
+    tentacle_exit_code=$waited_exit_code
+else
+    if [ "$exited_process" == "$DIND_PID" ]; then
+        echo "Docker-in-Docker Daemon exited with code $waited_exit_code"
+        echo "Terminating Tentacle..."
+        kill -s INT $TENTACLE_PID
+        wait $TENTACLE_PID
+        tentacle_exit_code=$?
+    else
+        echo "Tentacle exited with code $waited_exit_code"
+        echo "Terminating Docker-in-Docker..."
+        kill -s INT $DIND_PID
+        wait $DIND_PID
+        tentacle_exit_code=$waited_exit_code
+    fi
+fi
+
+exit $tentacle_exit_code
