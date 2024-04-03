2bab67c23fe13c0c5844da05d7565eed4c48e3fc
Rhys Parry <rhys.parry@octopus.com>
2024-04-03 11:30:54 +1000

    Trap SIGTERM and send to tentacle (#856)
    Resolves #865

--- docker/linux/scripts/run-tentacle.sh
+++ docker/linux/scripts/run-tentacle.sh
@@ -1,11 +1,52 @@
 #!/bin/bash
-set -eux
+set -ux
 
 if [[ "$DISABLE_DIND" == "Y" ]]; then
     echo Docker-in-Docker is disabled.
-else
-    echo "Starting Docker-in-Docker daemon. This requires that this container be run in privileged mode."
-    nohup /usr/local/bin/dockerd-entrypoint.sh dockerd &
+    echo "Starting Tentacle"
+    exec tentacle agent --instance Tentacle --noninteractive
 fi
 
-tentacle agent --instance Tentacle --noninteractive
+echo "Starting Docker-in-Docker daemon. This requires that this container be run in privileged mode."
+nohup /usr/local/bin/dockerd-entrypoint.sh dockerd &
+DIND_PID=$!
+
+echo "Starting Tentacle"
+tentacle agent --instance Tentacle --noninteractive &
+TENTACLE_PID=$!
+
+EXTERNAL_SIGNAL=""
+
+function handle_term() {
+    EXTERNAL_SIGNAL="TERM"
+    echo "Received SIGTERM. Forwarding..."
+    kill -s TERM $TENTACLE_PID $DIND_PID
+}
+
+trap handle_term SIGTERM
+
+exited_process=0
+
+wait -n -p exited_process $TENTACLE_PID $DIND_PID
+waited_exit_code=$?
+if [ -n "$EXTERNAL_SIGNAL" ]; then
+    echo "Exiting due to external signal: $EXTERNAL_SIGNAL"
+    wait
+    tentacle_exit_code=$waited_exit_code
+else
+    if [ "$exited_process" == "$DIND_PID" ]; then
+        echo "Docker-in-Docker Daemon exited with code $waited_exit_code"
+        echo "Terminating Tentacle..."
+        kill -s TERM $TENTACLE_PID
+        wait $TENTACLE_PID
+        tentacle_exit_code=$?
+    else
+        echo "Tentacle exited with code $waited_exit_code"
+        echo "Terminating Docker-in-Docker..."
+        kill -s TERM $DIND_PID
+        wait $DIND_PID
+        tentacle_exit_code=$waited_exit_code
+    fi
+fi
+
+exit $tentacle_exit_code
