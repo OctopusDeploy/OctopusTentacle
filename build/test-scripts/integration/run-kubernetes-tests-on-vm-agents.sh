45fabc916588a02032d94684757ae0b567e0af96
Alastair Pitts <alastair.pitts@octopus.com>
2024-04-10 12:39:00 +1000

    Only build Kubernetes Tests in .NET 6

---
+++ build/test-scripts/integration/run-kubernetes-tests-on-vm-agents.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o xtrace
+set -e
+
+# Runs integration tests on VM backed agents.
+# Currently VM agents don't supply sudo, so tests that need that are not run.
+
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+$SCRIPT_DIR/common/setup-vm-agent.sh
+
+source $SCRIPT_DIR/common/set-dotnet-envvars.sh
+
+itdll=`pwd`/build/outputs/integrationtests/net6.0/linux-x64/Octopus.Tentacle.Kubernetes.Tests.Integration.dll
+
+
+# We don't care about the exit code of dotnet test and instead depend on tests passing.
+set +e
+if [ "$TENTACLE_IT_WITH_SUDO" = "1" ]; then
+sudo -E env PATH=$PATH dotnet vstest $itdll "/testcasefilter:TestCategory!=TentacleBackwardsCompatibility" /logger:logger://teamcity /TestAdapterPath:/opt/TeamCity/BuildAgent/plugins/dotnet/tools/vstest15 /logger:console;verbosity=detailed
+else
+dotnet vstest $itdll "/testcasefilter:TestCategory!=RequiresSudoOnLinux&TestCategory!=TentacleBackwardsCompatibility" /logger:logger://teamcity /TestAdapterPath:/opt/TeamCity/BuildAgent/plugins/dotnet/tools/vstest15 /logger:console;verbosity=detailed
+fi
+
