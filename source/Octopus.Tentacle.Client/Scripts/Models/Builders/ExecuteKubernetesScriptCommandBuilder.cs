84bbfcda7ee58e5195a969ab4a1be1b2ebb5ff17
Alastair Pitts <alastair.pitts@octopus.com>
2024-04-12 10:07:32 +1000

    Add ability to set Kubernetes script pod service account

--- source/Octopus.Tentacle.Client/Scripts/Models/Builders/ExecuteKubernetesScriptCommandBuilder.cs
+++ source/Octopus.Tentacle.Client/Scripts/Models/Builders/ExecuteKubernetesScriptCommandBuilder.cs
@@ -6,15 +6,22 @@ namespace Octopus.Tentacle.Client.Scripts.Models.Builders
     public class ExecuteKubernetesScriptCommandBuilder : ExecuteScriptCommandBuilder
     {
         KubernetesImageConfiguration? configuration;
+        string? scriptPodServiceAccountName;
 
         public ExecuteKubernetesScriptCommandBuilder(string taskId)
             : base(taskId, ScriptIsolationLevel.NoIsolation) //Kubernetes Agents don't need isolation since the scripts won't clash with each other (it won't clash more than Workers anyway)
         {
         }
 
-        public ExecuteKubernetesScriptCommandBuilder SetKubernetesImageConfiguration(KubernetesImageConfiguration configuration)
+        public ExecuteKubernetesScriptCommandBuilder WithKubernetesImageConfiguration(KubernetesImageConfiguration configuration)
         {
             this.configuration = configuration;
+            return this;
+        }
+
+        public ExecuteKubernetesScriptCommandBuilder WithScriptPodServiceAccountName(string serviceAccountName)
+        {
+            scriptPodServiceAccountName = serviceAccountName;
             return this;
         }
 
@@ -27,7 +34,8 @@ namespace Octopus.Tentacle.Client.Scripts.Models.Builders
                 IsolationConfiguration,
                 AdditionalScripts,
                 Files.ToArray(),
-                configuration
+                configuration,
+                scriptPodServiceAccountName
             );
     }
 }
