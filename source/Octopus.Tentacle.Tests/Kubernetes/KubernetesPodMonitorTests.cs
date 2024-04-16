887c5506fe704acd450a9b180b790ee8ddb3e666
Frank Lin <frank.lin@octopus.com>
2024-04-17 09:50:54 +1000

    Fix bug where Deployment hangs if the Script Pod is deleted (#886)

--- source/Octopus.Tentacle.Tests/Kubernetes/KubernetesPodMonitorTests.cs
+++ source/Octopus.Tentacle.Tests/Kubernetes/KubernetesPodMonitorTests.cs
@@ -49,6 +49,13 @@ namespace Octopus.Tentacle.Tests.Kubernetes
                 }
             };
 
+            podService.ListAllPods(Arg.Any<CancellationToken>())
+                .Returns(new V1PodList
+                {
+                    Items = new List<V1Pod>()
+                });
+            await monitor.InitialLoadAsync(CancellationToken.None);
+
             //Act
             await monitor.OnNewEvent(type, pod, CancellationToken.None);
 
@@ -58,8 +65,8 @@ namespace Octopus.Tentacle.Tests.Kubernetes
 
             status.Should().Match<TrackedScriptPod>(status =>
                 status.ScriptTicket == scriptTicket &&
-                status.State == TrackedScriptPodState.Running &&
-                status.ExitCode == null
+                status.State.Phase == TrackedScriptPodPhase.Running &&
+                status.State.ExitCode == null
             );
         }
 
@@ -114,8 +121,8 @@ namespace Octopus.Tentacle.Tests.Kubernetes
 
             status.Should().Match<TrackedScriptPod>(status =>
                 status.ScriptTicket == scriptTicket &&
-                status.State == TrackedScriptPodState.Succeeded &&
-                status.ExitCode == 0
+                status.State.Phase == TrackedScriptPodPhase.Succeeded &&
+                status.State.ExitCode == 0
             );
         }
 
@@ -177,8 +184,8 @@ namespace Octopus.Tentacle.Tests.Kubernetes
 
             status.Should().Match<TrackedScriptPod>(status =>
                 status.ScriptTicket == scriptTicket &&
-                status.State == TrackedScriptPodState.Failed &&
-                status.ExitCode == -99
+                status.State.Phase == TrackedScriptPodPhase.Failed &&
+                status.State.ExitCode == -99
             );
         }
 
