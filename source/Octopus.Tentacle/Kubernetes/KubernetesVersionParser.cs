f18a5aef01c39643363064c7808925eed4c989d9
Travis Leeden <travis.leeden@octopus.com>
2024-04-17 14:36:50 +1000

    Sanitize Kubernetes cluster version (#892)
    * Added KubernetesVersionParser and tests
    
    * Remove int try parse and just always santize
    
    * Remove unnecessary variable
    
    * Actually remove the unnecessary variable

---
+++ source/Octopus.Tentacle/Kubernetes/KubernetesVersionParser.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text.RegularExpressions;
+using k8s.Models;
+
+namespace Octopus.Tentacle.Kubernetes
+{
+    public static class KubernetesVersionParser
+    {
+        public static ClusterVersion ParseClusterVersion(VersionInfo versionInfo)
+        {
+            return new ClusterVersion(SanitizeAndParseVersionNumber(versionInfo.Major), SanitizeAndParseVersionNumber(versionInfo.Minor));
+        }
+        
+        static int SanitizeAndParseVersionNumber(string version)
+        {
+            return int.Parse(Regex.Replace(version, "[^0-9]", ""));
+        }
+
+    }
+    public record ClusterVersion(int Major, int Minor);
+}
