5f67ca4b91d120c234f9fb12ec7d7ea4e96c1b44
Frank Lin <frank.lin@octopus.com>
2024-04-23 15:49:37 +1000

    Add retry to secret reading

--- source/Octopus.Tentacle/Kubernetes/KubernetesSecretService.cs
+++ source/Octopus.Tentacle/Kubernetes/KubernetesSecretService.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using k8s;
 using k8s.Autorest;
 using k8s.Models;
+using Octopus.Diagnostics;
+using Octopus.Tentacle.Time;
+using Polly;
+using Polly.Retry;
 
 namespace Octopus.Tentacle.Kubernetes
 {
@@ -18,22 +22,34 @@ namespace Octopus.Tentacle.Kubernetes
 
     public class KubernetesSecretService : KubernetesService, IKubernetesSecretService
     {
-        public KubernetesSecretService(IKubernetesClientConfigProvider configProvider)
+        readonly AsyncRetryPolicy getSecretRetryPolicy;
+        const int MaxDurationSeconds = 30;
+
+        public KubernetesSecretService(IKubernetesClientConfigProvider configProvider, ISystemLog log)
             : base(configProvider)
         {
+            getSecretRetryPolicy = Policy.Handle<Exception>().WaitAndRetryAsync(5,
+                retry => TimeSpan.FromSeconds(ExponentialBackoff.GetDuration(retry, MaxDurationSeconds)),
+                (ex, duration) =>
+                {
+                    log.Verbose(ex, "An unexpected error occured while querying Kubernetes secret, waiting for: " + duration);
+                });
         }
 
         public async Task<V1Secret?> TryGetSecretAsync(string name, CancellationToken cancellationToken)
         {
-            try
+            return await getSecretRetryPolicy.ExecuteAsync(async () =>
             {
-                return await Client.ReadNamespacedSecretAsync(name, KubernetesConfig.Namespace, cancellationToken: cancellationToken);
-            }
-            catch (HttpOperationException opException)
-                when (opException.Response.StatusCode == HttpStatusCode.NotFound)
-            {
-                return null;
-            }
+                try
+                {
+                    return await Client.ReadNamespacedSecretAsync(name, KubernetesConfig.Namespace, cancellationToken: cancellationToken);
+                }
+                catch (HttpOperationException opException)
+                    when (opException.Response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+            });
         }
 
         public async Task CreateSecretAsync(V1Secret secret, CancellationToken cancellationToken)
