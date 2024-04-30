92ddb793871ee81b94b88066af1a95819bc84f5d
Frank Lin <frank.lin@octopus.com>
2024-04-30 22:14:53 +1000

    try disable du

--- source/Octopus.Tentacle.Tests/Kubernetes/KubernetesPhysicalFileSystemFixture.cs
+++ source/Octopus.Tentacle.Tests/Kubernetes/KubernetesPhysicalFileSystemFixture.cs
@@ -9,39 +9,39 @@ using Octopus.Tentacle.Kubernetes;
 
 namespace Octopus.Tentacle.Tests.Kubernetes
 {
-    [TestFixture]
-    public class KubernetesPhysicalFileSystemFixture
-    {
-        const ulong Megabyte = 1000 * 1000;
-        const ulong Mebibyte = 1024 * 1024;
-        const ulong Gibibyte = 1024 * 1024 * 1024;
-
-        [TestCase(100 * Mebibyte, 300 * Mebibyte, true)]
-        [TestCase(100 * Mebibyte, 800 * Mebibyte, false)]
-        [TestCase(800 * Megabyte, 1 * Gibibyte, true)]
-        public void DiskSpaceUsed(ulong diskSpaceUsed, ulong totalDiskSpace, bool throwException)
-        {
-            const string directoryPath = "/octopus";
-            
-            var directoryInformationProvider = Substitute.For<IKubernetesDirectoryInformationProvider>();
-            directoryInformationProvider.GetPathTotalBytes().Returns(totalDiskSpace);
-            directoryInformationProvider.GetPathUsedBytes("/octopus").Returns(diskSpaceUsed);
-            
-            var homeConfiguration = Substitute.For<IHomeConfiguration>();
-            homeConfiguration.HomeDirectory.Returns("/octopus");
-            
-            var fileSystem = new KubernetesPhysicalFileSystem(directoryInformationProvider, Substitute.For<ISystemLog>());
-            
-            if (throwException)
-            {
-                Action a = () => fileSystem.EnsureDiskHasEnoughFreeSpace(directoryPath);
-                a.Should().Throw<IOException>();
-            }
-            else
-            {
-                Action a = () => fileSystem.EnsureDiskHasEnoughFreeSpace(directoryPath);
-                a.Should().NotThrow();
-            }
-        }
-    }
+    // [TestFixture]
+    // public class KubernetesPhysicalFileSystemFixture
+    // {
+    //     const ulong Megabyte = 1000 * 1000;
+    //     const ulong Mebibyte = 1024 * 1024;
+    //     const ulong Gibibyte = 1024 * 1024 * 1024;
+    //
+    //     [TestCase(100 * Mebibyte, 300 * Mebibyte, true)]
+    //     [TestCase(100 * Mebibyte, 800 * Mebibyte, false)]
+    //     [TestCase(800 * Megabyte, 1 * Gibibyte, true)]
+    //     public void DiskSpaceUsed(ulong diskSpaceUsed, ulong totalDiskSpace, bool throwException)
+    //     {
+    //         const string directoryPath = "/octopus";
+    //         
+    //         var directoryInformationProvider = Substitute.For<IKubernetesDirectoryInformationProvider>();
+    //         directoryInformationProvider.GetPathTotalBytes().Returns(totalDiskSpace);
+    //         directoryInformationProvider.GetPathUsedBytes("/octopus").Returns(diskSpaceUsed);
+    //         
+    //         var homeConfiguration = Substitute.For<IHomeConfiguration>();
+    //         homeConfiguration.HomeDirectory.Returns("/octopus");
+    //         
+    //         var fileSystem = new KubernetesPhysicalFileSystem(directoryInformationProvider, Substitute.For<ISystemLog>());
+    //         
+    //         if (throwException)
+    //         {
+    //             Action a = () => fileSystem.EnsureDiskHasEnoughFreeSpace(directoryPath);
+    //             a.Should().Throw<IOException>();
+    //         }
+    //         else
+    //         {
+    //             Action a = () => fileSystem.EnsureDiskHasEnoughFreeSpace(directoryPath);
+    //             a.Should().NotThrow();
+    //         }
+    //     }
+    // }
 }
--- source/Octopus.Tentacle/Kubernetes/KubernetesPhysicalFileSystem.cs
+++ source/Octopus.Tentacle/Kubernetes/KubernetesPhysicalFileSystem.cs
@@ -22,23 +22,23 @@ namespace Octopus.Tentacle.Kubernetes
             Log = log;
         }
 
-        public override void EnsureDiskHasEnoughFreeSpace(string directoryPath, long requiredSpaceInBytes)
-        {
-            var freeBytes = GetPathFreeBytes(HomeDir);
-            Log.Verbose($"Directory to be checked is {HomeDir}, script directory is {directoryPath}, required space is {requiredSpaceInBytes} bytes");
-
-            // If we can't get the free bytes, we just skip the check
-            if (freeBytes is null) return;
-
-            var required = requiredSpaceInBytes < 0 ? 0 : (ulong)requiredSpaceInBytes;
-            // Make sure there is 10% (and a bit extra) more than we need
-            required += required / 10 + 1024 * 1024;
-            Log.Verbose($"Checking free space on disk. Required: {required} bytes, available: {freeBytes} bytes");
-            if (freeBytes < required)
-            {
-                throw new IOException($"Not enough free space on disk. Required: {required} bytes, available: {freeBytes} bytes");
-            }
-        }
+        // public override void EnsureDiskHasEnoughFreeSpace(string directoryPath, long requiredSpaceInBytes)
+        // {
+        //     var freeBytes = GetPathFreeBytes(HomeDir);
+        //     Log.Verbose($"Directory to be checked is {HomeDir}, script directory is {directoryPath}, required space is {requiredSpaceInBytes} bytes");
+        //
+        //     // If we can't get the free bytes, we just skip the check
+        //     if (freeBytes is null) return;
+        //
+        //     var required = requiredSpaceInBytes < 0 ? 0 : (ulong)requiredSpaceInBytes;
+        //     // Make sure there is 10% (and a bit extra) more than we need
+        //     required += required / 10 + 1024 * 1024;
+        //     Log.Verbose($"Checking free space on disk. Required: {required} bytes, available: {freeBytes} bytes");
+        //     if (freeBytes < required)
+        //     {
+        //         throw new IOException($"Not enough free space on disk. Required: {required} bytes, available: {freeBytes} bytes");
+        //     }
+        // }
         
         ulong? GetPathFreeBytes(string directoryPath)           
         {
