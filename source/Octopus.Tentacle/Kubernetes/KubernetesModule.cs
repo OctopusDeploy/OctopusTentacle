c60c0ba0b2e7b8ef047768c924ea9190d7ca275b
Frank Lin <frank.lin@octopus.com>
2024-05-01 13:19:59 +1000

    fix rego

--- source/Octopus.Tentacle/Kubernetes/KubernetesModule.cs
+++ source/Octopus.Tentacle/Kubernetes/KubernetesModule.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 using Octopus.Tentacle.Background;
 
 namespace Octopus.Tentacle.Kubernetes
@@ -26,6 +27,7 @@ namespace Octopus.Tentacle.Kubernetes
             builder.RegisterType<KubernetesOrphanedPodCleaner>().As<IKubernetesOrphanedPodCleaner>().SingleInstance();
             builder.RegisterType<KubernetesDirectoryInformationProvider>().As<IKubernetesDirectoryInformationProvider>().SingleInstance();
             builder.RegisterType<MemoryCache>().As<IMemoryCache>().SingleInstance();
+            builder.RegisterType<MemoryCacheOptions>().As<IOptions<MemoryCacheOptions>>().SingleInstance();
 #if DEBUG
             builder.RegisterType<LocalMachineKubernetesClientConfigProvider>().As<IKubernetesClientConfigProvider>().SingleInstance();
 #else
