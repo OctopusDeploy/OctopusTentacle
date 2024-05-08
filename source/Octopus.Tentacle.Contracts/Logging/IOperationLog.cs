c59e0eabcbe5a17a1a1cacd53311dcf74c67c9c9
Shane <tothegills@gmail.com>
2024-05-09 08:29:32 +1000

    ocd

--- source/Octopus.Tentacle.Contracts/Logging/IOperationLog.cs
+++ source/Octopus.Tentacle.Contracts/Logging/IOperationLog.cs
@@ -5,9 +5,9 @@ namespace Octopus.Tentacle.Contracts.Logging
     public interface IOperationLog
     {
         void Info(string message);
-        void Verbose(Exception exception);
-        void Warn(Exception exception, string message);
-        void Warn(string message);
         void Verbose(string message);
+        void Verbose(Exception exception);
+        void Warn(string message);
+        void Warn(Exception exception, string message);
     }
 }
