944df323fa991ca143eca6a6a59a67cb61b2521e
Samdanae Imran <samdanae.imran@octopus.com>
2024-05-15 13:17:41 +1200

    Append version information to Capabilities Service

--- source/Octopus.Tentacle/Services/Capabilities/CapabilitiesServiceV2.cs
+++ source/Octopus.Tentacle/Services/Capabilities/CapabilitiesServiceV2.cs
@@ -16,25 +16,20 @@ namespace Octopus.Tentacle.Services.Capabilities
     [Service(typeof(ICapabilitiesServiceV2))]
     public class CapabilitiesServiceV2 : IAsyncCapabilitiesServiceV2
     {
-        readonly ISystemLog log;
-        public CapabilitiesServiceV2(ISystemLog log)
-        {
-            this.log = log;
-        }
         public async Task<CapabilitiesResponseV2> GetCapabilitiesAsync(CancellationToken cancellationToken)
         {
             await Task.CompletedTask;
-            
-            log.Verbose($"Tentacle Version: {OctopusTentacle.InformationalVersion}");
-            
+
+            var versionInformation = $"Tentacle_Version_{OctopusTentacle.InformationalVersion}";
+
             //the kubernetes agent only supports the kubernetes script services
             if (PlatformDetection.Kubernetes.IsRunningAsKubernetesAgent)
             {
-                return new CapabilitiesResponseV2(new List<string> { nameof(IFileTransferService), nameof(IKubernetesScriptServiceV1Alpha), nameof(IKubernetesScriptServiceV1)  });
+                return new CapabilitiesResponseV2(new List<string> { nameof(IFileTransferService), nameof(IKubernetesScriptServiceV1Alpha), nameof(IKubernetesScriptServiceV1), versionInformation });
             }
 
             //non-kubernetes agent tentacles only support the standard script services
-            return new CapabilitiesResponseV2(new List<string> { nameof(IScriptService), nameof(IFileTransferService), nameof(IScriptServiceV2) });
+            return new CapabilitiesResponseV2(new List<string> { nameof(IScriptService), nameof(IFileTransferService), nameof(IScriptServiceV2), versionInformation });
         }
     }
 }
