c43607937d35ea6dac7d66391be9dff1c539bcc2
Trent Mohay <trent.mohay@octopus.com>
2024-05-24 17:02:10 +1000

    cleanup

--- source/Octopus.Tentacle/Diagnostics/KubernetesAgentMetrics.cs
+++ source/Octopus.Tentacle/Diagnostics/KubernetesAgentMetrics.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
 using System.Threading;
 using k8s.Models;
-using Octopus.Tentacle.Configuration;
-using Octopus.Tentacle.Configuration.Instances;
 using Octopus.Tentacle.Kubernetes;
 
 namespace Octopus.Tentacle.Diagnostics
@@ -21,59 +17,34 @@ namespace Octopus.Tentacle.Diagnostics
         readonly IKubernetesConfigMapService configMapService;
         const string Name = "kubernetes-agent-metrics";
 
-        readonly Lazy<V1ConfigMap> configMap;
-        IDictionary<string, string> ConfigMapData => configMap.Value.Data ??= new Dictionary<string, string>();
+        readonly Lazy<V1ConfigMap> metricsConfigMap;
+        IDictionary<string, string> ConfigMapData => metricsConfigMap.Value.Data ??= new Dictionary<string, string>();
 
         public KubernetesAgentMetrics(IKubernetesConfigMapService configMapService)
         {
             this.configMapService = configMapService;
-            configMap = new Lazy<V1ConfigMap>(() => configMapService.TryGet(Name, CancellationToken.None).GetAwaiter().GetResult()
+            metricsConfigMap = new Lazy<V1ConfigMap>(() => configMapService.TryGet(Name, CancellationToken.None).GetAwaiter().GetResult()
                 ?? throw new InvalidOperationException($"Unable to retrieve Tentacle Configuration from config map for namespace {KubernetesConfig.Namespace}"));
         }
 
         public void IncrementMetric(KubernetesMetric metric)
         {
             var metricName = Enum.GetName(typeof(KubernetesMetric), metric)!;
-            lock (ConfigMapData)
+            try
             {
-                var initialValue = GetValueFromMap(metricName);
-                ConfigMapData[metricName] = (initialValue + 1).ToString();
-                Persist();
+                lock (ConfigMapData)
+                {
+                    var initialValue = GetValueFromMap(metricName);
+                    ConfigMapData[metricName] = (initialValue + 1).ToString();
+                    Persist();
+                }
+            }
+            catch (Exception)
+            {
+                //no idea how to actually log this exception, nor to where
+                //will land here if the metrics-config-map does not exist.
             }
         }
-
-        // Chances are, this needs to happen in the healthcheck script
-        // public void Reset()
-        // {
-        //     var values = Enum.GetValues(typeof(KubernetesMetric)).Cast<KubernetesMetric>();
-        //     lock (ConfigMapData)
-        //     {
-        //         foreach (var metric in values)
-        //         {
-        //             var metricName = Enum.GetName(typeof(KubernetesMetric), metric)!;
-        //             ConfigMapData[metricName] = "0";
-        //             ConfigMapData[metricName] = "0";
-        //             Persist();
-        //         }
-        //     }
-        // }
-
-        // Chances are, this needs to happen in the healthcheck script
-        // public ImmutableDictionary<string, int> Extract()
-        // {
-        //     var metrics = Enum.GetValues(typeof(KubernetesMetric)).Cast<KubernetesMetric>();
-        //     var result = new Dictionary<string, int>();
-        //     lock (ConfigMapData)
-        //     {
-        //         foreach (var metric in metrics)
-        //         {
-        //             var metricName = Enum.GetName(typeof(KubernetesMetric), metric)!;
-        //             result[metricName] = GetValueFromMap(metricName);
-        //         }
-        //     }
-        //
-        //     return result.ToImmutableDictionary();
-        // }
 
         void Persist()
         {
