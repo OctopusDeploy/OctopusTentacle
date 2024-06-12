fd234bc205c55396e919b3b38dd72cbd1acc4a10
Scott Merchant <scott.merchant@octopus.com>
2024-06-12 15:32:35 +0930

    Remove SharpZipLib references

--- source/Octopus.Tentacle/Kubernetes/KubernetesPodService.cs
+++ source/Octopus.Tentacle/Kubernetes/KubernetesPodService.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using ICSharpCode.SharpZipLib.Tar;
 using k8s;
 using k8s.Models;
 using Octopus.Diagnostics;
