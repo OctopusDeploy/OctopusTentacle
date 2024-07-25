f44bf7be5239aa4bccc80a963a0c587d700e82f2
Trent Mohay <trent.mohay@octopus.com>
2024-07-25 12:11:07 +1000

    cleanup

--- source/Octopus.Tentacle/Kubernetes/KubernetesRawScriptPodCreator.cs
+++ source/Octopus.Tentacle/Kubernetes/KubernetesRawScriptPodCreator.cs
@@ -40,7 +40,7 @@ namespace Octopus.Tentacle.Kubernetes
             {
                 Name = $"{podName}-init",
                 Image = command.PodImageConfiguration?.Image ?? await containerResolver.GetContainerImageForCluster(),
-                ImagePullPolicy = KubernetesConfig.ScriptPodPullPolicy.IsNullOrEmpty() ? "" : KubernetesConfig.ScriptPodPullPolicy,
+                ImagePullPolicy = KubernetesConfig.ScriptPodPullPolicy.IsNullOrEmpty() ? null : KubernetesConfig.ScriptPodPullPolicy,
                 Command = new List<string> { "sh", "-c", GetInitExecutionScript("/nfs-mount", homeDir, workspacePath) },
                 VolumeMounts = new List<V1VolumeMount> { new("/nfs-mount", "init-nfs-volume"), new(homeDir, "tentacle-home") },
                 Resources = new V1ResourceRequirements
