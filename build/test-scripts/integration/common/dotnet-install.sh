7baa231472532fcf5179389975cbecedfc611fdf
Rhys Parry <rhys.parry@octopus.com>
2024-08-05 15:47:41 +1000

    This time for sure

--- build/test-scripts/integration/common/dotnet-install.sh
+++ build/test-scripts/integration/common/dotnet-install.sh
@@ -24,9 +24,6 @@ function install_dotnet {
         shift
     done
 
-    echo "Installing dotnet $1"
-    ./dotnet-install.sh  --channel "$1" --verbose || exit 1
-
     echo "Removing dotnet-install.sh script"
     rm -f dotnet-install.sh || exit 1
 }
