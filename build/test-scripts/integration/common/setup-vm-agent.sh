76c2e92bb8c6a653ba0d8d1988c41c45a83981bd
Rhys Parry <rhys.parry@octopus.com>
2024-08-14 10:03:25 +1000

    Rename TARGET_RUNTIME to TARGET_FRAMEWORK

--- build/test-scripts/integration/common/setup-vm-agent.sh
+++ build/test-scripts/integration/common/setup-vm-agent.sh
@@ -6,14 +6,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source $SCRIPT_DIR/dotnet-install.sh
 
-if [ "${TARGET_RUNTIME:-net6.0}" == "net6.0" ]; then
+if [ "${TARGET_FRAMEWORK:-net6.0}" == "net6.0" ]; then
     # We install the 8.0 sdk and also the 6.0 runtime as the tests require it
     install_dotnet --runtime 6.0 --sdk 6.0
-elif [ "${TARGET_RUNTIME:-net6.0}" == "net8.0" ]; then
+elif [ "${TARGET_FRAMEWORK:-net6.0}" == "net8.0" ]; then
     # Only the 8.0 sdk is required
     install_dotnet --sdk 8.0
 else
-    echo "WARNING: Unknown TARGET_RUNTIME: ${TARGET_RUNTIME:-net6.0}" 1>&2
+    echo "WARNING: Unknown TARGET_FRAMEWORK: ${TARGET_FRAMEWORK:-net6.0}" 1>&2
     # We install the 8.0 sdk and also the 6.0 runtime as the tests may require it
     install_dotnet --runtime 6.0 --sdk 8.0
 fi
