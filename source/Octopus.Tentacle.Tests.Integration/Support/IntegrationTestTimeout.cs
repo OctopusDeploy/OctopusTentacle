490d33251646a2ad87665bcc0683b5faf5e1e0ff
michaelongso <michael.ongso@octopus.com>
2024-08-29 09:48:50 +1000

    add configurable integration tests timeout to be used with windows2012.

--- source/Octopus.Tentacle.Tests.Integration/Support/IntegrationTestTimeout.cs
+++ source/Octopus.Tentacle.Tests.Integration/Support/IntegrationTestTimeout.cs
@@ -21,7 +21,23 @@ namespace Octopus.Tentacle.Tests.Integration.Support
                 return (int)TimeSpan.FromHours(1).TotalMilliseconds;
             }
 
+            var windows2012Timeout = GetWindows2012Timeout();
+            if (windows2012Timeout is not null)
+            {
+                return windows2012Timeout.Value;
+            }
+
             return (int)TimeSpan.FromMinutes(2).TotalMilliseconds;
+        }
+
+        static int? GetWindows2012Timeout()
+        {
+            if (int.TryParse(Environment.GetEnvironmentVariable("Windows2012_IntegrationTests_Timeout_Minute"), out var windows2012Timeout))
+            {
+                return (int)TimeSpan.FromMinutes(windows2012Timeout).TotalMilliseconds;
+            }
+
+            return null;
         }
     }
 }
