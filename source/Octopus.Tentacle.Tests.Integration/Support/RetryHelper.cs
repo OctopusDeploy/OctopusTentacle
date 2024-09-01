c5d4762b49910a5d56283ccbeeb0c56e78239bf6
michaelongso <michael.ongso@octopus.com>
2024-09-01 14:57:18 +1000

    Revert "use silent process runner"
    This reverts commit 2a58ee994caccaecd8e507c1a64191b1bca69d44.

--- source/Octopus.Tentacle.Tests.Integration/Support/RetryHelper.cs
+++ source/Octopus.Tentacle.Tests.Integration/Support/RetryHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Polly;
-using Serilog;
 
 namespace Octopus.Tentacle.Tests.Integration.Support
 {
@@ -13,20 +12,6 @@ namespace Octopus.Tentacle.Tests.Integration.Support
             return await Policy<TResult>
                 .Handle<TException>()
                 .WaitAndRetryAsync(retryCount, rc => TimeSpan.FromMilliseconds(retryBackoffDurationMilliseconds * rc))
-                .ExecuteAsync(command);
-        }
-        
-        public static async Task<TResult> RetryAsync<TResult, TException>(
-            Func<Task<TResult>> command, ILogger logger, int retryCount = 10, int retryBackoffDurationMilliseconds = 100)
-            where TException : Exception
-        {
-            return await Policy<TResult>
-                .Handle<TException>()
-                .WaitAndRetryAsync(retryCount, rc =>
-                {
-                    logger.Information("Retrying command at {Time}", DateTimeOffset.UtcNow);
-                    return TimeSpan.FromMilliseconds(retryBackoffDurationMilliseconds * rc);
-                })
                 .ExecuteAsync(command);
         }
     }
