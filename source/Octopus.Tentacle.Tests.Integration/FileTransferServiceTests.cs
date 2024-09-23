82e8c2084be69c11ebde8421853ce94412cdb1b3
Samdanae Imran <samdanae.imran@octopus.com>
2024-09-23 19:05:43 +1200

    Comment out everything except ListInstances

--- source/Octopus.Tentacle.Tests.Integration/FileTransferServiceTests.cs
+++ source/Octopus.Tentacle.Tests.Integration/FileTransferServiceTests.cs
@@ -1,61 +1,61 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Halibut;
-using NUnit.Framework;
-using Octopus.Tentacle.CommonTestUtils;
-using Octopus.Tentacle.Tests.Integration.Support;
-using Octopus.Tentacle.Tests.Integration.Util;
-
-namespace Octopus.Tentacle.Tests.Integration
-{
-    [IntegrationTestTimeout]
-    public class FileTransferServiceTests : IntegrationTest
-    {
-        [Test]
-        [TentacleConfigurations(scriptServiceToTest: ScriptServiceVersionToTest.None)]
-        public async Task UploadFileSuccessfully(TentacleConfigurationTestCase tentacleConfigurationTestCase)
-        {
-            using var fileToUpload = new RandomTemporaryFileBuilder().Build();
-
-            await using var clientAndTentacle = await tentacleConfigurationTestCase.CreateLegacyBuilder().Build(CancellationToken);
-
-            var dataStream = new DataStream(
-                fileToUpload.File.Length,
-                async (stream, ct) =>
-                {
-                    using var fileStream = File.OpenRead(fileToUpload.File.FullName);
-                    await fileStream.CopyToAsync(stream);
-                });
-
-            var uploadResult = await clientAndTentacle.TentacleClient.FileTransferService.UploadFileAsync("the_remote_uploaded_file", dataStream, new(CancellationToken));
-
-            Console.WriteLine($"Source: {fileToUpload.File.FullName}");
-            Console.WriteLine($"Destination: {uploadResult.FullPath}");
-
-            var sourceBytes = File.ReadAllBytes(fileToUpload.File.FullName);
-            var destinationBytes = File.ReadAllBytes(uploadResult.FullPath);
-
-            sourceBytes.Should().BeEquivalentTo(destinationBytes);
-        }
-
-        [Test]
-        [TentacleConfigurations(scriptServiceToTest: ScriptServiceVersionToTest.None)]
-        public async Task DownloadFileSuccessfully(TentacleConfigurationTestCase tentacleConfigurationTestCase)
-        {
-            using var fileToDownload = new RandomTemporaryFileBuilder().Build();
-
-            await using var clientAndTentacle = await tentacleConfigurationTestCase.CreateLegacyBuilder().Build(CancellationToken);
-
-            var downloadedData = await clientAndTentacle.TentacleClient.FileTransferService.DownloadFileAsync(
-                fileToDownload.File.FullName,
-                new(CancellationToken));
-
-            var sourceBytes = File.ReadAllBytes(fileToDownload.File.FullName);
-            var destinationBytes = await downloadedData.ToBytes(CancellationToken);
-
-            destinationBytes.Should().BeEquivalentTo(sourceBytes);
-        }
-    }
-}
+﻿// using System;
+// using System.IO;
+// using System.Threading.Tasks;
+// using FluentAssertions;
+// using Halibut;
+// using NUnit.Framework;
+// using Octopus.Tentacle.CommonTestUtils;
+// using Octopus.Tentacle.Tests.Integration.Support;
+// using Octopus.Tentacle.Tests.Integration.Util;
+//
+// namespace Octopus.Tentacle.Tests.Integration
+// {
+//     [IntegrationTestTimeout]
+//     public class FileTransferServiceTests : IntegrationTest
+//     {
+//         [Test]
+//         [TentacleConfigurations(scriptServiceToTest: ScriptServiceVersionToTest.None)]
+//         public async Task UploadFileSuccessfully(TentacleConfigurationTestCase tentacleConfigurationTestCase)
+//         {
+//             using var fileToUpload = new RandomTemporaryFileBuilder().Build();
+//
+//             await using var clientAndTentacle = await tentacleConfigurationTestCase.CreateLegacyBuilder().Build(CancellationToken);
+//
+//             var dataStream = new DataStream(
+//                 fileToUpload.File.Length,
+//                 async (stream, ct) =>
+//                 {
+//                     using var fileStream = File.OpenRead(fileToUpload.File.FullName);
+//                     await fileStream.CopyToAsync(stream);
+//                 });
+//
+//             var uploadResult = await clientAndTentacle.TentacleClient.FileTransferService.UploadFileAsync("the_remote_uploaded_file", dataStream, new(CancellationToken));
+//
+//             Console.WriteLine($"Source: {fileToUpload.File.FullName}");
+//             Console.WriteLine($"Destination: {uploadResult.FullPath}");
+//
+//             var sourceBytes = File.ReadAllBytes(fileToUpload.File.FullName);
+//             var destinationBytes = File.ReadAllBytes(uploadResult.FullPath);
+//
+//             sourceBytes.Should().BeEquivalentTo(destinationBytes);
+//         }
+//
+//         [Test]
+//         [TentacleConfigurations(scriptServiceToTest: ScriptServiceVersionToTest.None)]
+//         public async Task DownloadFileSuccessfully(TentacleConfigurationTestCase tentacleConfigurationTestCase)
+//         {
+//             using var fileToDownload = new RandomTemporaryFileBuilder().Build();
+//
+//             await using var clientAndTentacle = await tentacleConfigurationTestCase.CreateLegacyBuilder().Build(CancellationToken);
+//
+//             var downloadedData = await clientAndTentacle.TentacleClient.FileTransferService.DownloadFileAsync(
+//                 fileToDownload.File.FullName,
+//                 new(CancellationToken));
+//
+//             var sourceBytes = File.ReadAllBytes(fileToDownload.File.FullName);
+//             var destinationBytes = await downloadedData.ToBytes(CancellationToken);
+//
+//             destinationBytes.Should().BeEquivalentTo(sourceBytes);
+//         }
+//     }
+// }
--- source/Octopus.Tentacle.Tests.Integration/Support/TestDecoratorsAreCalledInTheCorrectOrder.cs
+++ source/Octopus.Tentacle.Tests.Integration/Support/TestDecoratorsAreCalledInTheCorrectOrder.cs
@@ -1,114 +1,114 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Halibut.ServiceModel;
-using NUnit.Framework;
-using Octopus.Tentacle.Contracts;
-using Octopus.Tentacle.Contracts.ClientServices;
-using Octopus.Tentacle.Contracts.ScriptServiceV2;
-using Octopus.Tentacle.Tests.Integration.Common.Builders.Decorators;
-using Octopus.Tentacle.Tests.Integration.Common.Builders.Decorators.Proxies;
-using Octopus.Tentacle.Tests.Integration.Util.Builders.Decorators;
-
-namespace Octopus.Tentacle.Tests.Integration.Support
-{
-    public class TestDecoratorsAreCalledInTheCorrectOrder
-    {
-        [Test]
-        public async Task MethodDecoratorsAreCalledInTheOrderTheyAreDefined()
-        {
-            var list = new List<int>();
-            var decorator = new TentacleServiceDecoratorBuilder()
-                .DecorateScriptServiceV2With(b => b.BeforeCompleteScript(async (_, _, _) =>
-                {
-                    list.Add(1);
-                    await Task.CompletedTask;
-                }))
-                .DecorateScriptServiceV2With(b => b.BeforeCompleteScript(async (_, _, _) =>
-                {
-                    list.Add(2);
-                    await Task.CompletedTask;
-                }))
-                .DecorateScriptServiceV2With(b => b.DecorateCompleteScriptWith(async (inner, command, options) =>
-                {
-                    await Task.CompletedTask;
-                    await inner.CompleteScriptAsync(command, options);
-                    list.Add(3);
-                }))
-                .DecorateScriptServiceV2With(b => b.DecorateCompleteScriptWith(async (inner, command, options) =>
-                {
-                    await Task.CompletedTask;
-                    await inner.CompleteScriptAsync(command, options);
-                    list.Add(4);
-                }))
-                .Build();
-
-            await decorator.Decorate(new NoOPClientScriptService()).CompleteScriptAsync(SomeCompleteScriptCommandV2(), SomeHalibutProxyRequestOptions());
-
-            list.Should().BeEquivalentTo(new List<int> {1, 2, 3, 4});
-        }
-
-        [Test]
-        public void ProxyGeneratedDecoratorsAreFirst()
-        {
-            var list = new List<int>();
-            IRecordedMethodUsages recordedUsages = new MethodUsages();
-            long startedCountInCall = 0;
-            var decorator = new TentacleServiceDecoratorBuilder()
-                .DecorateScriptServiceV2With(b => b.BeforeCompleteScript(async (_, _, _) =>
-                    {
-                        await Task.CompletedTask;
-                        // We should find that the proxy decorator has already counted this call.
-                        startedCountInCall = recordedUsages.ForAll().Started;
-                        // If the proxy decorator after this then throwing an exception here would result in calls not being counted.
-                        throw new Exception();
-                    })
-                    .Build())
-                .RecordMethodUsages<IAsyncClientScriptServiceV2>(out recordedUsages)
-                .Build();
-
-            Assert.ThrowsAsync<Exception>(async () => await decorator.Decorate(new NoOPClientScriptService()).CompleteScriptAsync(SomeCompleteScriptCommandV2(), SomeHalibutProxyRequestOptions()));
-
-            var usage = recordedUsages.ForAll();
-
-            usage.LastException.Should().NotBeNull();
-            usage.Started.Should().Be(1);
-            startedCountInCall.Should().Be(1, "Because the proxy decorator which count calls should happen before any method specific decorators.");
-        }
-
-        static CompleteScriptCommandV2 SomeCompleteScriptCommandV2()
-        {
-            return new CompleteScriptCommandV2(new ScriptTicket("a"));
-        }
-
-        static HalibutProxyRequestOptions SomeHalibutProxyRequestOptions()
-        {
-            return new HalibutProxyRequestOptions(CancellationToken.None);
-        }
-
-        class NoOPClientScriptService : IAsyncClientScriptServiceV2
-        {
-            public Task<ScriptStatusResponseV2> StartScriptAsync(StartScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions)
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task<ScriptStatusResponseV2> GetStatusAsync(ScriptStatusRequestV2 request, HalibutProxyRequestOptions proxyRequestOptions)
-            {
-                throw new NotImplementedException();
-            }
-
-            public Task<ScriptStatusResponseV2> CancelScriptAsync(CancelScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions)
-            {
-                throw new NotImplementedException();
-            }
-
-            public async Task CompleteScriptAsync(CompleteScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions)
-            {
-                await Task.CompletedTask;
-            }
-        }
-    }
-}
+// using System;
+// using System.Collections.Generic;
+// using System.Threading;
+// using System.Threading.Tasks;
+// using FluentAssertions;
+// using Halibut.ServiceModel;
+// using NUnit.Framework;
+// using Octopus.Tentacle.Contracts;
+// using Octopus.Tentacle.Contracts.ClientServices;
+// using Octopus.Tentacle.Contracts.ScriptServiceV2;
+// using Octopus.Tentacle.Tests.Integration.Common.Builders.Decorators;
+// using Octopus.Tentacle.Tests.Integration.Common.Builders.Decorators.Proxies;
+// using Octopus.Tentacle.Tests.Integration.Util.Builders.Decorators;
+//
+// namespace Octopus.Tentacle.Tests.Integration.Support
+// {
+//     public class TestDecoratorsAreCalledInTheCorrectOrder
+//     {
+//         [Test]
+//         public async Task MethodDecoratorsAreCalledInTheOrderTheyAreDefined()
+//         {
+//             var list = new List<int>();
+//             var decorator = new TentacleServiceDecoratorBuilder()
+//                 .DecorateScriptServiceV2With(b => b.BeforeCompleteScript(async (_, _, _) =>
+//                 {
+//                     list.Add(1);
+//                     await Task.CompletedTask;
+//                 }))
+//                 .DecorateScriptServiceV2With(b => b.BeforeCompleteScript(async (_, _, _) =>
+//                 {
+//                     list.Add(2);
+//                     await Task.CompletedTask;
+//                 }))
+//                 .DecorateScriptServiceV2With(b => b.DecorateCompleteScriptWith(async (inner, command, options) =>
+//                 {
+//                     await Task.CompletedTask;
+//                     await inner.CompleteScriptAsync(command, options);
+//                     list.Add(3);
+//                 }))
+//                 .DecorateScriptServiceV2With(b => b.DecorateCompleteScriptWith(async (inner, command, options) =>
+//                 {
+//                     await Task.CompletedTask;
+//                     await inner.CompleteScriptAsync(command, options);
+//                     list.Add(4);
+//                 }))
+//                 .Build();
+//
+//             await decorator.Decorate(new NoOPClientScriptService()).CompleteScriptAsync(SomeCompleteScriptCommandV2(), SomeHalibutProxyRequestOptions());
+//
+//             list.Should().BeEquivalentTo(new List<int> {1, 2, 3, 4});
+//         }
+//
+//         [Test]
+//         public void ProxyGeneratedDecoratorsAreFirst()
+//         {
+//             var list = new List<int>();
+//             IRecordedMethodUsages recordedUsages = new MethodUsages();
+//             long startedCountInCall = 0;
+//             var decorator = new TentacleServiceDecoratorBuilder()
+//                 .DecorateScriptServiceV2With(b => b.BeforeCompleteScript(async (_, _, _) =>
+//                     {
+//                         await Task.CompletedTask;
+//                         // We should find that the proxy decorator has already counted this call.
+//                         startedCountInCall = recordedUsages.ForAll().Started;
+//                         // If the proxy decorator after this then throwing an exception here would result in calls not being counted.
+//                         throw new Exception();
+//                     })
+//                     .Build())
+//                 .RecordMethodUsages<IAsyncClientScriptServiceV2>(out recordedUsages)
+//                 .Build();
+//
+//             Assert.ThrowsAsync<Exception>(async () => await decorator.Decorate(new NoOPClientScriptService()).CompleteScriptAsync(SomeCompleteScriptCommandV2(), SomeHalibutProxyRequestOptions()));
+//
+//             var usage = recordedUsages.ForAll();
+//
+//             usage.LastException.Should().NotBeNull();
+//             usage.Started.Should().Be(1);
+//             startedCountInCall.Should().Be(1, "Because the proxy decorator which count calls should happen before any method specific decorators.");
+//         }
+//
+//         static CompleteScriptCommandV2 SomeCompleteScriptCommandV2()
+//         {
+//             return new CompleteScriptCommandV2(new ScriptTicket("a"));
+//         }
+//
+//         static HalibutProxyRequestOptions SomeHalibutProxyRequestOptions()
+//         {
+//             return new HalibutProxyRequestOptions(CancellationToken.None);
+//         }
+//
+//         class NoOPClientScriptService : IAsyncClientScriptServiceV2
+//         {
+//             public Task<ScriptStatusResponseV2> StartScriptAsync(StartScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions)
+//             {
+//                 throw new NotImplementedException();
+//             }
+//
+//             public Task<ScriptStatusResponseV2> GetStatusAsync(ScriptStatusRequestV2 request, HalibutProxyRequestOptions proxyRequestOptions)
+//             {
+//                 throw new NotImplementedException();
+//             }
+//
+//             public Task<ScriptStatusResponseV2> CancelScriptAsync(CancelScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions)
+//             {
+//                 throw new NotImplementedException();
+//             }
+//
+//             public async Task CompleteScriptAsync(CompleteScriptCommandV2 command, HalibutProxyRequestOptions proxyRequestOptions)
+//             {
+//                 await Task.CompletedTask;
+//             }
+//         }
+//     }
+// }
