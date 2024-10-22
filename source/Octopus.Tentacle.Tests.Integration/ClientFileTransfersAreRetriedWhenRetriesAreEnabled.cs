1c13439726a538c13002090726edecca8539e075
Samdanae Imran <samdanae.imran@octopus.com>
2024-10-23 10:12:44 +1300

    Comment out all tests other than file transfer tests

--- source/Octopus.Tentacle.Tests.Integration/ClientFileTransfersAreRetriedWhenRetriesAreEnabled.cs
+++ source/Octopus.Tentacle.Tests.Integration/ClientFileTransfersAreRetriedWhenRetriesAreEnabled.cs
@@ -1,108 +1,108 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Halibut;
-using NUnit.Framework;
-using Octopus.Tentacle.CommonTestUtils;
-using Octopus.Tentacle.CommonTestUtils.Diagnostics;
-using Octopus.Tentacle.Contracts.ClientServices;
-using Octopus.Tentacle.Tests.Integration.Common.Builders.Decorators;
-using Octopus.Tentacle.Tests.Integration.Support;
-using Octopus.Tentacle.Tests.Integration.Support.ExtensionMethods;
-using Octopus.Tentacle.Tests.Integration.Support.TestAttributes;
-using Octopus.Tentacle.Tests.Integration.Util.Builders;
-using Octopus.Tentacle.Tests.Integration.Util.Builders.Decorators;
-using Octopus.Tentacle.Tests.Integration.Util.TcpTentacleHelpers;
-
-namespace Octopus.Tentacle.Tests.Integration
-{
-    public class ClientFileTransfersAreRetriedWhenRetriesAreEnabled : IntegrationTest
-    {
-        [Test]
-        [TentacleConfigurations(testCommonVersions: true, scriptServiceToTest: ScriptServiceVersionToTest.None)]
-        [SkipOnEnvironmentsWithKnownPerformanceIssues("it relies on timing, which may be inconsistent within the environment")]
-        public async Task FailedUploadsAreRetriedAndIsEventuallySuccessful(TentacleConfigurationTestCase tentacleConfigurationTestCase)
-        {
-            await using var clientTentacle = await tentacleConfigurationTestCase.CreateBuilder()
-                .WithPortForwarderDataLogging()
-                .WithResponseMessageTcpKiller(out var responseMessageTcpKiller)
-                .WithTcpConnectionUtilities(Logger, out var tcpConnectionUtilities)
-                .WithTentacleServiceDecorator(new TentacleServiceDecoratorBuilder()
-                    .RecordMethodUsages<IAsyncClientFileTransferService>(out var recordedUsages)
-                    .DecorateFileTransferServiceWith(d => d
-                        .BeforeUploadFile(
-                            async () =>
-                            {
-                                await tcpConnectionUtilities.RestartTcpConnection();
-
-                                // Only kill the connection the first time, causing the upload
-                                // to succeed - and therefore failing the test - if retries are attempted
-                                if (recordedUsages.For(nameof(IAsyncClientFileTransferService.UploadFileAsync)).LastException is null)
-                                {
-                                    responseMessageTcpKiller.KillConnectionOnNextResponse();
-                                }
-                            }))
-                    .Build())
-                .Build(CancellationToken);
-
-            var inMemoryLog = new InMemoryLog();
-
-            var remotePath = Path.Combine(clientTentacle.TemporaryDirectory.DirectoryPath, "UploadFile.txt");
-
-            var res = await clientTentacle.TentacleClient.UploadFile(remotePath, DataStream.FromString("Hello"), CancellationToken, inMemoryLog);
-            res.Length.Should().Be(5);
-
-            recordedUsages.For(nameof(IAsyncClientFileTransferService.UploadFileAsync)).LastException.Should().NotBeNull();
-            recordedUsages.For(nameof(IAsyncClientFileTransferService.UploadFileAsync)).Started.Should().Be(2);
-
-            var downloadFile = await clientTentacle.TentacleClient.DownloadFile(remotePath, CancellationToken);
-            var actuallySent = await downloadFile.GetUtf8String(CancellationToken);
-            actuallySent.Should().Be("Hello");
-
-            inMemoryLog.ShouldHaveLoggedRetryAttemptsAndNoRetryFailures();
-        }
-
-        [Test]
-        [TentacleConfigurations(testCommonVersions: true, scriptServiceToTest: ScriptServiceVersionToTest.None)]
-        public async Task FailedDownloadsAreRetriedAndIsEventuallySuccessful(TentacleConfigurationTestCase tentacleConfigurationTestCase)
-        {
-            await using var clientTentacle = await tentacleConfigurationTestCase.CreateBuilder()
-                .WithPortForwarderDataLogging()
-                .WithResponseMessageTcpKiller(out var responseMessageTcpKiller)
-                .WithTcpConnectionUtilities(Logger, out var tcpConnectionUtilities)
-                .WithTentacleServiceDecorator(new TentacleServiceDecoratorBuilder()
-                    .RecordMethodUsages<IAsyncClientFileTransferService>(out var recordedUsages)
-                    .DecorateFileTransferServiceWith(d => d
-                        .BeforeDownloadFile(
-                            async () =>
-                            {
-                                await tcpConnectionUtilities.RestartTcpConnection();
-
-                                // Only kill the connection the first time, causing the upload
-                                // to succeed - and therefore failing the test - if retries are attempted
-                                if (recordedUsages.For(nameof(IAsyncClientFileTransferService.DownloadFileAsync)).LastException is null)
-                                {
-                                    responseMessageTcpKiller.KillConnectionOnNextResponse();
-                                }
-                            }))
-                    .Build())
-                .Build(CancellationToken);
-
-            var inMemoryLog = new InMemoryLog();
-
-            var remotePath = Path.Combine(clientTentacle.TemporaryDirectory.DirectoryPath, "UploadFile.txt");
-
-            await clientTentacle.TentacleClient.UploadFile(remotePath, DataStream.FromString("Hello"), CancellationToken);
-            var downloadFile = await clientTentacle.TentacleClient.DownloadFile(remotePath, CancellationToken, inMemoryLog);
-            var actuallySent = await downloadFile.GetUtf8String(CancellationToken);
-
-            recordedUsages.For(nameof(IAsyncClientFileTransferService.DownloadFileAsync)).LastException.Should().NotBeNull();
-            recordedUsages.For(nameof(IAsyncClientFileTransferService.DownloadFileAsync)).Started.Should().Be(2);
-
-            actuallySent.Should().Be("Hello");
-
-            inMemoryLog.ShouldHaveLoggedRetryAttemptsAndNoRetryFailures();
-        }
-    }
-}
+// using System;
+// using System.IO;
+// using System.Threading.Tasks;
+// using FluentAssertions;
+// using Halibut;
+// using NUnit.Framework;
+// using Octopus.Tentacle.CommonTestUtils;
+// using Octopus.Tentacle.CommonTestUtils.Diagnostics;
+// using Octopus.Tentacle.Contracts.ClientServices;
+// using Octopus.Tentacle.Tests.Integration.Common.Builders.Decorators;
+// using Octopus.Tentacle.Tests.Integration.Support;
+// using Octopus.Tentacle.Tests.Integration.Support.ExtensionMethods;
+// using Octopus.Tentacle.Tests.Integration.Support.TestAttributes;
+// using Octopus.Tentacle.Tests.Integration.Util.Builders;
+// using Octopus.Tentacle.Tests.Integration.Util.Builders.Decorators;
+// using Octopus.Tentacle.Tests.Integration.Util.TcpTentacleHelpers;
+//
+// namespace Octopus.Tentacle.Tests.Integration
+// {
+//     public class ClientFileTransfersAreRetriedWhenRetriesAreEnabled : IntegrationTest
+//     {
+//         [Test]
+//         [TentacleConfigurations(testCommonVersions: true, scriptServiceToTest: ScriptServiceVersionToTest.None)]
+//         [SkipOnEnvironmentsWithKnownPerformanceIssues("it relies on timing, which may be inconsistent within the environment")]
+//         public async Task FailedUploadsAreRetriedAndIsEventuallySuccessful(TentacleConfigurationTestCase tentacleConfigurationTestCase)
+//         {
+//             await using var clientTentacle = await tentacleConfigurationTestCase.CreateBuilder()
+//                 .WithPortForwarderDataLogging()
+//                 .WithResponseMessageTcpKiller(out var responseMessageTcpKiller)
+//                 .WithTcpConnectionUtilities(Logger, out var tcpConnectionUtilities)
+//                 .WithTentacleServiceDecorator(new TentacleServiceDecoratorBuilder()
+//                     .RecordMethodUsages<IAsyncClientFileTransferService>(out var recordedUsages)
+//                     .DecorateFileTransferServiceWith(d => d
+//                         .BeforeUploadFile(
+//                             async () =>
+//                             {
+//                                 await tcpConnectionUtilities.RestartTcpConnection();
+//
+//                                 // Only kill the connection the first time, causing the upload
+//                                 // to succeed - and therefore failing the test - if retries are attempted
+//                                 if (recordedUsages.For(nameof(IAsyncClientFileTransferService.UploadFileAsync)).LastException is null)
+//                                 {
+//                                     responseMessageTcpKiller.KillConnectionOnNextResponse();
+//                                 }
+//                             }))
+//                     .Build())
+//                 .Build(CancellationToken);
+//
+//             var inMemoryLog = new InMemoryLog();
+//
+//             var remotePath = Path.Combine(clientTentacle.TemporaryDirectory.DirectoryPath, "UploadFile.txt");
+//
+//             var res = await clientTentacle.TentacleClient.UploadFile(remotePath, DataStream.FromString("Hello"), CancellationToken, inMemoryLog);
+//             res.Length.Should().Be(5);
+//
+//             recordedUsages.For(nameof(IAsyncClientFileTransferService.UploadFileAsync)).LastException.Should().NotBeNull();
+//             recordedUsages.For(nameof(IAsyncClientFileTransferService.UploadFileAsync)).Started.Should().Be(2);
+//
+//             var downloadFile = await clientTentacle.TentacleClient.DownloadFile(remotePath, CancellationToken);
+//             var actuallySent = await downloadFile.GetUtf8String(CancellationToken);
+//             actuallySent.Should().Be("Hello");
+//
+//             inMemoryLog.ShouldHaveLoggedRetryAttemptsAndNoRetryFailures();
+//         }
+//
+//         [Test]
+//         [TentacleConfigurations(testCommonVersions: true, scriptServiceToTest: ScriptServiceVersionToTest.None)]
+//         public async Task FailedDownloadsAreRetriedAndIsEventuallySuccessful(TentacleConfigurationTestCase tentacleConfigurationTestCase)
+//         {
+//             await using var clientTentacle = await tentacleConfigurationTestCase.CreateBuilder()
+//                 .WithPortForwarderDataLogging()
+//                 .WithResponseMessageTcpKiller(out var responseMessageTcpKiller)
+//                 .WithTcpConnectionUtilities(Logger, out var tcpConnectionUtilities)
+//                 .WithTentacleServiceDecorator(new TentacleServiceDecoratorBuilder()
+//                     .RecordMethodUsages<IAsyncClientFileTransferService>(out var recordedUsages)
+//                     .DecorateFileTransferServiceWith(d => d
+//                         .BeforeDownloadFile(
+//                             async () =>
+//                             {
+//                                 await tcpConnectionUtilities.RestartTcpConnection();
+//
+//                                 // Only kill the connection the first time, causing the upload
+//                                 // to succeed - and therefore failing the test - if retries are attempted
+//                                 if (recordedUsages.For(nameof(IAsyncClientFileTransferService.DownloadFileAsync)).LastException is null)
+//                                 {
+//                                     responseMessageTcpKiller.KillConnectionOnNextResponse();
+//                                 }
+//                             }))
+//                     .Build())
+//                 .Build(CancellationToken);
+//
+//             var inMemoryLog = new InMemoryLog();
+//
+//             var remotePath = Path.Combine(clientTentacle.TemporaryDirectory.DirectoryPath, "UploadFile.txt");
+//
+//             await clientTentacle.TentacleClient.UploadFile(remotePath, DataStream.FromString("Hello"), CancellationToken);
+//             var downloadFile = await clientTentacle.TentacleClient.DownloadFile(remotePath, CancellationToken, inMemoryLog);
+//             var actuallySent = await downloadFile.GetUtf8String(CancellationToken);
+//
+//             recordedUsages.For(nameof(IAsyncClientFileTransferService.DownloadFileAsync)).LastException.Should().NotBeNull();
+//             recordedUsages.For(nameof(IAsyncClientFileTransferService.DownloadFileAsync)).Started.Should().Be(2);
+//
+//             actuallySent.Should().Be("Hello");
+//
+//             inMemoryLog.ShouldHaveLoggedRetryAttemptsAndNoRetryFailures();
+//         }
+//     }
+// }
--- source/Octopus.Tentacle.Tests.Integration/TentacleStartupAndShutdownTests.cs
+++ source/Octopus.Tentacle.Tests.Integration/TentacleStartupAndShutdownTests.cs
@@ -1,72 +1,72 @@
-#nullable enable
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Halibut.Exceptions;
-using NUnit.Framework;
-using Octopus.Tentacle.Client.Scripts;
-using Octopus.Tentacle.CommonTestUtils.Builders;
-using Octopus.Tentacle.Contracts;
-using Octopus.Tentacle.Tests.Integration.Support;
-using Octopus.Tentacle.Tests.Integration.Support.ExtensionMethods;
-using Octopus.Tentacle.Tests.Integration.Support.TestAttributes;
-using Octopus.Tentacle.Tests.Integration.Util.Builders;
-
-namespace Octopus.Tentacle.Tests.Integration
-{
-    [IntegrationTestTimeout]
-    public class TentacleStartupAndShutdownTests : IntegrationTest
-    {
-        [Test]
-        [TentacleConfigurations]
-        [RequiresSudoOnLinux]
-        public async Task WhenRunningTentacleAsAServiceItShouldBeAbleToRestartItself(TentacleConfigurationTestCase tentacleConfigurationTestCase)
-        {
-            await using (var clientAndTentacle = await tentacleConfigurationTestCase
-                             .CreateBuilder()
-                             .InstallAsAService()
-                             .Build(CancellationToken))
-            {
-                
-                var startScriptCommand = new TestExecuteShellScriptCommandBuilder()                    
-                    .SetScriptBodyForCurrentOs(
-$@"cd ""{clientAndTentacle.RunningTentacle.TentacleExe.DirectoryName}""
-.\Tentacle.exe service --instance {clientAndTentacle.RunningTentacle.InstanceName} --stop --start",
-$@"#!/bin/sh
-cd ""{clientAndTentacle.RunningTentacle.TentacleExe.DirectoryName}""
-./Tentacle service --instance {clientAndTentacle.RunningTentacle.InstanceName} --stop --start")
-                    .Build();
-
-                (Client.Scripts.Models.ScriptExecutionResult ScriptExecutionResult, List<ProcessOutput> ProcessOutput) result;
-
-                try
-                {
-                    result = await clientAndTentacle.TentacleClient.ExecuteScript(startScriptCommand, CancellationToken);
-                }
-                catch (ServiceInvocationHalibutClientException ex)
-                {
-                    Logger.Information(ex, "ServiceInvocationHalibutClientException thrown while Tentacle was restarting itself. This can be ignored for the purpose of this test.");
-
-                    // Making Tentacle restart itself can cause internal errors with Script Service
-                    // Execute the script again to get the final result and logs. This will not rerun the script.
-                    result = await clientAndTentacle.TentacleClient.ExecuteScript(startScriptCommand, CancellationToken);
-                }
-
-                result.LogExecuteScriptOutput(Logger);
-
-                result.ProcessOutput.Any(x => x.Text.Contains("Stopping service")).Should().BeTrue("Stopping service should be logged");
-                result.ScriptExecutionResult.State.Should().Be(ProcessState.Complete);
-
-                startScriptCommand = new TestExecuteShellScriptCommandBuilder()                    
-                    .SetScriptBody(new ScriptBuilder().Print("Running..."))
-                    .Build();
-
-                result = await clientAndTentacle.TentacleClient.ExecuteScript(startScriptCommand, CancellationToken);
-                result.ProcessOutput.Any(x => x.Text.Contains("Running...")).Should().BeTrue("Running... should be logged");
-                result.ScriptExecutionResult.ExitCode.Should().Be(0);
-                result.ScriptExecutionResult.State.Should().Be(ProcessState.Complete);
-            }
-        }
-    }
-}
+// #nullable enable
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using FluentAssertions;
+// using Halibut.Exceptions;
+// using NUnit.Framework;
+// using Octopus.Tentacle.Client.Scripts;
+// using Octopus.Tentacle.CommonTestUtils.Builders;
+// using Octopus.Tentacle.Contracts;
+// using Octopus.Tentacle.Tests.Integration.Support;
+// using Octopus.Tentacle.Tests.Integration.Support.ExtensionMethods;
+// using Octopus.Tentacle.Tests.Integration.Support.TestAttributes;
+// using Octopus.Tentacle.Tests.Integration.Util.Builders;
+//
+// namespace Octopus.Tentacle.Tests.Integration
+// {
+//     [IntegrationTestTimeout]
+//     public class TentacleStartupAndShutdownTests : IntegrationTest
+//     {
+//         [Test]
+//         [TentacleConfigurations]
+//         [RequiresSudoOnLinux]
+//         public async Task WhenRunningTentacleAsAServiceItShouldBeAbleToRestartItself(TentacleConfigurationTestCase tentacleConfigurationTestCase)
+//         {
+//             await using (var clientAndTentacle = await tentacleConfigurationTestCase
+//                              .CreateBuilder()
+//                              .InstallAsAService()
+//                              .Build(CancellationToken))
+//             {
+//                 
+//                 var startScriptCommand = new TestExecuteShellScriptCommandBuilder()                    
+//                     .SetScriptBodyForCurrentOs(
+// $@"cd ""{clientAndTentacle.RunningTentacle.TentacleExe.DirectoryName}""
+// .\Tentacle.exe service --instance {clientAndTentacle.RunningTentacle.InstanceName} --stop --start",
+// $@"#!/bin/sh
+// cd ""{clientAndTentacle.RunningTentacle.TentacleExe.DirectoryName}""
+// ./Tentacle service --instance {clientAndTentacle.RunningTentacle.InstanceName} --stop --start")
+//                     .Build();
+//
+//                 (Client.Scripts.Models.ScriptExecutionResult ScriptExecutionResult, List<ProcessOutput> ProcessOutput) result;
+//
+//                 try
+//                 {
+//                     result = await clientAndTentacle.TentacleClient.ExecuteScript(startScriptCommand, CancellationToken);
+//                 }
+//                 catch (ServiceInvocationHalibutClientException ex)
+//                 {
+//                     Logger.Information(ex, "ServiceInvocationHalibutClientException thrown while Tentacle was restarting itself. This can be ignored for the purpose of this test.");
+//
+//                     // Making Tentacle restart itself can cause internal errors with Script Service
+//                     // Execute the script again to get the final result and logs. This will not rerun the script.
+//                     result = await clientAndTentacle.TentacleClient.ExecuteScript(startScriptCommand, CancellationToken);
+//                 }
+//
+//                 result.LogExecuteScriptOutput(Logger);
+//
+//                 result.ProcessOutput.Any(x => x.Text.Contains("Stopping service")).Should().BeTrue("Stopping service should be logged");
+//                 result.ScriptExecutionResult.State.Should().Be(ProcessState.Complete);
+//
+//                 startScriptCommand = new TestExecuteShellScriptCommandBuilder()                    
+//                     .SetScriptBody(new ScriptBuilder().Print("Running..."))
+//                     .Build();
+//
+//                 result = await clientAndTentacle.TentacleClient.ExecuteScript(startScriptCommand, CancellationToken);
+//                 result.ProcessOutput.Any(x => x.Text.Contains("Running...")).Should().BeTrue("Running... should be logged");
+//                 result.ScriptExecutionResult.ExitCode.Should().Be(0);
+//                 result.ScriptExecutionResult.State.Should().Be(ProcessState.Complete);
+//             }
+//         }
+//     }
+// }
