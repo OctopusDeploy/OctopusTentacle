8e60c3f706a94183a0db6c55e7c803dd9ad28bd4
William Laugesen <will.laugesen@octopus.com>
2025-11-03 16:41:10 +1300

    Removed dependency on `Octopus.Client.Model.SemanticVersion` that used the NuGetVersion.ToString to instead use SemanticVersion directly.

--- source/Octopus.Tentacle/Properties/OctopusTentacle.cs
+++ source/Octopus.Tentacle/Properties/OctopusTentacle.cs
@@ -1,8 +1,7 @@
 using System.Reflection;
-using Octopus.Client.Model;
 using Octopus.Tentacle.Util;
 using Octopus.Tentacle.Versioning;
-using AssemblyExtensions = Octopus.Tentacle.Versioning.AssemblyExtensions;
+using Octopus.Versioning.Semver;
 
 namespace Octopus.Tentacle.Properties
 {
@@ -10,8 +9,8 @@ namespace Octopus.Tentacle.Properties
     {
         public static readonly Assembly Assembly = typeof (OctopusTentacle).Assembly;
         public static readonly string InformationalVersion = Assembly.GetInformationalVersion() ?? "0.0.0";
-        public static readonly SemanticVersionInfo SemanticVersionInfo = new SemanticVersionInfo(Assembly);
-        public static readonly SemanticVersion Version = new SemanticVersion(SemanticVersionInfo.NuGetVersion);
+        public static readonly SemanticVersionInfo SemanticVersionInfo = new(Assembly);
+        public static readonly SemanticVersion Version = SemanticVersionInfo.SemanticVersion;
         public static readonly string[] EnvironmentInformation = EnvironmentHelper.SafelyGetEnvironmentInformation();
     }
 }
