50630ad9020ed4c7e2d23eba73b03aed619b7650
Luke Butters <luke.butters@octopus.com>
2025-11-19 15:03:26 +1100

    When we can't write script output to the script log, log to nlog (#1030)
    * When we can't write script outout to the script log, log to serilog
    
    * Remove undeeded dotsettings change
    
    ---------
    
    Co-authored-by: David Staniec <david.staniec@octopus.com>

--- source/Octopus.Tentacle.Core/Services/Scripts/RunningScript.cs
+++ source/Octopus.Tentacle.Core/Services/Scripts/RunningScript.cs
@@ -178,9 +178,9 @@ namespace Octopus.Tentacle.Core.Services.Scripts
                     shellPath,
                     shell.FormatCommandArguments(workspace.BootstrapScriptFilePath, workspace.ScriptArguments, false),
                     workspace.WorkingDirectory,
-                    output => writer.WriteOutput(ProcessOutputSource.Debug, output),
-                    output => writer.WriteOutput(ProcessOutputSource.StdOut, output),
-                    output => writer.WriteOutput(ProcessOutputSource.StdErr, output),
+                    LogScriptOutputTo(writer, ProcessOutputSource.Debug),
+                    LogScriptOutputTo(writer, ProcessOutputSource.StdOut),
+                    LogScriptOutputTo(writer, ProcessOutputSource.StdErr),
                     environmentVariables,
                     token);
 
@@ -192,6 +192,19 @@ namespace Octopus.Tentacle.Core.Services.Scripts
                 writer.WriteOutput(ProcessOutputSource.StdErr, ex.ToString());
 
                 return ScriptExitCodes.PowershellInvocationErrorExitCode;
+            }
+        }
+
+        Action<string> LogScriptOutputTo(IScriptLogWriter logOutput, ProcessOutputSource level)
+        {
+            try
+            {
+                return output => logOutput.WriteOutput(level, output);
+            }
+            catch (Exception e)
+            {
+                log.Warn(e, $"Could not write script output to log, for task {taskId}");
+                throw;
             }
         }
     }
