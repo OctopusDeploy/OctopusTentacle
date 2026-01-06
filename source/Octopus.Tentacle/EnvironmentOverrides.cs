03c4b9c843f5a7b42ac3222242cefdaf092dc476
Rhys Parry <rhys.parry@octopus.com>
2026-01-06 12:13:25 +1000

    Maintain the existing behavior
    - Use true/false (case-insensitive) for environment variable values
    - Invert the toggle so that existing behavior is maintained
    - Users can opt-in to system configure TLS by setting the environment
      variable to false

--- source/Octopus.Tentacle/EnvironmentOverrides.cs
+++ source/Octopus.Tentacle/EnvironmentOverrides.cs
@@ -4,7 +4,19 @@ namespace Octopus.Tentacle
 {
     public static class EnvironmentOverrides
     {
+        /// <summary>
+        /// By default, (i.e. with no environment variable set) we will use the legacy explicit SSL
+        /// configuration. For users that choose to opt-in to the new behavior early they can set
+        /// the OCTOPUS_TENTACLE_USE_LEGACY_TLS environment variable to "FALSE".
+        ///
+        /// In the future, the default will change to using the system default, and this flag will
+        /// exist to allow opting back into the legacy behavior by setting the
+        /// OCTOPUS_TENTACLE_USE_LEGACY_TLS environment variable to "TRUE".
+        /// </summary>
         public static bool UseLegacyExplicitSslConfiguration =>
-            Environment.GetEnvironmentVariable("OCTOPUS_TENTACLE_USE_LEGACY_TLS") == "YES";
+            !bool.FalseString.Equals(
+                Environment.GetEnvironmentVariable("OCTOPUS_TENTACLE_USE_LEGACY_TLS"),
+                StringComparison.OrdinalIgnoreCase
+            );
     }
 }
